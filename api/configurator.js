601dc07b1f94a52fcfb39c2d5ce040754d5d6d36
IONKA <ionka@reso.ru>
2020-03-25 14:29:41 +0300

    Тестирование метода /api/module

--- api/configurator.js
+++ api/configurator.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser')
 import converter from '../converters/menu'
 import consts from '../api/urls'
 
+import data from './data.js'
 app.use(express.json())
 app.use(cookieParser())
 
@@ -13,43 +14,44 @@ const modules = {}
 const menu = {}
 
 app.get('/module', (req, res) => {
-  try {
-    if(req.cookies){
-      axios.defaults.headers.common['Authorization'] = req.cookies['auth._token.local']
-    }
-    modules.getItems = () => {
-      return new Promise((resolve, reject) => {
-        axios({url: `https://mobile2.reso.ru${consts.MODULE}`, method: 'GET'})
-          .then(resp => {
-            let modules = converter.modules(resp.data)
-            resolve(modules)
-          })
-          .catch(err => {
-            res.send(err.response.data)
-          })
-      })
-    }
-    menu.getItems = (modules) => {
-      return new Promise((resolve, reject) => {
-        axios.all(modules.map(l => axios.get(`${consts.MENU}/${l.id}`)))
-          .then(axios.spread(function (...res) {
-            resolve(res)
-          })).catch(err => {
-          res.send(err.response.data)
-        })
-      })
-    }
-    modules.getItems().then((modules) => {
-      menu.getItems(modules).then((menu) => {
-        converter.sidebar(modules, menu)
-        res.send(modules)
-      }).catch(err => {
-        res.send(err.response.data)
-      })
-    })
-  } catch (e) {
-    res.send(e)
-  }
+  res.send(data.userinfo)
+  // try {
+  //   if(req.cookies){
+  //     axios.defaults.headers.common['Authorization'] = req.cookies['auth._token.local']
+  //   }
+  //   modules.getItems = () => {
+  //     return new Promise((resolve, reject) => {
+  //       axios({url: `https://mobile2.reso.ru${consts.MODULE}`, method: 'GET'})
+  //         .then(resp => {
+  //           let modules = converter.modules(resp.data)
+  //           resolve(modules)
+  //         })
+  //         .catch(err => {
+  //           res.send(err.response.data)
+  //         })
+  //     })
+  //   }
+  //   menu.getItems = (modules) => {
+  //     return new Promise((resolve, reject) => {
+  //       axios.all(modules.map(l => axios.get(`${consts.MENU}/${l.id}`)))
+  //         .then(axios.spread(function (...res) {
+  //           resolve(res)
+  //         })).catch(err => {
+  //         res.send(err.response.data)
+  //       })
+  //     })
+  //   }
+  //   modules.getItems().then((modules) => {
+  //     menu.getItems(modules).then((menu) => {
+  //       converter.sidebar(modules, menu)
+  //       res.send(modules)
+  //     }).catch(err => {
+  //       res.send(err.response.data)
+  //     })
+  //   })
+  // } catch (e) {
+  //   res.send(e)
+  // }
 })
 
 module.exports = {
