df46a05db6786bacbb2debeb2662e881434d3e9d
IONKA <ionka@reso.ru>
2020-04-13 17:44:14 +0300

    Добавлено отображение превью страницы.

--- components/Pages/PreviewPage/Preview.vue
+++ components/Pages/PreviewPage/Preview.vue
@@ -1,10 +1,25 @@
 <template>
-
+<div>
+  <v-runtime-template :template="content"></v-runtime-template>
+</div>
 </template>
 
 <script>
+  import VRuntimeTemplate from "v-runtime-template";
   export default {
-    name: 'Preview'
+    name: 'Preview',
+    components: {VRuntimeTemplate},
+    head: {
+      title: 'Карусель'
+    },
+    async asyncData ({ $axios, store, route }) {
+      let dataPage = await $axios.get(`/wp-json/wpreso/v1/previews/${route.params.pageId}`)
+      let dataMenu = await $axios.get(`/wp-json/wp/v2/component/354`)
+      store.commit('pages/setMenu', dataMenu.data.acf.list);
+      return {
+        content: dataPage.data.content.rendered,
+      }
+    }
   }
 </script>
 
