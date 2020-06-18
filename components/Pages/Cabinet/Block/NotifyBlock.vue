7752657771a3389b088e304611562e656c90e5c1
IONKA <ionka@reso.ru>
2020-06-18 16:01:16 +0300

    Добавлена реализация вывода информации на странице по HTML шаблонам конфигуратора.

---
+++ components/Pages/Cabinet/Block/NotifyBlock.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-runtime-template :template="templateData"></v-runtime-template>
+  </div>
+</template>
+
+<script>
+  import ContentBlock from './ContentBlock'
+  import VRuntimeTemplate from "v-runtime-template";
+  export default {
+    name: 'NotifyBlock',
+    components: {ContentBlock, VRuntimeTemplate},
+    props: {
+      itemId: {
+        type: String,
+        required: true,
+        default: () => null
+      },
+      name: {
+        type: String,
+        required: true,
+        default: () => null
+      }
+    },
+    computed: {
+      templateData: {
+        get: function () {
+          return this.$store.getters['menu/getMenuById'](this.itemId).SVJCARDGRID
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
