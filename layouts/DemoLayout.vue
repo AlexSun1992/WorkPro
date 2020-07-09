eb0f367dad78ad83d5f204001d17c9c65a9a633c
Головлев Вячеслав <golvi@reso.ru>
2020-07-09 13:57:24 +0300

    Уменьшенное меню.

--- layouts/DemoLayout.vue
+++ layouts/DemoLayout.vue
@@ -1,72 +1,75 @@
 <template>
   <div class="app">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    <Sidebar :nav-items="nav"/>
     <Header/>
     <div class="wrapper">
+      <Sidebar :nav-items="nav"/>
       <div class="body">
         <main class="main">
           <div class="container-fluid">
             <nuxt/>
           </div>
         </main>
+        <Footer/>
       </div>
-      <Footer/>
     </div>
   </div>
 </template>
 <script>
-  import Header from '~/components/Pages/Cabinet/Header/Header'
-  import Footer from '~/components/Pages/Cabinet/Footer/Footer'
-  import Sidebar from '~/components/Pages/Cabinet/Sidebar/Sidebar'
-  import breadcrumbs from '~/converters/breadcrumbs'
+    import Header from '~/components/Pages/Cabinet/Header/Header'
+    import Footer from '~/components/Pages/Cabinet/Footer/Footer'
+    import Sidebar from '~/components/Pages/Cabinet/Sidebar/Sidebar'
+    import breadcrumbs from '~/converters/breadcrumbs'
 
-  export default {
-    name: 'full',
-    head: {
-      title: 'РЕСО-Гарантия'
-    },
-    components: {
-      Header,
-      Sidebar,
-      Footer
-    },
-    mounted () {
-      // this.$store.dispatch('menu/fetchMenu', this.$route.params)
-    },
-    watch: {
-      $route(to, from) {
-        if(to.meta === 'Cabinet'){
-          this.setParams();
-        }else{
-          let url;
-          if (to.path === '/') {
-            url = 'index'
-          }
-          this.$store.dispatch('pages/fetchPageByUrl', url);
+    export default {
+        name: 'full',
+        head: {
+            title: 'РЕСО-Гарантия'
+        },
+        components: {
+            Header,
+            Sidebar,
+            Footer
+        },
+        mounted() {
+            // this.$store.dispatch('menu/fetchMenu', this.$route.params)
+        },
+        watch: {
+            $route(to, from) {
+                if (to.meta === 'Cabinet') {
+                    this.setParams();
+                } else {
+                    let url;
+                    if (to.path === '/') {
+                        url = 'index'
+                    }
+                    this.$store.dispatch('pages/fetchPageByUrl', url);
+                }
+            }
+        },
+        methods: {
+            setParams() {
+                const bc = breadcrumbs.getData(this.$store.getters['menu/menu'], this.$route.params)
+                this.$store.commit('menu/setBreadcrumbs', bc)
+                this.$store.dispatch('card/setCard', {
+                    page: this.$route.params,
+                    settings: bc.slice(-1).pop()
+                });
+            }
+        },
+        computed: {
+            nav() {
+                return this.$store.getters['menu/menu'][0].children
+            },
+            name() {
+                return this.$route.name
+            },
+            items() {
+                return this.$store.getters['menu/breadcrumbs']
+            }
         }
-      }
-    },
-    methods: {
-      setParams  () {
-        const bc = breadcrumbs.getData(this.$store.getters['menu/menu'], this.$route.params)
-        this.$store.commit('menu/setBreadcrumbs', bc)
-        this.$store.dispatch('card/setCard', {page: this.$route.params, settings: bc.slice(-1).pop()});
-      }
-    },
-    computed: {
-      nav () {
-        return this.$store.getters['menu/menu'][0].children
-      },
-      name () {
-        return this.$route.name
-      },
-      items () {
-        return this.$store.getters['menu/breadcrumbs']
-      }
     }
-  }
 </script>
 <style lang="scss" scoped>
-  @import '~/assets/scss/style_new.scss'
+    @import '~/assets/scss/style_new.scss'
 </style>
