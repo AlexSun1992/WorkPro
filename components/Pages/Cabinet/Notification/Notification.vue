5b08d4e6fe97e797343942bff295a3a5357c0bd3
Головлев Вячеслав <golvi@reso.ru>
2020-07-15 09:16:41 +0300

    Мои полисы

--- components/Pages/Cabinet/Notification/Notification.vue
+++ components/Pages/Cabinet/Notification/Notification.vue
@@ -1,37 +1,56 @@
 <template>
-  <div class="block  bg-white block-border-one">
+  <div class="block  bg-one block-border-two">
     <div class="block-title">
-      Уведомления
+      Мои полисы
     </div>
-    <div class="block-notification-item">
+    <div class="block-mypolicies-item">
       <svg width="23" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M21.3584 11.7498V17.183L17.0103 12.804L15.848 13.9734L20.1961 18.3524H2.80395L7.15197 13.9862L5.98967 12.804L1.64165 17.183V5.69343L11.5 12.7656L14.5118 10.6059C14.0016 10.3226 13.5212 9.98798 13.078 9.60721L11.5 10.7382L3.71597 5.15139H9.85835V3.50393H0V19.9999H23V11.7498H21.3584ZM19.5852 6.72202C20.2467 6.72287 20.8935 6.52623 21.4439 6.157C21.9942 5.78777 22.4233 5.26255 22.6769 4.64783C22.9304 4.03311 22.9969 3.35653 22.8681 2.70373C22.7393 2.05094 22.4208 1.45128 21.9531 0.980668C21.4853 0.510058 20.8893 0.189656 20.2405 0.0600222C19.5917 -0.0696118 18.9193 -0.00264938 18.3083 0.252433C17.6973 0.507515 17.1753 0.939248 16.8083 1.49298C16.4414 2.04671 16.2459 2.69754 16.2468 3.36308C16.2479 4.25358 16.6 5.10729 17.2258 5.73696C17.8517 6.36664 18.7001 6.72089 19.5852 6.72202Z"/>
       </svg>
-      <div class="block-notification-item-text">
-        Добро пожаловать на страницу личного кабинета!
+      <div class="block-mypolicies-item-object">
+        ОСАГО,
       </div>
-      <div class="block-notification-btn">
-        <b-button type="button" ><div>Отметить как прочтенное</div></b-button>
-        <b-button type="button" class="notification-close"><div>Отметить как прочтенное</div></b-button>
+      <div class="block-mypolicies-item-number">
+        EE1011000091
+      </div>
+      <div class="block-mypolicies-item-name">
+        Nissan qashqai, 2008, М664Р3150
+      </div>
+      <div class="block-mypolicies-date">
+        по 04.12.2020
+      </div>
+      <div class="block-btn">
+        <b-button type="button" class="arrow-right-bold"><div>Перейти</div></b-button>
       </div>
     </div>
-    <div class="block-notification-item">
+    <div class="block-mypolicies-item">
       <svg width="23" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M21.3584 11.7498V17.183L17.0103 12.804L15.848 13.9734L20.1961 18.3524H2.80395L7.15197 13.9862L5.98967 12.804L1.64165 17.183V5.69343L11.5 12.7656L14.5118 10.6059C14.0016 10.3226 13.5212 9.98798 13.078 9.60721L11.5 10.7382L3.71597 5.15139H9.85835V3.50393H0V19.9999H23V11.7498H21.3584ZM19.5852 6.72202C20.2467 6.72287 20.8935 6.52623 21.4439 6.157C21.9942 5.78777 22.4233 5.26255 22.6769 4.64783C22.9304 4.03311 22.9969 3.35653 22.8681 2.70373C22.7393 2.05094 22.4208 1.45128 21.9531 0.980668C21.4853 0.510058 20.8893 0.189656 20.2405 0.0600222C19.5917 -0.0696118 18.9193 -0.00264938 18.3083 0.252433C17.6973 0.507515 17.1753 0.939248 16.8083 1.49298C16.4414 2.04671 16.2459 2.69754 16.2468 3.36308C16.2479 4.25358 16.6 5.10729 17.2258 5.73696C17.8517 6.36664 18.7001 6.72089 19.5852 6.72202Z"/>
       </svg>
-      <div class="block-notification-item-text">
-        Добро пожаловать на страницу личного кабинета!
+      <div class="block-mypolicies-item-object">
+        Каско,
       </div>
-      <div class="block-notification-btn">
-        <b-button type="button" ><div>Отметить как прочтенное</div></b-button>
-        <b-button type="button" class="notification-close"><div>Отметить как прочтенное</div></b-button>
+      <div class="block-mypolicies-item-number">
+        XXFFEE678978965468
+      </div>
+      <div class="block-mypolicies-item-name">
+        Nissan qashqai, 2008, М664Р3150
+      </div>
+      <div class="block-mypolicies-date">
+        по 04.12.2020
+      </div>
+      <div class="block-btn">
+        <b-button type="button" class="arrow-right-bold"><div>Перейти</div></b-button>
       </div>
     </div>
-    <div class="block-footer bg-white row justify-content-between align-items-center">
-      <a href="/" class="col-auto link-arrow-right">Отметить как прочитанное<i></i></a>
-      <a href="/" class="col-auto link-arrow-right">Посмотреть все уведомления<i></i></a>
+    <div class="block-footer bg-one row justify-content-between align-items-center">
+      <b-button  type="button" item-id="itemId" class="col-auto link-arrow-right" data-v-1d62bb47=""><div data-v-1d62bb47="">Отметить все как прочтенное</div></b-button>
+      <b-button type="button" item-id="itemId" class="col-auto link-arrow-right" data-v-1d62bb47=""><div data-v-1d62bb47="">Посмотреть все уведомления</div></b-button>
     </div>
   </div>
+
+
+  
 </template>
 
 <script>
