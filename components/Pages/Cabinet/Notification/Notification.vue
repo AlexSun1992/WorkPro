a6f542d2c585bf824c7649211deccb0f7da81a5a
Головлев Вячеслав <golvi@reso.ru>
2020-07-17 10:26:25 +0300

    2 новых блока

--- components/Pages/Cabinet/Notification/Notification.vue
+++ components/Pages/Cabinet/Notification/Notification.vue
@@ -1,56 +1,81 @@
 <template>
-  <div class="block  bg-one block-border-two">
+<div>
+ <div class="block  bg-one block-border-two">
     <div class="block-title">
-      Мои полисы
+      Страховые случаи
     </div>
-    <div class="block-mypolicies-item">
-      <svg width="23" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M21.3584 11.7498V17.183L17.0103 12.804L15.848 13.9734L20.1961 18.3524H2.80395L7.15197 13.9862L5.98967 12.804L1.64165 17.183V5.69343L11.5 12.7656L14.5118 10.6059C14.0016 10.3226 13.5212 9.98798 13.078 9.60721L11.5 10.7382L3.71597 5.15139H9.85835V3.50393H0V19.9999H23V11.7498H21.3584ZM19.5852 6.72202C20.2467 6.72287 20.8935 6.52623 21.4439 6.157C21.9942 5.78777 22.4233 5.26255 22.6769 4.64783C22.9304 4.03311 22.9969 3.35653 22.8681 2.70373C22.7393 2.05094 22.4208 1.45128 21.9531 0.980668C21.4853 0.510058 20.8893 0.189656 20.2405 0.0600222C19.5917 -0.0696118 18.9193 -0.00264938 18.3083 0.252433C17.6973 0.507515 17.1753 0.939248 16.8083 1.49298C16.4414 2.04671 16.2459 2.69754 16.2468 3.36308C16.2479 4.25358 16.6 5.10729 17.2258 5.73696C17.8517 6.36664 18.7001 6.72089 19.5852 6.72202Z"/>
-      </svg>
-      <div class="block-mypolicies-item-object">
-        ОСАГО,
-      </div>
-      <div class="block-mypolicies-item-number">
+    <div class="block-insurancecase-item">
+      <div class="img-insurancecase-kasko"></div>
+      <div class="block-insurancecase-text">
+        УЩЕРБ от 
+      <span class="block-insurancecase-item-number">
         EE1011000091
-      </div>
-      <div class="block-mypolicies-item-name">
-        Nissan qashqai, 2008, М664Р3150
-      </div>
-      <div class="block-mypolicies-date">
-        по 04.12.2020
+      </span>
       </div>
       <div class="block-btn">
         <b-button type="button" class="arrow-right-bold"><div>Перейти</div></b-button>
       </div>
     </div>
-    <div class="block-mypolicies-item">
-      <svg width="23" height="20" viewBox="0 0 23 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M21.3584 11.7498V17.183L17.0103 12.804L15.848 13.9734L20.1961 18.3524H2.80395L7.15197 13.9862L5.98967 12.804L1.64165 17.183V5.69343L11.5 12.7656L14.5118 10.6059C14.0016 10.3226 13.5212 9.98798 13.078 9.60721L11.5 10.7382L3.71597 5.15139H9.85835V3.50393H0V19.9999H23V11.7498H21.3584ZM19.5852 6.72202C20.2467 6.72287 20.8935 6.52623 21.4439 6.157C21.9942 5.78777 22.4233 5.26255 22.6769 4.64783C22.9304 4.03311 22.9969 3.35653 22.8681 2.70373C22.7393 2.05094 22.4208 1.45128 21.9531 0.980668C21.4853 0.510058 20.8893 0.189656 20.2405 0.0600222C19.5917 -0.0696118 18.9193 -0.00264938 18.3083 0.252433C17.6973 0.507515 17.1753 0.939248 16.8083 1.49298C16.4414 2.04671 16.2459 2.69754 16.2468 3.36308C16.2479 4.25358 16.6 5.10729 17.2258 5.73696C17.8517 6.36664 18.7001 6.72089 19.5852 6.72202Z"/>
-      </svg>
-      <div class="block-mypolicies-item-object">
-        Каско,
-      </div>
-      <div class="block-mypolicies-item-number">
-        XXFFEE678978965468
-      </div>
-      <div class="block-mypolicies-item-name">
-        Nissan qashqai, 2008, М664Р3150
-      </div>
-      <div class="block-mypolicies-date">
-        по 04.12.2020
+    <div class="block-insurancecase-item">
+      <div class="img-insurancecase-kasko"></div>
+      <div class="block-insurancecase-text">
+        УЩЕРБ от 
+      <span class="block-insurancecase-item-number">
+        EE1011000091
+      </span>
       </div>
       <div class="block-btn">
         <b-button type="button" class="arrow-right-bold"><div>Перейти</div></b-button>
       </div>
     </div>
-    <div class="block-footer bg-one row justify-content-between align-items-center">
-      <b-button  type="button" item-id="itemId" class="col-auto link-arrow-right" data-v-1d62bb47=""><div data-v-1d62bb47="">Отметить все как прочтенное</div></b-button>
-      <b-button type="button" item-id="itemId" class="col-auto link-arrow-right" data-v-1d62bb47=""><div data-v-1d62bb47="">Посмотреть все уведомления</div></b-button>
+    <div class="block-footer bg-one row justify-content-center align-items-center">
+      <button type="button" class="col-auto link-arrow-right">Архив страховых случаев</button>
     </div>
   </div>
-
-
   
+  <div class="row">
+        <div class="col-lg-4">
+          <div class="block bg-two h100 d-grid">
+            <div class="block-title">
+              ДМС для москвичей
+            </div>
+            <div class="block-offer">
+              Полис страхования здоровья  при обслуживании у стратегического партнера РЕСО-Гарантия – сети медцентров Medswiss
+            </div>
+            <a href="" class="block-offer-button">
+              Полис страхования<br>от <b>17 100</b> рублей
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="block bg-tree h100 d-grid">
+            <div class="block-title">
+              Туризм
+            </div>
+            <div class="block-offer">
+              <img src="/img/card2.svg">
+            </div>
+            <a href="" class="block-offer-button">
+              Полис “Активная защита” для<br>любителей активного отдыха
+            </a>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="block bg-for h100  d-grid">
+            <div class="block-title">
+              Несчастный случай
+            </div>
+            <div class="block-offer">
+              <img src="/img/card3.svg">
+            </div>
+            <a href=""  class="block-offer-button">
+              Получите доступ к<br>качественной медицине
+            </a>
+          </div>
+        </div>
+  </div>
+
+</div>
 </template>
 
 <script>
@@ -62,4 +87,5 @@
 <style scoped>
 
 </style>
+
 
