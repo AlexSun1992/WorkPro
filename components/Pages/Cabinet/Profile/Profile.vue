6601b48e66b408da6f6c88b98f3dd8578c76a440
Головлев Вячеслав <golvi@reso.ru>
2020-08-06 10:13:45 +0300

    Правки в размещение сайдбара

--- components/Pages/Cabinet/Profile/Profile.vue
+++ components/Pages/Cabinet/Profile/Profile.vue
@@ -4,8 +4,8 @@
       <div>
       <div class="block-title pt-0 position-relative mt-2 mb-4"><i class="icon-my-profile"></i>Мой профиль</div>
         <div class="profile row"> 
-          <profile-info @field-changed="refresh()" @saved="showSaveToast()" @error="showErrorToast()" v-if="show" @load="dataLoaded=true" @cancel="refresh()" :params="params" class="bg-six block-border-one block col-lg-9 col-12"></profile-info>
-          <profile-side-block v-if="dataLoaded" class="col-lg-3 d-none d-lg-block">
+          <profile-info @field-changed="refresh()" @saved="showSaveToast()" @error="showErrorToast()" v-if="show" @load="dataLoaded=true" @cancel="refresh()" :params="params" class="bg-six block-border-one block col-xl-9 col-12"></profile-info>
+          <profile-side-block v-if="dataLoaded" class="col-xl-3 d-none d-xl-block">
             <slot></slot>
           </profile-side-block>
         </div>
