c10b328919b01bc29c0d393e00a24b3ad88db36e
Головлев Вячеслав <golvi@reso.ru>
2020-09-08 23:23:46 +0300

    Главная страница

--- layouts/DemoMainPageLayout.vue
+++ layouts/DemoMainPageLayout.vue
@@ -1,13 +1,195 @@
 <template>
-    <div>Шапка сайта<nuxt/>Подвал сайта</div>
+<div>
+
+<MainHeader/>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="block-container block-container-big mt-1_25">
+                        <div class="block-title">
+                            ваплывждп ывжадлп
+                        </div>
+                        <div class="block-description">
+                            <strong>dfklsad;lfk ;asdfk ;asdk</strong> f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas f;k asf;k as;fkas ;dfkj as df;lk
+                        </div>
+                        <div class="block-img">
+                            <img src="">
+                        </div>
+                        <div class="block-button">
+                            <a href="" class="btn-icon-right icon-arrow-right">Каско</a>
+                            <a href="" class="btn-icon-right icon-arrow-right">ОСАГО</a>
+                            <a href="" class="btn-icon-right icon-arrow-right">Зеленая карта</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-1_25">
+                <div class="col-12">
+                    <div class="buy-block">
+                        1
+                    </div>
+                </div>
+            </div>
+            <div class="row flex-wrap">
+                <div class="col-6 d-flex">
+                    <div class="block-container mt-1_25">
+                        <div class="block-title">
+                            ваплывждп ывжадлп
+                        </div>
+                        <div class="block-description">
+                            dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas f;k asf;k as;fkas ;dfkj as df;lk
+                        </div>
+                        <div class="block-img">
+                            <img src="">
+                        </div>
+                        <div class="block-button">
+                            <a href="" class="btn-icon-right icon-arrow-right">Каско</a>
+                            <a href="" class="btn-icon-right icon-arrow-right">ОСАГО</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 d-flex">
+                    <div class="block-container mt-1_25">
+                        <div class="block-title">
+                            ваплывждп ывжадлп
+                        </div>
+                        <div class="block-description">
+                            dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas f;k asf;k as;fkas ;dfkj as df;lk
+                        </div>
+                        <div class="block-img">
+                            <img src="">
+                        </div>
+                        <div class="block-button">
+                            <a href="">1</a>
+                            <a href="">1</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 d-flex">
+                    <div class="block-container mt-1_25">
+                        <div class="block-title">
+                            ваплывждп ывжадлп
+                        </div>
+                        <div class="block-description">
+                            dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas f;k asf;k as;fkas ;dfkj as df;lk
+                        </div>
+                        <div class="block-img">
+                            <img src="">
+                        </div>
+                        <div class="block-button">
+                            <a href="">1</a>
+                            <a href="">1</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 d-flex">
+                    <div class="block-container mt-1_25">
+                        <div class="block-title">
+                            ваплывждп ывжадлп
+                        </div>
+                        <div class="block-description">
+                            dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas f;k asf;k as;fkas ;dfkj as df;lk
+                        </div>
+                        <div class="block-img">
+                            <img src="">
+                        </div>
+                        <div class="block-button">
+                            <a href="">1</a>
+                            <a href="">1</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3 d-flex">
+                    <div class="block-container mt-1_25">
+                        <div class="block-title">
+                            ваплывждп ывжадлп
+                        </div>
+                        <div class="block-description">
+                            dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas f;k asf;k as;fkas ;dfkj as df;lk
+                        </div>
+                        <div class="block-img">
+                            <img src="">
+                        </div>
+                        <div class="block-button">
+                            <a href="" class="btn-icon-right icon-arrow-right">Каско</a>
+                            <a href="" class="btn-icon-right icon-arrow-right">ОСАГО</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3 d-flex">
+                    <div class="block-container mt-1_25">
+                        <div class="block-title">
+                            ваплывждп ывжадлп
+                        </div>
+                        <div class="block-description">
+                            dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas f;k asf;k as;fkas ;dfkj as df;lk
+                        </div>
+                        <div class="block-img">
+                            <img src="">
+                        </div>
+                        <div class="block-button">
+                            <a href="">1</a>
+                            <a href="">1</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3 d-flex">
+                    <div class="block-container mt-1_25">
+                        <div class="block-title">
+                            ваплывждп ывжадлп
+                        </div>
+                        <div class="block-description">
+                            dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas f;k asf;k as;fkas ;dfkj as df;lk
+                        </div>
+                        <div class="block-img">
+                            <img src="">
+                        </div>
+                        <div class="block-button">
+                            <a href="">1</a>
+                            <a href="">1</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3 d-flex">
+                    <div class="block-container mt-1_25">
+                        <div class="block-title">
+                            ваплывждп ывжадлп
+                        </div>
+                        <div class="block-description">
+                            dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas f;k asf;k as;fkas ;dfkj as df;lk
+                        </div>
+                        <div class="block-img">
+                            <img src="">
+                        </div>
+                        <div class="block-button">
+                            <a href="">1</a>
+                            <a href="">1</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </main>
+    
+        <nuxt/>Подвал сайта
+    </div>
 </template>
 
 <script>
+  import MainHeader from '~/components/Pages/Main/Header/MainHeader'
   export default {
-    name: 'DemoMainPageLayout'
+    name: 'DemoMainPageLayout',
+    components: {
+            MainHeader}
   }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+    @import '~/assets/scss/main.scss'
 </style>
+
