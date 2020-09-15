685342ed01b9662bb33a9f028521865fdba68e1c
Канев Евгений <kanen@reso.ru>
2020-09-15 16:38:27 +0300

    [+] Компонент вопроса

--- osago_calculator_react/Calculator.vue
+++ osago_calculator_react/Calculator.vue
@@ -16,17 +16,35 @@
           {chosenListAnswers}
 
           {questionEl} -->
+        <Question :question="this.currentQuestion" />
       </div>
     </div>
   </div>
 </template>
 <script>
+import questions from "./fixtures/questions";
+import answers from "./fixtures/answers";
+
+import Question from "./Question";
+
 export default {
+  components: { Question },
   data() {
     return {
       stitle_h1: "Калькулятор ОСАГО",
-      stitle_h2: "Узнайте стоимость полиса за пару минут"
+      stitle_h2: "Узнайте стоимость полиса за пару минут",
+      questions,
+      answers,
+      quizId: 1,
+      firstQuestion: 2100,
+      partnerId: -1,
+      pageId: 1
     };
+  },
+  computed: {
+    currentQuestion() {
+      return this.questions.find(item => item.id === this.firstQuestion);
+    }
   }
 };
 </script>
---
+++ osago_calculator_react/Question.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div className="calc-action-title">
+      {{ question.name }}
+    </div>
+    <div className="calc-action">
+      <div className="calc-select-label">
+        {{ question.properties.sdop_info }}
+      </div>
+      <!-- {answers} -->
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["question"]
+};
+</script>
