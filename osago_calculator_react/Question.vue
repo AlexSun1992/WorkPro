bceee6be25342d9b47aa18027c63708f4fba7f5e
Канев Евгений <kanen@reso.ru>
2020-09-16 15:40:14 +0300

    [+] bootstrap-select

--- osago_calculator_react/Question.vue
+++ osago_calculator_react/Question.vue
@@ -7,7 +7,10 @@
       <div class="calc-select-label">
         {{ question.properties.sdop_info }}
       </div>
-      <ul class="select-items">
+      <ul
+        v-if="question.properties.sshow_type === 'choice'"
+        class="select-items"
+      >
         <li
           v-for="item in answers"
           :value="item.id"
@@ -17,12 +20,36 @@
           {{ item.name }}
         </li>
       </ul>
-      <!-- {answers} -->
+      <div v-else-if="question.properties.sshow_type === 'select'">
+        <b-form-select
+          v-model="selected"
+          :options="selectOptions"
+          @change="onSelectChange"
+          size="sm"
+          class="mt-3"
+        ></b-form-select>
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  props: ["question", "answers"]
+  props: ["question", "answers"],
+  computed: {
+    selectOptions() {
+      return this.answers.map(({ id: value, name: text }) => ({ value, text }));
+    }
+  },
+  methods: {
+    onSelectChange(id) {
+      const answer = this.answers.find(item => item.id === id);
+      this.$emit("choose-answer", answer);
+    }
+  },
+  data() {
+    return {
+      selected: null
+    };
+  }
 };
 </script>
