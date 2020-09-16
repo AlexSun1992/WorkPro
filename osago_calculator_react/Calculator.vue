62cbf65ffd6c30c2f9dbd68fcda82f2dc7433a24
Канев Евгений <kanen@reso.ru>
2020-09-16 14:45:27 +0300

    [+] Выбор ответа

--- osago_calculator_react/Calculator.vue
+++ osago_calculator_react/Calculator.vue
@@ -11,12 +11,18 @@
           {{ stitle_h2 }}
         </div>
 
-        <!-- {calcResult}
-
-          {chosenListAnswers}
-
-          {questionEl} -->
-        <Question :question="this.currentQuestion" />
+        <!-- {calcResult}-->
+        <ul class="select-finish-items">
+          <li v-for="answer in chosenAnswers" :key="answer.id">
+            {{ answer.name }}
+            <button class="select-finish-items-del"></button>
+          </li>
+        </ul>
+        <Question
+          :question="currentQuestion"
+          :answers="currentAnswers"
+          @choose-answer="chooseAnswer"
+        />
       </div>
     </div>
   </div>
@@ -35,19 +41,38 @@ export default {
       stitle_h2: "Узнайте стоимость полиса за пару минут",
       questions,
       answers,
+      chosenAnswers: [],
       quizId: 1,
       firstQuestion: 2100,
+      currentQuestionId: null,
       partnerId: -1,
       pageId: 1
     };
   },
+  methods: {
+    chooseAnswer: function(answer) {
+      this.chosenAnswers.push(answer);
+      this.currentQuestionId = Number(answer.properties.nnext_issue);
+    }
+  },
   computed: {
     currentQuestion() {
-      return this.questions.find(item => item.id === this.firstQuestion);
+      return this.questions.find(
+        item => item.id === (this.currentQuestionId || this.firstQuestion)
+      );
+    },
+    currentAnswers() {
+      return this.answers.filter(item => {
+        return (
+          String(item.properties.idclient_quiz_issue) ===
+            String(this.currentQuestion.id) &&
+          String(item.properties.lactive) === "1"
+        );
+      });
     }
   }
 };
 </script>
-<style scoped>
+<style>
 @import url("./calculator.css");
 </style>
--- osago_calculator_react/Question.vue
+++ osago_calculator_react/Question.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
-    <div className="calc-action-title">
+    <div class="calc-action-title">
       {{ question.name }}
     </div>
-    <div className="calc-action">
-      <div className="calc-select-label">
+    <div class="calc-action">
+      <div class="calc-select-label">
         {{ question.properties.sdop_info }}
       </div>
+      <ul class="select-items">
+        <li
+          v-for="item in answers"
+          :value="item.id"
+          :key="item.id"
+          @click="$emit('choose-answer', item)"
+        >
+          {{ item.name }}
+        </li>
+      </ul>
       <!-- {answers} -->
     </div>
   </div>
 </template>
 <script>
 export default {
-  props: ["question"]
+  props: ["question", "answers"]
 };
 </script>
