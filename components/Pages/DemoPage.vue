3994593d65e88b343b0cb98a55424e0bce91aca6
Головлев Вячеслав <golvi@reso.ru>
2020-09-17 14:47:10 +0300

    правки

--- components/Pages/DemoPage.vue
+++ components/Pages/DemoPage.vue
@@ -1,25 +1,70 @@
 <template>
+<<<<<<< HEAD
+  <div class="slider-bg pt-4 pb-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 block-title">Наши предложения</div>
+    </div>
+    <div class="row d-block">
+      <div class="col-12 p-0">
+    <agile :slidesToShow="4" :dots="false">
+      <div class="block-slider">
+        <div class="block-container block-container-slider">
+=======
   <div>
     <agile :slidesToShow="2" :infinite="false">
       <div class="col-3 d-flex">
         <div class="block-container mt-1_25">
+>>>>>>> master
           <div class="block-title">
-            ваплывждп ывжадлп
+            Сезон Renault
           </div>
           <div class="block-description">
-            dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas f;k asf;k as;fkas ;dfkj as df;lk
+            Скидки для новых автомобилей при покупке полиса в офисах компании
           </div>
           <div class="block-img">
-            <img src="">
+            <img src="/img/main/car.svg">
           </div>
           <div class="block-button">
-            <a href="" class="btn-icon-right icon-arrow-right">Каско</a>
-            <a href="" class="btn-icon-right icon-arrow-right">ОСАГО</a>
+            <a href="" class="btn-icon-right icon-arrow-right">Подробнее</a>
           </div>
         </div>
       </div>
-      <div class="col-3 d-flex">
-        <div class="block-container mt-1_25">
+      <div class="block-slider">
+        <div class="block-container block-container-slider">
+          <div class="block-title">
+            Московский хит
+          </div>
+          <div class="block-description">
+            Сэкономьте 35% на страховании автомобилей BMW, AUDI, MERCEDES и другие
+          </div>
+          <div class="block-img">
+            <img src="/img/main/zont.svg">
+          </div>
+          <div class="block-button">
+            <a href="" class="btn-icon-right icon-arrow-right">Подробнее</a>
+          </div>
+        </div>
+      </div>
+      <div class="block-slider">
+        <div class="block-container block-container-slider">
+          <div class="block-title">
+            Домовой-Экспресс
+          </div>
+          <div class="block-description">
+            <span>9 800 ₽/год</span>
+            Страхование квартиры от всех рисков
+          </div>
+          <div class="block-img">
+            <img src="/img/main/house.svg">
+          </div>
+          <div class="block-button">
+            <a href="" class="btn-icon-right icon-arrow-right">Подробнее</a>
+          </div>
+        </div>
+      </div>
+      <div class="block-slider">
+        <div class="block-container block-container-slider">
           <div class="block-title">
             ваплывждп ывжадлп
           </div>
@@ -35,54 +80,46 @@
           </div>
         </div>
       </div>
-      <div class="col-3 d-flex">
-        <div class="block-container mt-1_25">
+      <div class="block-slider">
+        <div class="block-container block-container-slider">
           <div class="block-title">
-            ваплывждп ывжадлп
+            Доктор РЕСО
           </div>
           <div class="block-description">
-            dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas f;k asf;k as;fkas ;dfkj as df;lk
+            <span>от 11 700 ₽/год</span>
+            Лечение у лучших врачей сети MedSwiss
           </div>
           <div class="block-img">
-            <img src="">
+            <img src="/img/main/heart.svg">
           </div>
           <div class="block-button">
-            <a href="">1</a>
-            <a href="">1</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-3 d-flex">
-        <div class="block-container mt-1_25">
-          <div class="block-title">
-            ваплывждп ывжадлп
-          </div>
-          <div class="block-description">
-            dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas f;k asf;k as;fkas ;dfkj as df;lk
-          </div>
-          <div class="block-img">
-            <img src="">
-          </div>
-          <div class="block-button">
-            <a href="">1</a>
-            <a href="">1</a>
+            <a href="" class="btn-icon-right icon-arrow-right">Подробнее</a>
           </div>
         </div>
       </div>
     </agile>
   </div>
+    </div>
+ </div>
+ </div>
 </template>
 
 <script>
-  export default {
-    layout: 'DemoMainPageLayout',
-    name: 'DemoPage',
-    head: {
-      title: 'Демонстрационная страница'
+    export default {
+        layout: 'DemoMainPageLayout',
+        name: 'DemoPage',
+        head: {
+            title: 'Демонстрационная страница'
+        }
     }
-  }
 </script>
 
 <style scoped>
-
+    .block-slider {
+        padding: 0 15px 40px;
+    }
+    
+    .slider-bg {
+        background-color: #F5F8F6;
+    }
 </style>
--- layouts/DemoMainPageLayout.vue
+++ layouts/DemoMainPageLayout.vue
@@ -162,6 +162,83 @@
             </div>
           </div>
 
+<<<<<<< HEAD
+                <div class="row d-none">
+                    <div class="col-3 d-flex">
+                        <div class="block-container mt-1_25">
+                            <div class="block-title">
+                                ваплывждп ывжадлп
+                            </div>
+                            <div class="block-description">
+                                dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas
+                                f;k asf;k as;fkas ;dfkj as df;lk
+                            </div>
+                            <div class="block-img">
+                                <img src="">
+                            </div>
+                            <div class="block-button">
+                                <a href="" class="btn-icon-right icon-arrow-right">Каско</a>
+                                <a href="" class="btn-icon-right icon-arrow-right">ОСАГО</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3 d-flex">
+                        <div class="block-container mt-1_25">
+                            <div class="block-title">
+                                ваплывждп ывжадлп
+                            </div>
+                            <div class="block-description">
+                                dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas
+                                f;k asf;k as;fkas ;dfkj as df;lk
+                            </div>
+                            <div class="block-img">
+                                <img src="">
+                            </div>
+                            <div class="block-button">
+                                <a href="">1</a>
+                                <a href="">1</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3 d-flex">
+                        <div class="block-container mt-1_25">
+                            <div class="block-title">
+                                ваплывждп ывжадлп
+                            </div>
+                            <div class="block-description">
+                                dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas
+                                f;k asf;k as;fkas ;dfkj as df;lk
+                            </div>
+                            <div class="block-img">
+                                <img src="">
+                            </div>
+                            <div class="block-button">
+                                <a href="">1</a>
+                                <a href="">1</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3 d-flex">
+                        <div class="block-container mt-1_25">
+                            <div class="block-title">
+                                ваплывждп ывжадлп
+                            </div>
+                            <div class="block-description">
+                                dfklsad;lfk ;asdfk ;asdk f;askdf;askdf;lkas fkasdf;kas df;lk as;dfk ;askd f;ask df;kas
+                                f;k asf;k as;fkas ;dfkj as df;lk
+                            </div>
+                            <div class="block-img">
+                                <img src="">
+                            </div>
+                            <div class="block-button">
+                                <a href="">1</a>
+                                <a href="">1</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+=======
+>>>>>>> master
 
         </div>
 
