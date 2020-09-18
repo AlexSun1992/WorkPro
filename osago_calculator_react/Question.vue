64b9f878c9fb2c75cb0d29b0e28a795a82019b08
Канев Евгений <kanen@reso.ru>
2020-09-18 14:38:30 +0300

    [+] Поддержка STRING и NUMBER типов вопросов

--- osago_calculator_react/Question.vue
+++ osago_calculator_react/Question.vue
@@ -27,6 +27,24 @@
           class="mt-3"
         ></b-form-select>
       </div>
+      <form
+        v-else-if="question.SSHOW_TYPE === 'STRING'"
+        @submit.prevent="onSubmitValue"
+      >
+        <input autoFocus type="text" v-model="inputValue" />
+        <a class="next-button" @click="onSubmitValue">
+          Далее
+        </a>
+      </form>
+      <form
+        v-else-if="question.SSHOW_TYPE === 'NUMBER'"
+        @submit.prevent="onSubmitValue"
+      >
+        <input autoFocus type="number" v-model="inputValue" />
+        <a class="next-button" @click="onSubmitValue">
+          Далее
+        </a>
+      </form>
       <div v-else-if="question.SSHOW_TYPE === 'SYSTEM_END'" />
       <div v-else>
         Отображения вопроса SSHOW_TYPE = "{{ question.SSHOW_TYPE }}" не
@@ -38,6 +56,11 @@
 <script>
 export default {
   props: ["question", "answers"],
+  data() {
+    return {
+      inputValue: ""
+    };
+  },
   computed: {
     selectOptions() {
       return this.answers.map(({ ID: value, STITLE: text }) => ({
@@ -50,12 +73,21 @@ export default {
     onSelectChange(id) {
       const answer = this.answers.find(item => item.ID === id);
       this.$emit("choose-answer", answer);
+    },
+    onSubmitValue() {
+      const answer = this.answers[0];
+      answer.STITLE = this.inputValue;
+      answer.SVALUE_VALUE = this.inputValue;
+      this.$emit("choose-answer", answer);
     }
   },
   data() {
     return {
       selected: null
     };
+  },
+  created: function() {
+    this.inputValue = "";
   }
 };
 </script>
