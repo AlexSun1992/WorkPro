26337d7c41b058b093db324b27ebe11024c001e3
Дмитрий С. Александрин <ALDS@main.reso.ru>
2020-09-21 16:39:05 +0300

    рефакторинг

--- store/data_card.js
+++ store/data_card.js
@@ -6,17 +6,22 @@ export const getters = {
   getForm: state => state.form,
   getCaptions: state => state.captions,
   getDataFieldByName: state => name => {
+    debugger
     return state.form.find(b => b.name === name);
   },
   getDataFieldByFieldId: state => id => {
+    debugger
     return state.form.find(b => b.fieldId === id);
   },
 }
 export const actions = {
   async fetchForm ({commit, getters}, params) {
+    commit('clearFormData')
     await  this.$axios.get(`/api/card/${params.idModule}/${params.idItem}/${params.idCard}`)
       .then((res) => {
+        debugger
         commit('setForm', res.data.metaData.data);
+        debugger
         if (res.data.metaData.captions) {
           commit('setCaptions', res.data.metaData.captions);
         }
