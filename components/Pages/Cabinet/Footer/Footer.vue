a0db9e8f47f6f2cf77a4dc1756179398ee2527e7
IONKA <ionka@reso.ru>
2020-09-22 17:31:25 +0300

    Исправление footer

--- components/Pages/Cabinet/Footer/Footer.vue
+++ components/Pages/Cabinet/Footer/Footer.vue
@@ -1,64 +1,26 @@
 <template>
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-5  menu-link">
+  <footer class="footer">
+    <div class="row">
+      <div class="footer-link w-100 d-block d-md-flex position-relative">
+        <div class="col-md-4 col-12"><a href="">Политика обработки персональных данных</a></div>
+        <div class="col-md-4 col-12 mt-3 mt-md-0"><a href="" class="position-relative footer-office-link">Найти офисы</a></div>
+        <div class="col-md-4 col-12 my-3 my-md-0">
           <div class="row">
-            <div class="col-5">
-              <div class="title">Авто</div>
-              <a href="">ОСАГО</a>
-              <a href="">КАСКО</a>
-              <a href="">ЗЕЛЕНАЯ КАРТА</a>
-              <div class="block-h-line w-25 my-4"></div>
-              <div class="title">Имущество</div>
-              <a href="">КВАРТИРА</a>
-              <a href="">ДОМ</a>
-              <a href="">ИПОТЕКА</a>
-            </div>
-            <div class="col-7">
-              <div class="title">Жизнь и здоровье</div>
-              <a href="">ДМС</a>
-              <a href="">ОМС</a>
-              <a href="">НЕСЧАСТНЫЙ СЛУЧАЙ</a>
-              <a href="">ТУРИЗМ</a>
-              <a href="">Накопительное инвестиционное страхование жизни</a>
+            <div class="col-12 col-xl-auto"><a href="">Свяжитесь с нами</a></div>
+            <div class="col-auto text-right">
+              <a href="" class="icon-footer-vk share-link"></a>
+              <a href="" class="icon-footer-inst share-link"></a>
+              <a href="" class="icon-footer-fb share-link"></a>
+              <a href="" class="icon-footer-twit share-link"></a>
             </div>
           </div>
-        </div>
-        <div class="col-3 block-v-line menu-link">
-          <div class="title">РЕСО-Гарантия</div>
-          <a href="">О компании</a>
-          <a href="">Новости</a>
-          <a href="">Раскрытие сведений</a>
-          <a href="">Карьера в РЕСО</a>
-          <a href="">Закупки</a>
-          <a href="">Правила и тарифы</a>
-          <a href="">Информация для получателей страховых услуг</a>
-          <a href="">Карта сайта</a>
-        </div>
-        <div class="col-4 pl-4">
-          <div class="title mb-3">Наши приложения</div>
-          <a href="" class="footer-apple"></a>
-          <a href="" class="footer-google"></a>
-          <div class="title mt-5 mb-3">Мы в социальных сетях</div>
-          <div class="social">
-            <a href="" class="footer-fb"></a>
-            <a href="" class="footer-vk"></a>
-            <a href="" class="footer-ok"></a>
-            <a href="" class="footer-in"></a>
-            <a href="" class="footer-tw"></a>
-          </div>
-        </div>
-        <div class="col-12">
-          <div class="block-h-line my-4"></div>
-        </div>
-        <div class="col-12">
-          <div class="info mb-2">СПАО «РЕСО-Гарантия» раскрывает информацию на странице в сети Интернет информационного агентства, аккредитованного ЦБ РФ на раскрытие информации, по адресу: <a href="http://www.disclosure.ru/issuer/7710045520/">http://www.disclosure.ru/issuer/7710045520/</a></div>
-          <div class="info_ppl mb-2">Информация об акционерах и лицах, под контролем или значительным влиянием которых находится страховщик на 17.12.2018 год</div>
-          <div class="copyright">© 2020, «РЕСО-Гарантия»&nbsp;&nbsp;&nbsp;&nbsp;<a href="">Конфиденциальность</a></div>
         </div>
       </div>
     </div>
+    <a href="/" class="logo-footer"></a>
+    СПАО «РЕСО-Гарантия» раскрывает информацию на странице в сети Интернет информационного агентства, аккредитованного ЦБ РФ на раскрытие информации, по адресу: <a href="http://www.disclosure.ru/issuer/7710045520/">http://www.disclosure.ru/issuer/7710045520/</a>
+    <br>Информация об акционерах и лицах, под контролем или значительным влиянием которых находится страховщик на 19.03.2020г.
+    <br>Информация о компании по базовым стандартам Центробанка
   </footer>
 </template>
 
--- layouts/DemoMainPageLayout.vue
+++ layouts/DemoMainPageLayout.vue
@@ -247,72 +247,14 @@
         </div>
     </div>
 </div>
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-5  menu-link">
-                <div class="row">
-                    <div class="col-5">
-                        <div class="title">Авто</div>
-                        <a href="">ОСАГО</a>
-                        <a href="">КАСКО</a>
-                        <a href="">ЗЕЛЕНАЯ КАРТА</a>
-                        <div class="block-h-line w-25 my-4"></div>
-                        <div class="title">Имущество</div>
-                        <a href="">КВАРТИРА</a>
-                        <a href="">ДОМ</a>
-                        <a href="">ИПОТЕКА</a>
-                    </div>
-                    <div class="col-7">
-                        <div class="title">Жизнь и здоровье</div>
-                        <a href="">ДМС</a>
-                        <a href="">ОМС</a>
-                        <a href="">НЕСЧАСТНЫЙ СЛУЧАЙ</a>
-                        <a href="">ТУРИЗМ</a>
-                        <a href="">Накопительное инвестиционное страхование жизни</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3 block-v-line menu-link">
-                <div class="title">РЕСО-Гарантия</div>
-                <a href="">О компании</a>
-                <a href="">Новости</a>
-                <a href="">Раскрытие сведений</a>
-                <a href="">Карьера в РЕСО</a>
-                <a href="">Закупки</a>
-                <a href="">Правила и тарифы</a>
-                <a href="">Информация для получателей страховых услуг</a>
-                <a href="">Карта сайта</a>
-            </div>
-            <div class="col-4 pl-4">
-                <div class="title mb-3">Наши приложения</div>
-                <a href="" class="footer-apple"></a>
-                <a href="" class="footer-google"></a>
-                <div class="title mt-5 mb-3">Мы в социальных сетях</div>
-                <div class="social">
-                    <a href="" class="footer-fb"></a>
-                    <a href="" class="footer-vk"></a>
-                    <a href="" class="footer-ok"></a>
-                    <a href="" class="footer-in"></a>
-                    <a href="" class="footer-tw"></a>
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="block-h-line my-4"></div>
-            </div>
-            <div class="col-12">
-                <div class="info mb-2">СПАО «РЕСО-Гарантия» раскрывает информацию на странице в сети Интернет информационного агентства, аккредитованного ЦБ РФ на раскрытие информации, по адресу: <a href="http://www.disclosure.ru/issuer/7710045520/">http://www.disclosure.ru/issuer/7710045520/</a></div>
-                <div class="info_ppl mb-2">Информация об акционерах и лицах, под контролем или значительным влиянием которых находится страховщик на 17.12.2018 год</div>
-                <div class="copyright">© 2020, «РЕСО-Гарантия»&nbsp;&nbsp;&nbsp;&nbsp;<a href="">Конфиденциальность</a></div>
-            </div>
-        </div>
-    </div>
-</footer>
+    <Footer/>
+
 </div>
 </template>
 
 <script>
     import MainHeader from '~/components/Pages/Main/Header/MainHeader'
+    import Footer from '~/components/Footer';
 
     export default {
         name: 'DemoMainPageLayout',
@@ -324,7 +266,7 @@
             }
         },
         components: {
-            MainHeader
+            MainHeader, Footer
         }
     }
 </script>
@@ -333,7 +275,7 @@
     .main_page {
         @import '~/assets/scss/main.scss'
     }
-    
+
     .main_page {
         background-color: #fff;
     }
