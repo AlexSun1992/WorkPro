e8694be4e917f479d86b552fe1252d67a58dd0f3
IONKA <ionka@reso.ru>
2020-11-12 14:04:42 +0300

    Очистка данных карточки при переходе в другой пункт меню.

--- middleware/guest.js
+++ middleware/guest.js
@@ -1,5 +1,6 @@
 export default function ({ app, store, redirect, route }) {
   store.dispatch("blocks/clearBlock");
+  store.commit("data_card/clearFormData");
   store.commit("data_card/setError", false);
   app.$cookiz.set("url", route.path);
   if (!store.state.auth.loggedIn) {
