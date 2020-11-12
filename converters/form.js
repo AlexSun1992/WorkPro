00d1946fd230c34be6738c71fa425d34787d891e
Дмитрий С. Александрин <ALDS@main.reso.ru>
2020-11-12 12:30:53 +0300

    правка в форму

--- converters/form.js
+++ converters/form.js
@@ -79,9 +79,21 @@ converter.form = async (data, itemId) => {
     obj.value = item[webFields[i].SNAME]
       ? item[webFields[i].SNAME]
       : meta[webFields[i].SNAME];
-    if (obj.value == "Д") {
+    if (
+      obj.value === "Д" ||
+      obj.value === "д" ||
+      obj.value === "Y" ||
+      obj.value === "y" ||
+      obj.value === "1"
+    ) {
       obj.value = true;
-    } else if (obj.value == "Н") {
+    } else if (
+      obj.value === "Н" ||
+      obj.value === "н" ||
+      obj.value === "N" ||
+      obj.value === "n" ||
+      obj.value === "0"
+    ) {
       obj.value = false;
     }
     obj.type = webFields[i].STYPE;
