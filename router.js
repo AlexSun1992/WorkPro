e9a12f0c2f2ae41061b1a3ce9f89434bf339cfdb
IONKA <ionka@reso.ru>
2020-11-16 17:56:58 +0300

    Удалены неиспользованные страницы.

--- router.js
+++ router.js
@@ -1,24 +1,14 @@
 import Vue from "vue";
 import Router from "vue-router";
-
 import Main from "~/components/Pages/Main";
-import DemoAbout from "~/components/Pages/DemoAbout";
-import Feedback from "~/components/Pages/Feedback";
-import Calculator from "~/components/Pages/Calculator";
 import DemoPage from "~/components/Pages/DemoPage";
-import DemoCarousel from "~/components/Pages/DemoCarousel";
-
-import DemoPortlet from "~/components/Pages/DemoPortlet";
-
 import Cabinet from "~/components/Pages/Cabinet/Full";
 import Dashboard from "~/components/Pages/Cabinet/Dashboard";
 import Fluid from "~/components/Pages/Cabinet/Fluid";
 import CardPage from "~/components/Pages/Cabinet/CardPage";
-
 import Login from "~/components/Pages/Login/LoginPage";
 import PasswordRecovery from "~/components/Pages/Login/PasswordRecovery/PasswordRecoveryForm";
 import DynamicRoutesRenderer from "~/components/Libs/DynamicRoutesRenderer/DynamicRoutesRenderer";
-
 import Preview from "~/components/Pages/PreviewPage/Preview";
 
 Vue.use(Router);
@@ -45,31 +35,6 @@ export function createRouter() {
         meta: "Recovery",
         path: "/recovery",
         component: PasswordRecovery,
-      },
-      {
-        meta: "About",
-        path: "/about",
-        component: DemoAbout,
-      },
-      {
-        meta: "Обратная связь",
-        path: "/feedback",
-        component: Feedback,
-      },
-      {
-        meta: "Калькулятор",
-        path: "/calculator",
-        component: Calculator,
-      },
-      {
-        meta: "Карусель",
-        path: "/demo-carousel",
-        component: DemoCarousel,
-      },
-      {
-        meta: "Демо контент",
-        path: "/demo-portlet",
-        component: DemoPage,
       },
       {
         meta: "Демонстрационная страница",
