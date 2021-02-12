77043d6cf87317225ab4047cf6ed53f81f03222e
IONKA <ionka@reso.ru>
2021-02-12 12:51:34 +0300

    Исправлена подстановка значений по умолчанию.

--- converters/form.js
+++ converters/form.js
@@ -76,11 +76,16 @@ converter.form = async (data, itemId) => {
   for (let i = 0; i < webFields.length; i++) {
     let obj = {};
     obj.label = webFields[i].SCAPTION;
-    if (item[webFields[i].SNAME] || item[webFields[i].SNAME] === 0) {
+    if (
+      item[webFields[i].SNAME] ||
+      item[webFields[i].SNAME] === 0 ||
+      item[webFields[i].SNAME] === false
+    ) {
       obj.value = item[webFields[i].SNAME];
-    }
-    if (meta[webFields[i].SNAME] || meta[webFields[i].SNAME] === 0) {
-      obj.value = meta[webFields[i].SNAME];
+    } else {
+      if (meta[webFields[i].SNAME] || meta[webFields[i].SNAME] === 0) {
+        obj.value = meta[webFields[i].SNAME];
+      }
     }
     if (
       obj.value === "Д" ||
