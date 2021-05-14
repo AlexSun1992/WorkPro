33aa10aad6863ee425ff421a158d694bc304641d
IONKA <ionka@reso.ru>
2021-05-14 13:21:39 +0300

    Поправлена форма.

--- components/Libs/VerifyUser/VerifyUser.vue
+++ components/Libs/VerifyUser/VerifyUser.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="row">
     <p>{{ label }}</p>
-    <b-form-group
-      class="col-12 col-md-6"
-      :label="loginType === 'phone' ? 'Телефон' : 'Email'"
-    >
+    <b-form-group class="col-12 col-md-6">
       <b-form-input
         v-if="loginType === 'phone'"
         ref="userInput"
