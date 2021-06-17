8c87cc3d1aab3fb921b105624c4ce67ebf9b6746
IONKA <ionka@reso.ru>
2021-06-17 15:49:14 +0300

    [LK2-22] Исправлено отображение инфо блока справа.

--- components/Pages/Cabinet/CardPage.vue
+++ components/Pages/Cabinet/CardPage.vue
@@ -50,6 +50,10 @@
         />
       </div>
       <v-runtime-template
+        v-if="settings.cardtemplate && isShowCardEditor"
+        :template="settings.cardtemplate"
+      ></v-runtime-template>
+      <v-runtime-template
         v-else-if="isShowTemplate"
         :template="settings.cardtemplate"
       ></v-runtime-template>
