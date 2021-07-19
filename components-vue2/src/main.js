792683aa26f5cc2b29f58bd409b73fee601449d2
Гаврилов Алексей <gavan@reso.ru>
2021-07-19 14:25:48 +0300

    добавление спиннера для кнопки LK

--- components-vue2/src/main.js
+++ components-vue2/src/main.js
@@ -4,7 +4,14 @@ import ComponentLoginButton from "./components/Login/LoginButton.vue";
 import ComponentLoginForm from "./components/Login/LoginForm.vue";
 import ComponentQuizCalculator from "./components/QuizCalculator/QuizCalculator.vue";
 import ComponentPasswordRecoveryForm from "./components/Login/RecoveryForm/PasswordRecoveryForm.vue";
+import { BootstrapVue } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import { BSpinner } from 'bootstrap-vue'
 Vue.use(vueCustomElement);
+Vue.component('b-spinner', BSpinner);
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false;
 new Vue({
   render: (h) => h(App),
