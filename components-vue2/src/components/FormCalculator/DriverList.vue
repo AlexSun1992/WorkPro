e5238254dce9ea3d3a599016556ee122f5c63508
Александр Батраков <bataan@reso.ru>
2021-07-26 16:03:44 +0300

    пофиксил баг

--- components-vue2/src/components/FormCalculator/DriverList.vue
+++ components-vue2/src/components/FormCalculator/DriverList.vue
@@ -1,36 +1,53 @@
 <template>
   <div>
-    <b-form-row>
-      <b-form-group class="col-md-3" label="Возраст">
-        <b-form-input type="number"></b-form-input>
+    <b-form-row v-if="drivers.length !== 0">
+      <b-form-group class="col-md-3" label="Возраст: ">
+        <span>{{ driver.age }}</span>
       </b-form-group>
 
-      <b-form-group class="col-md-3" label="Стаж">
-        <b-form-input type="number"></b-form-input>
+      <b-form-group class="col-md-3" label="Стаж: ">
+        <span>{{ driver.standing }}</span>
       </b-form-group>
 
       <b-form-group class="col-md-3" label="КБМ, сколько лет без аварий?">
-        <b-form-select class="mb-3">
-          <b-form-select-option value="a"
-            >Страхуюсь впервые</b-form-select-option
-          >
-          <b-form-select-option value="a">1 год</b-form-select-option>
-          <b-form-select-option value="a">2 года</b-form-select-option>
-          <b-form-select-option value="a">3 года</b-form-select-option>
-          <b-form-select-option value="a">4 года</b-form-select-option>
-          <b-form-select-option value="a">5 лет</b-form-select-option>
-          <b-form-select-option value="a">6 лет</b-form-select-option>
-          <b-form-select-option value="a">7 лет</b-form-select-option>
-          <b-form-select-option value="a">8 лет</b-form-select-option>
-          <b-form-select-option value="a">9 лет</b-form-select-option>
-          <b-form-select-option value="a">10 лет и более</b-form-select-option>
-          <b-form-select-option value="a"
-            >Был в ДТП менее года назад</b-form-select-option
+        <span>{{ driver.experience.text }}</span>
+      </b-form-group>
+    </b-form-row>
+
+    <b-form-row @submit.prevent>
+      <b-form-group class="col-md-3" label="Возраст">
+        <b-form-input
+          v-model="driver.age"
+          @input="$emit($event.target.value)"
+          type="number"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group class="col-md-3" label="Стаж">
+        <b-form-input
+          v-model="driver.standing"
+          @input="$emit($event.target.value)"
+          type="number"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group class="col-md-3" label="КБМ, сколько лет без аварий?">
+        <b-form-select
+          v-model="driver.experience"
+          @input="$emit($event.target.value)"
+          class="mb-3"
+        >
+          <b-form-select-option
+            v-for="item in driver.experience"
+            :key="item.id"
+            >{{ item.text }}</b-form-select-option
           >
         </b-form-select>
       </b-form-group>
     </b-form-row>
-    <a href="#"><span>+ Добавить еще одного водителя</span></a>
+    <a href="#"
+      ><span @click="addDriver">+ Добавить еще одного водителя</span></a
+    >
   </div>
 </template>
 
@@ -53,10 +70,41 @@ export default {
     BFormGroup,
   },
   data() {
-    return {};
+    return {
+      driver: {
+        age: "",
+        standing: "",
+        experience: [
+          { id: 1, text: "" },
+          { id: 2, text: "Страхуюсь впервые" },
+          { id: 3, text: "1 год" },
+          { id: 4, text: "2 года" },
+          { id: 5, text: "3 года" },
+          { id: 6, text: "4 года" },
+          { id: 7, text: "5 лет" },
+          { id: 8, text: "6 лет" },
+          { id: 9, text: "7 лет" },
+          { id: 10, text: "8 лет" },
+          { id: 11, text: "9 лет" },
+          { id: 12, text: "10 лет" },
+          { id: 13, text: "Был в ДТП менее года назад" },
+        ],
+      },
+      drivers: [],
+    };
   },
 
-  methods: {},
+  methods: {
+    addDriver() {
+      const newDriver = {
+        age: this.age,
+        standing: this.standing,
+        experience: this.driver.experience.text,
+      };
+      console.log(newDriver);
+      this.drivers.push(newDriver);
+    },
+  },
 
   computed: {},
   created() {},
