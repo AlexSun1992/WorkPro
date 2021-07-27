e9755a59e68c80678b1960ab6666614f7cdb145c
Дмитрий С. Александрин <ALDS@main.reso.ru>
2021-07-27 11:37:20 +0300

    убрал лишнее

--- components-vue2/src/store/index.js
+++ components-vue2/src/store/index.js
@@ -1,12 +1,12 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import card from "../../../store/card";
-import data_card from "../../../store/data_card";
-import index from "../../../store/index";
-import menu from "../../../store/menu";
+import * as card from "../../../store/card";
+import * as data_card from "../../../store/data_card";
+import * as index from "../../../store/index";
+import * as menu from "../../../store/menu";
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+export const store = new Vuex.Store({
   modules: {
     card,
     data_card,
