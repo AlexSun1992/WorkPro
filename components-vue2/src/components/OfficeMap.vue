6103329d9fef4d3b83751ae6a8a8613db6a614aa
Дмитрий С. Александрин <ALDS@main.reso.ru>
2021-08-09 09:43:08 +0300

    доработки в карту офисов

---
+++ components-vue2/src/components/OfficeMap.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="map-container mt-3">
+    <input type="text" id="suggest" />
+    <div class="mt-2 mb-2">
+      <a href="https://reso.ru/About/Contacts/OfficesList"
+        >Список офисов продаж и центров выплат</a
+      >
+    </div>
+    <div ref="map" id="map" class="map"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OfficeMap",
+  data() {
+    return {
+      myMap: null,
+      myClusterer: null,
+    };
+  },
+  async created() {
+    try {
+      await this.$store.dispatch("general/fetchAgencies");
+    } catch (error) {
+      console.log(error);
+    }
+    if (this.$store.getters["general/getAgencies"]) {
+      ymaps.ready(this.init);
+    }
+  },
+  methods: {
+    init() {
+      let suggestView = new ymaps.SuggestView("suggest");
+      let showOnMap = this.showOnMap.bind(this);
+      suggestView.events.add("select", function (e) {
+        showOnMap(e.get("item").value);
+      });
+
+      this.myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 6,
+      });
+
+      let agencies = this.$store.getters["general/getAgencies"];
+      let myGeoObjects = [];
+      for (let i = 0; i < agencies.length; i++) {
+        myGeoObjects[i] = new ymaps.GeoObject({
+          geometry: {
+            type: "Point",
+            coordinates: [agencies[i].NLAT, agencies[i].NLONG],
+          },
+          properties: {
+            balloonContentBody: `
+          <strong><span>${agencies[i].SSHORTNAME}</span></strong><br><br>
+          <span>${agencies[i].SADDRESS}</span><br>
+          <strong>Тел.:</strong><span>${agencies[i].SPHONE}</span><br>
+          <strong>Email.:</strong><span>${agencies[i].SPHONE}</span><br>
+          <strong>Режим работы:</strong><br><span>${agencies[i].SGRAF}</span>
+        `,
+            hintContent: `${agencies[i].SSHORTNAME}`,
+          },
+        });
+      }
+      this.myClusterer = new ymaps.Clusterer();
+      this.myClusterer.add(myGeoObjects);
+      this.myMap.geoObjects.add(this.myClusterer);
+    },
+    updateMap(state, caption) {
+      let placemark = new ymaps.Placemark(
+        this.myMap.getCenter(),
+        {
+          iconCaption: caption,
+          balloonContent: caption,
+        },
+        {
+          preset: "islands#redDotIconWithCaption",
+        }
+      );
+      this.myMap.geoObjects.add(placemark);
+      this.myMap.setCenter(state.center, state.zoom);
+      placemark.geometry.setCoordinates(state.center);
+      placemark.properties.set({
+        iconCaption: caption,
+        balloonContent: caption,
+      });
+      this.myMap.setCenter(state.center, state.zoom);
+    },
+    showResult(obj) {
+      let mapContainer = document.getElementById("map");
+      let bounds = obj.properties.get("boundedBy");
+      // Рассчитываем видимую область для текущего положения пользователя.
+      let mapState = ymaps.util.bounds.getCenterAndZoom(bounds, [
+        mapContainer.clientWidth,
+        mapContainer.clientHeight,
+      ]);
+      // Сохраняем полный адрес для сообщения под картой.
+      let address = [obj.getCountry(), obj.getAddressLine()].join(", ");
+      // Сохраняем укороченный адрес для подписи метки.
+      let shortAddress = [
+        obj.getThoroughfare(),
+        obj.getPremiseNumber(),
+        obj.getPremise(),
+      ].join(" ");
+      this.updateMap(mapState, shortAddress);
+    },
+    showOnMap(suggest) {
+      let showResult = this.showResult.bind(this);
+      ymaps.geocode(suggest).then(function (res, context) {
+        let obj = res.geoObjects.get(0);
+        if (obj) {
+          showResult(obj);
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.map {
+  width: 800px;
+  height: 600px;
+}
+select,
+.form-control,
+input {
+  min-width: 500px !important;
+}
+/* .map-container {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: absolute;
+  height: 100%;
+} */
+</style>
