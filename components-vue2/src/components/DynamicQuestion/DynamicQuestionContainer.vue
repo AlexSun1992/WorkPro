58f7ba334a9f973204b18abc8f2ffef192824720
Гаврилов Алексей <gavan@reso.ru>
2021-09-03 13:42:40 +0300

    перенос строки на другую строчку

--- components-vue2/src/components/DynamicQuestion/DynamicQuestionContainer.vue
+++ components-vue2/src/components/DynamicQuestion/DynamicQuestionContainer.vue
@@ -33,9 +33,29 @@ export default {
     const url = "/free/v2/question";
     let response = await fetch(url);
     let data = await response.json();
-
+    const urlAddress = /\bhttps?:\/\/\S+/;
     this.distinctData = data.filter((item) => {
       return item.IDRMPRODUCT === this.productId;
+    });
+
+    this.distinctData.forEach((item) => {
+      if (item.SANSWER.includes("\n")) {
+        item.SANSWER = item.SANSWER.replace(/\n/g, "<br />");
+      }
+      if (item.SANSWER.match(urlAddress)) {
+        // item.SANSWER = item.SANSWER.replace(urlAddress, `<a>${urlAddress}</a>`);
+        // console.log(item.SANSWER.match(urlAddress)[0]);
+        // item.SANSWER = item.SANSWER.replace(
+        //   "a",
+        //   `<a href="#">${item.SANSWER.match(urlAddress)[0]}</a>`
+        // );
+        // item.SANSWER = item.SANSWER.replace(
+        //   item.SANSWER.match(urlAddress)[0],
+        //   `<a href="#">${item.SANSWER.match(urlAddress)[0]}</a>`
+        // );
+
+        console.log(item.SANSWER.match(urlAddress));
+      }
     });
   },
 };
