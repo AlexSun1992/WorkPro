dfdeb7c4e5ec1bfe41e2dd6444824f7bd5dd794b
Гаврилов Алексей <gavan@reso.ru>
2021-09-03 15:08:57 +0300

    активные ссылки

--- components-vue2/src/components/DynamicQuestion/DynamicQuestionContainer.vue
+++ components-vue2/src/components/DynamicQuestion/DynamicQuestionContainer.vue
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import { isArray } from "util";
 import DynamicQuestion from "./DynamicQuestion.vue";
 export default {
   props: ["productId", "choosenData"],
@@ -33,28 +34,26 @@ export default {
     const url = "/free/v2/question";
     let response = await fetch(url);
     let data = await response.json();
-    const urlAddress = /\bhttps?:\/\/\S+/;
+    const urlAddress = /\bhttps?:\/\/\S+/g;
     this.distinctData = data.filter((item) => {
       return item.IDRMPRODUCT === this.productId;
     });
 
     this.distinctData.forEach((item) => {
+      if (item.SANSWER.match(urlAddress)) {
+        if (item.SANSWER.match(urlAddress).length > 0) {
+          for (let i = 0; i < item.SANSWER.match(urlAddress).length; i++) {
+            item.SANSWER = item.SANSWER.replace(
+              item.SANSWER.match(urlAddress)[i],
+              `<a href="${item.SANSWER.match(urlAddress)[i]}">${
+                item.SANSWER.match(urlAddress)[i]
+              }</a>`
+            );
+          }
+        }
+      }
       if (item.SANSWER.includes("\n")) {
         item.SANSWER = item.SANSWER.replace(/\n/g, "<br />");
-      }
-      if (item.SANSWER.match(urlAddress)) {
-        // item.SANSWER = item.SANSWER.replace(urlAddress, `<a>${urlAddress}</a>`);
-        // console.log(item.SANSWER.match(urlAddress)[0]);
-        // item.SANSWER = item.SANSWER.replace(
-        //   "a",
-        //   `<a href="#">${item.SANSWER.match(urlAddress)[0]}</a>`
-        // );
-        // item.SANSWER = item.SANSWER.replace(
-        //   item.SANSWER.match(urlAddress)[0],
-        //   `<a href="#">${item.SANSWER.match(urlAddress)[0]}</a>`
-        // );
-
-        console.log(item.SANSWER.match(urlAddress));
       }
     });
   },
