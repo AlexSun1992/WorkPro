85bd8de6df56661f72350c58b055815ef46a4c2e
Гаврилов Алексей <gavan@reso.ru>
2021-09-06 09:49:49 +0300

    регулярное выражение для ссылок

--- components-vue2/src/components/DynamicQuestion/DynamicQuestionContainer.vue
+++ components-vue2/src/components/DynamicQuestion/DynamicQuestionContainer.vue
@@ -8,7 +8,6 @@
 </template>
 
 <script>
-import { isArray } from "util";
 import DynamicQuestion from "./DynamicQuestion.vue";
 export default {
   props: ["productId", "choosenData"],
@@ -35,6 +34,9 @@ export default {
     let response = await fetch(url);
     let data = await response.json();
     const urlAddress = /\bhttps?:\/\/\S+/g;
+    const email = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    const phone = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+
     this.distinctData = data.filter((item) => {
       return item.IDRMPRODUCT === this.productId;
     });
@@ -51,6 +53,22 @@ export default {
             );
           }
         }
+      }
+      if (item.SANSWER.match(email)) {
+        console.log(item.SANSWER);
+        // if (item.SANSWER.match(email).length > 0) {
+        //   for (let i = 0; i < item.SANSWER.match(email).length; i++) {
+        //     item.SANSWER = item.SANSWER.replace(
+        //       item.SANSWER.match(email)[i],
+        //       `<a href="${item.SANSWER.match(email)[i]}">${
+        //         item.SANSWER.match(email)[i]
+        //       }</a>`
+        //     );
+        //   }
+        // }
+      }
+      if (item.SANSWER.match(phone)) {
+        console.log(item.SANSWER);
       }
       if (item.SANSWER.includes("\n")) {
         item.SANSWER = item.SANSWER.replace(/\n/g, "<br />");
