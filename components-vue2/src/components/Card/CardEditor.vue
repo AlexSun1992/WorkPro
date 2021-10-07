29d07c86a1d546af84f4d3579a8933b3fda8502e
IONKA <ionka@reso.ru>
2021-10-07 15:41:38 +0300

    Доработка валидации формы.

--- components-vue2/src/components/Card/CardEditor.vue
+++ components-vue2/src/components/Card/CardEditor.vue
@@ -6,15 +6,15 @@
       @update="updateValue($event)"
       @blur="updateBlurValue($event)"
     />
-      <div>
-        <b-alert
-          :show="getSavedError || getError"
-          variant="danger"
-          class="mt-3 mb-0"
-        >
-          {{ getErrorMessage }}
-        </b-alert>
-      </div>
+    <div>
+      <b-alert
+        :show="getSavedError || getError"
+        variant="danger"
+        class="mt-3 mb-0"
+      >
+        {{ getErrorMessage }}
+      </b-alert>
+    </div>
     <div
       v-if="getBtnSave && isShowButtonSave && !getError"
       class="row mt-4 ml-2"
@@ -147,7 +147,6 @@ export default {
       for (let i = 0; i < data.length; i++) {
         const value =
           data[i].type === "enum" ? data[i].value.value : data[i].value;
-        data[i].checked = true;
         const error = data[i].error;
         if (
           (data[i].required &&
