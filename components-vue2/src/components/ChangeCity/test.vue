945c644fd2bda82a0dd1e7201eecc4abe1164c26
Александр Батраков <bataan@reso.ru>
2021-10-20 15:56:07 +0300

    дублировал функциональность ветки

---
+++ components-vue2/src/components/ChangeCity/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    name: "";
+  },
+  mounted() {
+    if (localStorage.name) {
+      this.name = localStorage.name;
+    }
+  },
+  watch: {
+    name(newName) {
+      localStorage.name = newName;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
