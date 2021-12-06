113dad5781159768dc8785a1c52e64fc5159d914
Гаврилов Алексей <gavan@reso.ru>
2021-12-06 18:56:38 +0300

    черновой рабочий вариант

--- components/Libs/Controls/ControlListSelect.vue
+++ components/Libs/Controls/ControlListSelect.vue
@@ -1,11 +1,22 @@
 <template>
-  <v-runtime-template :itemId="itemId" :template="getData"></v-runtime-template>
+  <!-- <content-block :itemId="itemId">
+    <div :item="itemId">
+      <v-runtime-template
+        :itemId="itemId"
+        :template="getData"
+      ></v-runtime-template>
+    </div>
+  </content-block> -->
+  <!-- <div>
+    <template :itemId="itemId" v-html="getTemplate"></template>
+  </div> -->
 
-  <!-- <div v-click-outside="outside">
+  <div v-click-outside="outside" :itemId="itemId">
     <b-form-group
       :label="data.label"
       :class="{ required: data.required }"
       :label-for="data.name"
+      :itemId="itemId"
     >
       <b-input
         v-model="data.value.text || 'Выберите из списка'"
@@ -18,29 +29,78 @@
       >
         {{ data.value.text || "Выберите из списка" }}
       </b-input>
-      <b-collapse id="collapse-4" v-model="visible" class="mt-2">
-        <b-card>
-          <b-col style="width: 60rem">
-            <grid
-              :load="isLoad"
-              :action="true"
-              :total="dataContent.total"
-              :fields="dataContent.fields"
-              :items="dataContent.items"
-            >
-              <template v-slot:actions="slotProps">
-                <b-button
-                  v-on:click="selectItem(slotProps)"
-                  class="btn-table-open"
-                  >Выбрать</b-button
+      <b-collapse
+        id="collapse-4"
+        v-model="visible"
+        class="mt-2"
+        :itemId="itemId"
+      >
+        <content-block
+          class="mypolices-all-block"
+          :item-id="itemId"
+          :itemId="itemId"
+        >
+          <v-runtime-template
+            :itemId="itemId"
+            :template="getData"
+          ></v-runtime-template>
+          <!-- <template v-slot:data="data">
+            <div>
+              <div class="mypolices-block" :data-id="data.content.IDPRODUCT">
+                <div class="mypolices-title">
+                  {{ data.content.SSECONDNAME }} {{ data.content.SFIRSTNAME }}
+                  {{ data.content.STHIRDNAME }}
+                </div>
+                <div class="img-mypolices-img"></div>
+                <div class="mypolices-name">
+                  {{
+                    data.content.SPOLOBJ && data.content.SPOLOBJ.match(/г\/н/)
+                      ? data.content.SPOLOBJ.trim()
+                          .split(/; +г\/н: +/)
+                          .shift()
+                          .trim()
+                      : data.content.SPOLOBJ
+                  }}
+                </div>
+
+                <div class="mypolices-info">
+                  <div class="mypolices-number">
+                    {{ data.content.SPOLICY }}
+                  </div>
+                  <div class="mypolices-time">
+                    Срок действия:
+                    <b>
+                      {{
+                        $moment(data.content.TO_DATE).locale("ru").fromNow(true)
+                      }}</b
+                    >
+                    ({{ data.content.TO_DATE | moment("DD.MM.YYYY") }})
+                  </div>
+                </div>
+                <div
+                  class="
+                    block-footer
+                    row
+                    px-0
+                    justify-content-between
+                    align-items-center
+                  "
                 >
-              </template>
-            </grid>
-          </b-col>
-        </b-card>
+                  <NLink
+                    :to="`/cabinet/55/0/901?SPOLICY=${data.content.SPOLICY}`"
+                    ><div>Перейти к полису</div></NLink
+                  >
+                  <NLink to="/cabinet/55/0/905?SPOLICY=306526-13/21"
+                    ><div>Список ЛПУ</div></NLink
+                  >
+                </div>
+              </div>
+            </div>
+          </template> -->
+        </content-block>
       </b-collapse>
     </b-form-group>
-  </div> -->
+  </div>
 </template>
 
 <script>
