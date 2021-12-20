9a2f37c5512824c211d98bb4415345f1c707d86d
Дмитрий С. Александрин <ALDS@main.reso.ru>
2021-12-20 16:19:06 +0300

    Реализован переход в список при наличии флага LCLOSEAFTERSAVE

--- components/Libs/CardEditor/CardEditor.vue
+++ components/Libs/CardEditor/CardEditor.vue
@@ -459,11 +459,18 @@ export default {
               );
               return;
             } else {
-              this.$router.push(
-                `/cabinet/${moduleId}/0/${itemId}/${cardId}${
-                  relId ? `/${relId}` : ""
-                }`
-              );
+              if (this.closeAfterSave) {
+                this.$router.push(
+                  `/cabinet/${moduleId}/0/${itemId}
+                `
+                );
+              } else {
+                this.$router.push(
+                  `/cabinet/${moduleId}/0/${itemId}/${cardId}${
+                    relId ? `/${relId}` : ""
+                  }`
+                );
+              }
               return;
             }
           }
@@ -607,6 +614,10 @@ export default {
     },
     actionSettings: function () {
       return this.params.actions.find((a) => a.id === this.actionParamsId);
+    },
+    closeAfterSave: function () {
+      return this.$store.getters["menu/getMenuById"](this.$route.params.idItem)
+        ?.LCLOSEAFTERSAVE;
     },
   },
 };
