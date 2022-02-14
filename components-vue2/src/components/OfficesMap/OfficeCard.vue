8f9193fa7493546b8158013ddae882266e08c863
Дмитрий С. Александрин <ALDS@main.reso.ru>
2022-02-14 10:11:19 +0300

    правки в список телефонов карточки офиса

--- components-vue2/src/components/OfficesMap/OfficeCard.vue
+++ components-vue2/src/components/OfficesMap/OfficeCard.vue
@@ -1,38 +1,48 @@
 <template>
   <b-card :title="office.SSHORTNAME" class="mb-2">
     <b-card-text>
-    <div class="card-office-adress row">
-      <div class="col-4">
-        <img src="">
+      <div class="card-office-adress row">
+        <div class="col-4">
+          <img src="" />
+        </div>
+        <div class="col-8">
+          <div>{{ office.SADDRESS }}</div>
+          <div class="card-office-opened">открыт до</div>
+        </div>
+        <div class="col-12">
+          <button type="button" class="show-maps-balloon">
+            Показать на карте
+          </button>
+        </div>
       </div>
-      <div class="col-8">
-        <div>{{ office.SADDRESS }}</div>
-        <div class="card-office-opened">открыт до</div>
-      </div>
-      <div class="col-12">
-            <button type="button" class="show-maps-balloon">Показать на карте</button>
-      </div>
-    </div>
-    <div v-if="office.IDUNDERGROUND.length" class="card-office-undeground">
-        <span class="undeground-color"></span><span v-for="(item, index) in office.IDUNDERGROUND" :key="index">
-          {{ item.SNAME }}</span>
+      <div v-if="office.IDUNDERGROUND.length" class="card-office-undeground">
+        <span class="undeground-color"></span
+        ><span v-for="(item, index) in office.IDUNDERGROUND" :key="index">
+          {{ item.SNAME }}</span
+        >
         <span v-if="office.NDISTANSE" class="card-office-distance">
           {{ office.NDISTANSE.toFixed(1) + " км" }}
         </span>
       </div>
-    </div>
-    <div v-if="office.SGRAF" class="card-office-time">
+
+      <div v-if="office.SGRAF" class="card-office-time">
         <button type="button">Режим работы:</button>
         <div class="card-office-times">{{ office.SGRAF }}</div>
-    </div>
-    <div v-if="office.SGRAF" class="card-office-contacts">
-      <div v-if="office.SPHONE" class="card-office-phone">
-        <a v-bind:href="'tel:'+office.SPHONE">{{ office.SPHONE }}</a>
       </div>
-      <div v-if="office.SEMAIL">
-      <a v-bind:href="'mailto:'+office.SEMAIL" class="card-office-e-mail">{{ office.SEMAIL }}</a>
+      <div v-if="office.SGRAF" class="card-office-contacts">
+        <div v-for="(phone, i) in getPhones(office.SPHONE)" :key="i">
+          <div v-if="office.SPHONE" class="card-office-phone">
+            <a v-bind:href="'tel:' + office.SPHONE">{{ phone }}</a>
+          </div>
+        </div>
+        <div v-if="office.SEMAIL">
+          <a
+            v-bind:href="'mailto:' + office.SEMAIL"
+            class="card-office-e-mail"
+            >{{ office.SEMAIL }}</a
+          >
+        </div>
       </div>
-    </div>
     </b-card-text>
   </b-card>
 </template>
@@ -47,9 +57,15 @@ export default {
     BCardText,
   },
   props: ["office"],
+  methods: {
+    getPhones(phones) {
+      let phonesArr = phones.split(";");
+      phones.split(";");
+      phonesArr.pop();
+      return phonesArr;
+    },
+  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
