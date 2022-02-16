c79aa81172b9b6ccef987db1a1add44c46cb9c80
Гаврилов Алексей <gavan@reso.ru>
2022-02-16 11:02:47 +0300

    создание файла ics

--- components/Pages/Cabinet/Block/AddToCalendarButton.vue
+++ components/Pages/Cabinet/Block/AddToCalendarButton.vue
@@ -21,22 +21,24 @@ export default {
   },
   methods: {
     addDataToCalendar() {
-      //   console.log(this.$ics);
-      //   console.log(this.$ics.calendar());
-      console.log(this.data.list.items);
-      //   this.$ics.addEvent(
-      //     "RUS",
-      //     `${this.data.list.items[0].ACTIVENAME}`,
-      //     `${this.data.list.items[0].SFULLNAME}`,
-      //     `${this.data.list.items[0].SLPU}`,
-      //     `${this.data.list.items[0].DDATE}`,
-      //     `${this.data.list.items[0].DDATE}`
-      //   );
-      //   console.log(this.$ics.calendar());
+      console.log(this.$ics);
+      console.log(this.$ics.calendar());
+      console.log(this.data);
 
-      for (let i = 0; i < this.data.list.items.length; i++) {
-        console.log(this.data.list.items[i]);
-      }
+      this.$ics.addEvent(
+        "RUS",
+        `${this.data.list.items[0].ACTIVENAME}`,
+        `${this.data.list.items[0].SFULLNAME}`,
+        `${this.data.list.items[0].SLPU}`,
+        `${this.data.list.items[0].DDATE}`,
+        `${this.data.list.items[0].DDATE}`
+      );
+
+      console.log(this.$ics.calendar());
+
+      // for (let i = 0; i < this.data.list.items.length; i++) {
+      //   console.log(this.data.list.items[i]);
+      // }
 
       this.$ics.removeAllEvents();
     },
