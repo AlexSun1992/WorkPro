e7e215e789d3381577d99ec1a560fd4b0de35c02
Головлев Вячеслав <golvi@reso.ru>
2022-03-01 16:24:27 +0300

    исправлениеошибки

--- components-vue2/src/components/OfficesMap/OfficesMap.vue
+++ components-vue2/src/components/OfficesMap/OfficesMap.vue
@@ -269,7 +269,7 @@ export default {
             ? this.centerCoords
             : this.$store.getters["map/getDefaultCoords"],
           zoom: 12,
-          controls:[],
+          controls: [],
         };
       }
 
@@ -582,4 +582,4 @@ circle:hover {
 .test:after {
   content: "dsfdsf";
 }
-</style
+</style>
