bbdf88a0c2a7fee36971e86af3cd7787f32a5cd1
Гаврилов Алексей <gavan@reso.ru>
2022-03-11 15:36:14 +0300

    рассчет стоимости полиса после отмены решения капчи

---
+++ components/EventHandler/666/eventHandler.js
@@ -0,0 +1,23 @@
+function eventHandler(fields, action, func) {
+  function findField(name) {
+    const field = fields.find((item) => item.name === name);
+    if (field) {
+      return field;
+    }
+
+    throw new Error(`Поле ${name} не найдено в данных`);
+  }
+
+  if (action.value !== "Item36918") {
+    findField(`Item36918`).visible = true;
+    findField(`NPRICE`).visible = false;
+  }
+
+  if (func == "afterSave") {
+    findField(`Item36918`).visible = false;
+  }
+
+  return fields;
+}
+
+export { eventHandler };
