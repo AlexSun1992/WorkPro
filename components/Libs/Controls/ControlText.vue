f3f3da692b9c6c510658a7b757d9e555e25367e8
IONKA <ionka@reso.ru>
2022-03-31 13:35:01 +0300

    Откатили изменения за 27.01.2022 16:08.

--- components/Libs/Controls/ControlText.vue
+++ components/Libs/Controls/ControlText.vue
@@ -1,8 +1,9 @@
 <template>
-  <b-form-group :label="label" :label-for="data.name">
+  <b-form-group
+    :label="label"
+    :label-for="data.name"
+  >
     <b-form-textarea
-      @blur="blur"
-      @focus="focus"
       id="textarea1"
       v-model="fieldValue"
       :disabled="!edit ? !edit : data.readonly"
@@ -10,14 +11,18 @@
       placeholder="Введите текст"
       :rows="3"
       :max-rows="6"
+    />
+    <template
+      #label
     >
-    </b-form-textarea>
-    <template v-slot:label
-      ><span v-html="data.label"></span
-      ><span v-if="data.helpText">
-        (?)<vue-easy-tooltip with-arrow="true" position="top" offset="4">
-          <span v-html="data.helpText"></span></vue-easy-tooltip></span
-    ></template>
+      <span v-html="data.label" /><span v-if="data.helpText">
+        (?)<vue-easy-tooltip
+          with-arrow="true"
+          position="top"
+          offset="4"
+        >
+          <span v-html="data.helpText" /></vue-easy-tooltip></span>
+    </template>
     <b-form-invalid-feedback>
       Обязательно для заполнения
     </b-form-invalid-feedback>
@@ -27,12 +32,6 @@
 <script>
 export default {
   name: "ControlText",
-  data() {
-    return {
-      isFetch: null,
-      fieldValue: "",
-    };
-  },
   props: {
     data: {
       type: Object,
@@ -45,47 +44,21 @@ export default {
       default: () => false,
     },
   },
-  methods: {
-    focus() {
-      this.isFetch = true;
-    },
-    blur() {
-      this.isFetch = false;
-    },
-  },
   computed: {
     label() {
       return this.data.label;
     },
-    // fieldValue: {
-    //   get: function () {
-    //      console.log("я из get", this.data);
-    //     return this.data.value;
-    //   },
-    //   set: function (value) {
-    //     console.log("я из set", value);
-    //     if (this.focusLost) {
-    //       this.$emit("update", {
-    //         fieldId: this.data.fieldId,
-    //         name: this.data.name,
-    //         value: value,
-    //       });
-    //     }
-    //   },
-    // },
-    // fieldValue() {
-    //   return this.data.value;
-    // },
-  },
-  watch: {
-    isFetch(value) {
-      if (value === false) {
+    fieldValue: {
+      get () {
+        return this.data.value;
+      },
+      set (value) {
         this.$emit("update", {
           fieldId: this.data.fieldId,
           name: this.data.name,
-          value: value,
+          value,
         });
-      }
+      },
     },
   },
 };
@@ -96,7 +69,4 @@ export default {
   font-size: 12px;
   margin-top: 10px;
 }
-/* .form-control:disabled, .form-control[readonly]{
-    background-color: white;
-  } */
 </style>
