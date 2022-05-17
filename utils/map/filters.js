877ec1aa3d33b9f4813eacdd3b4ae2ad769ec173
Дмитрий С. Александрин <ALDS@main.reso.ru>
2022-05-17 11:14:39 +0300

    доработаны фильтры офисов

--- utils/map/filters.js
+++ utils/map/filters.js
@@ -5,11 +5,6 @@ export const filters = [
     value: false,
     id: "maps-check-office",
   },
-  /*  {
-        text: "Работает в выходные",
-        name: "WEEKENDS",
-        value: true,
-      },*/
   {
     text: "Центр выплат",
     name: "LREG_CENTER",
@@ -24,43 +19,62 @@ export const filters = [
   },
 ];
 
+// export const filterData = function (data, filters) {
+//   debugger;
+//   let candidates = [];
+//   data.forEach((office) => {
+//     let counter = 0;
+//     filters.forEach((filter) => {
+//       if (
+//         office[filter.name] === filter.value ||
+//         (office.hasOwnProperty(filter.name) && filter.value === "")
+//       ) {
+//         counter++;
+//       }
+//     });
+//     if (counter == filters.length) {
+//       candidates.push(office);
+//     }
+//   });
+//   return candidates;
+// };
+
 export const filterData = function (data, filters) {
-  let candidates = [];
+  let set = new Set();
+  // let candidates = [];
   data.forEach((office) => {
     let counter = 0;
     filters.forEach((filter) => {
-      if (
-        office[filter.name] === filter.value ||
-        (office.hasOwnProperty(filter.name) && filter.value === "")
-      ) {
-        counter++;
+      if (office[filter.name] === filter.value) {
+        set.add(office);
       }
     });
     if (counter == filters.length) {
-      candidates.push(office);
+      set.add(office);
     }
   });
-  return candidates;
+  return Array.from(set);
 };
 
 export const getFilters = function (filters) {
   let arr = [];
-  let name;
+  // let name;
   let filtersArray = [...filters];
-  filtersArray.reduce((sum, filter) => {
-    sum[filter.name] = (sum[filter.name] || 0) + 1;
-    if (sum[filter.name] > 1) {
-      name = filter.name;
-      let obj = { ...filter };
-      obj.value = "";
-      arr.push(obj);
-    }
-    return sum;
-  }, {});
-  if (name) {
-    arr.push(...filtersArray.filter((item) => item.name !== name));
-  } else {
-    filtersArray.forEach((item) => arr.push(item));
-  }
+  // filtersArray.reduce((sum, filter) => {
+  //   sum[filter.name] = (sum[filter.name] || 0) + 1;
+  //   if (sum[filter.name] > 1) {
+  //     name = filter.name;
+  //     let obj = { ...filter };
+  //     obj.value = "";
+  //     arr.push(obj);
+  //   }
+  //   return sum;
+  // }, {});
+  // if (name) {
+  //   arr.push(...filtersArray.filter((item) => item.name !== name));
+  // }
+  // else {
+  filtersArray.forEach((item) => arr.push(item));
+  // }
   return arr;
 };
