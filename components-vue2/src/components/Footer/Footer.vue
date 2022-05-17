789dffd0f16d727a2f559e08be6e18f41de3e31f
Гаврилов Алексей <gavan@reso.ru>
2022-05-17 15:18:22 +0300

    разработка Footer

--- components-vue2/src/components/Footer/Footer.vue
+++ components-vue2/src/components/Footer/Footer.vue
@@ -1,16 +1,77 @@
 <template>
-  <div>
-    <h1>This is Footer</h1>
-    <p>width:{{ width }}</p>
-    <p>heigth: {{ height }}</p>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-12 order-1 pl-md-0 px-0">
+        <div class="menu-link d-md-flex">
+          <!-- <p>width:{{ width }}</p>
+          <p>heigth: {{ height }}</p> -->
+          <div class="products_all ps-0">
+            <a href>Оформить страховой полис</a>
 
-    <div v-for="item in footerMenu" :key="item.id">
-      <p>{{ item.title }}</p>
-      <ul>
-        <li v-for="elem in item.items" :key="elem.id">
-          {{ elem }}
-        </li>
-      </ul>
+            <div v-for="(item, index) in footerMenuCarSave" :key="item.title">
+              <p @click="toggleVisibility(index)">
+                {{ item.title }}
+              </p>
+              <ul ref="index">
+                <li v-for="elem in item.items" :key="elem.id">
+                  {{ elem }}
+                </li>
+              </ul>
+            </div>
+
+            <div v-for="item in footerMenuProperty" :key="item.title">
+              <p @click="toggleVisibility(index)">
+                {{ item.title }}
+              </p>
+              <ul ref="index">
+                <li v-for="(elem, index) in item.items" :key="index">
+                  {{ elem }}
+                </li>
+              </ul>
+            </div>
+
+            <div v-for="(item, index) in footerMenuHealth" :key="item.title">
+              <p @click="toggleVisibility(index)">
+                {{ item.title }}
+              </p>
+              <ul ref="index">
+                <li v-for="elem in item.items" :key="elem.id">
+                  {{ elem }}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="info_all block-v-line-lg pn-sm-none">
+            <div
+              v-for="(item, index) in footerMenuAboutCompany"
+              :key="item.title"
+            >
+              <p @click="toggleVisibility(index)">
+                {{ item.title }}
+              </p>
+              <ul ref="index">
+                <li v-for="elem in item.items" :key="elem.id">
+                  {{ elem }}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <!-- <div>
+          <a href="#">8(800)234-18-02</a>
+          <span>Круглосуточно</span>
+          <div>
+            <a href="#">Apple Store</a>
+            <a href="#">Google Play</a>
+          </div>
+          <div>
+            <a href="#">faceBook</a>
+            <a href="#">VK</a>
+            <a href="#">Insta</a>
+          </div>
+        </div> -->
+      </div>
     </div>
   </div>
 </template>
@@ -24,11 +85,29 @@ export default {
     return {
       width: 0,
       height: 0,
-      footerMenu: null,
+      footerMenuCarSave: [],
+      footerMenuAboutCompany: [],
+      footerMenuHealth: [],
+      footerMenuProperty: [],
     };
   },
   created() {
-    this.footerMenu = footerMenu;
+    this.footerMenuProperty = footerMenu.filter((item) => {
+      return item.title === "Имущество";
+    });
+
+    this.footerMenuAboutCompany = footerMenu.filter((item) => {
+      return item.title === "О компании";
+    });
+
+    this.footerMenuHealth = footerMenu.filter((item) => {
+      return item.title === "Жизнь и здоровье";
+    });
+
+    this.footerMenuCarSave = footerMenu.filter((item) => {
+      return item.title === "Автострахование";
+    });
+
     window.addEventListener("resize", this.handleResize);
     this.handleResize();
   },
@@ -36,6 +115,12 @@ export default {
     window.removeEventListener("resize", this.handleResize);
   },
   methods: {
+    toggleVisibility(index) {
+      //console.log(this.$refs.index[index].style.display);
+      //   if (this.$refs.index[index].style.display === "block") {
+      //     this.$refs.index[index].style.display = "none";
+      //   } else this.$refs.index[index].style.display = "block";
+    },
     handleResize() {
       this.width = window.innerWidth;
       this.height = window.innerHeight;
