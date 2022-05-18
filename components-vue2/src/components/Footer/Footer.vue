def316787938f1d00f358a845c4c716754b1caf7
Гаврилов Алексей <gavan@reso.ru>
2022-05-18 13:23:01 +0300

    отображение footer

--- components-vue2/src/components/Footer/Footer.vue
+++ components-vue2/src/components/Footer/Footer.vue
@@ -1,79 +1,128 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-8 col-12 order-1 pl-md-0 px-0">
-        <div class="menu-link d-md-flex">
-          <!-- <p>width:{{ width }}</p>
-          <p>heigth: {{ height }}</p> -->
-          <div class="products_all ps-0">
-            <a href>Оформить страховой полис</a>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-12 order-1 pl-md-0 px-0">
+          <div class="menu-link d-md-flex">
+            <div class="products_all ps-0">
+              <a href>Оформить страховой полис</a>
 
-            <div v-for="(item, index) in footerMenuCarSave" :key="item.title">
-              <p @click="toggleVisibility(index)">
-                {{ item.title }}
-              </p>
-              <ul ref="index">
-                <li v-for="elem in item.items" :key="elem.id">
-                  {{ elem }}
-                </li>
-              </ul>
-            </div>
+              <div class="product menu-auto mb-md-4 pb-md-4 position-relative">
+                <a
+                  v-for="(item, index) in footerMenuCarSave"
+                  :key="index"
+                  :href="item.titleRef"
+                  >{{ item.title }}</a
+                >
+                <div class="priduct_link">
+                  <a
+                    v-for="(item, index) in footerMenuCarSave[0].items"
+                    :key="index"
+                    :href="item.ref"
+                  >
+                    {{ item.title }}
+                  </a>
+                </div>
+              </div>
 
-            <div v-for="item in footerMenuProperty" :key="item.title">
-              <p @click="toggleVisibility(index)">
-                {{ item.title }}
-              </p>
-              <ul ref="index">
-                <li v-for="(elem, index) in item.items" :key="index">
-                  {{ elem }}
-                </li>
-              </ul>
-            </div>
+              <div class="product menu-im">
+                <a
+                  v-for="(item, index) in footerMenuProperty"
+                  :key="index"
+                  :href="item.titleRef"
+                  >{{ item.title }}</a
+                >
+                <div class="priduct_link">
+                  <a
+                    v-for="(item, index) in footerMenuProperty[0].items"
+                    :key="index"
+                    :href="item.ref"
+                  >
+                    {{ item.title }}
+                  </a>
+                </div>
+              </div>
 
-            <div v-for="(item, index) in footerMenuHealth" :key="item.title">
-              <p @click="toggleVisibility(index)">
-                {{ item.title }}
-              </p>
-              <ul ref="index">
-                <li v-for="elem in item.items" :key="elem.id">
-                  {{ elem }}
-                </li>
-              </ul>
+              <div class="product menu-med">
+                <a
+                  v-for="(item, index) in footerMenuHealth"
+                  :key="index"
+                  :href="item.titleRef"
+                  >{{ item.title }}</a
+                >
+                <div class="priduct_link">
+                  <a
+                    v-for="(item, index) in footerMenuHealth[0].items"
+                    :key="index"
+                    :href="item.ref"
+                  >
+                    {{ item.title }}
+                  </a>
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="info_all block-v-line-lg pn-sm-none">
-            <div
-              v-for="(item, index) in footerMenuAboutCompany"
-              :key="item.title"
-            >
-              <p @click="toggleVisibility(index)">
-                {{ item.title }}
-              </p>
-              <ul ref="index">
-                <li v-for="elem in item.items" :key="elem.id">
-                  {{ elem }}
-                </li>
-              </ul>
+            <!-- <div class="info_all block-v-line-lg pn-sm-none"></div> -->
+            <div class="info_all block-v-line-lg pn-sm-none">
+              <a href="/about/">О компании</a>
+              <div class="product menu-med">
+                <a
+                  v-for="(item, index) in footerMenuAboutCompany"
+                  :key="index"
+                  :href="item.titleRef"
+                  >{{ item.title }}</a
+                >
+                <div class="priduct_link">
+                  <a
+                    v-for="(item, index) in footerMenuAboutCompany[0].items"
+                    :key="index"
+                    :href="item.ref"
+                  >
+                    {{ item.title }}
+                  </a>
+                </div>
+              </div>
+              <!-- ///// -->
             </div>
           </div>
         </div>
+        <div
+          class="col-lg-4 pl-lg-4 col-md-6 col-12 order-2 order-md-3 order-lg-2 pt-4 pt-lg-0 pl-md-3"
+        >
+          <div class="title mt-0 mb-0 mb-lg-3">
+            <a href="tel:+78002341802" class="footer-tel">8 (800) 234-18-02</a>
+            <br /><span class="footer-tel-ph">Круглосуточно</span>
+          </div>
+          <div class="footer-app-btn">
+            <a
+              class="btn-light btn-icon icon-ios"
+              href="https://apps.apple.com/us/app/resomobile/id1127266069"
+              >Apple Store</a
+            >
+            <a
+              class="btn-light btn-icon icon-android"
+              href="https://play.google.com/store/apps/details?id=ru.reso.app"
+              >Google Play</a
+            >
+          </div>
+          <div class="social mt-3 mt-sm-0">
+            <a href="https://ru-ru.facebook.com/reso.ru" class="footer-fb"></a>
+            <a href="http://vk.com/reso_garantia " class="footer-vk"></a>
+            <a
+              href="https://www.instagram.com/reso.ru_official/"
+              class="footer-in"
+            ></a>
+          </div>
 
-        <!-- <div>
-          <a href="#">8(800)234-18-02</a>
-          <span>Круглосуточно</span>
-          <div>
-            <a href="#">Apple Store</a>
-            <a href="#">Google Play</a>
+          <div class="iconix">
+            Дизайн:
+            <img
+              src="/export/system/modules/ru.reso.v2/resources/img/icons/iconix.svg"
+            />
           </div>
-          <div>
-            <a href="#">faceBook</a>
-            <a href="#">VK</a>
-            <a href="#">Insta</a>
-          </div>
-        </div> -->
+        </div>
       </div>
     </div>
-  </div>
+  </footer>
 </template>
 
 <script>
@@ -115,12 +164,14 @@ export default {
     window.removeEventListener("resize", this.handleResize);
   },
   methods: {
-    toggleVisibility(index) {
-      //console.log(this.$refs.index[index].style.display);
-      //   if (this.$refs.index[index].style.display === "block") {
-      //     this.$refs.index[index].style.display = "none";
-      //   } else this.$refs.index[index].style.display = "block";
-    },
+    // toggleVisibility(index) {
+    //   console.log(index);
+
+    //   //console.log(this.$refs.index[index].style.display);
+    //   //   if (this.$refs.index[index].style.display === "block") {
+    //   //     this.$refs.index[index].style.display = "none";
+    //   //   } else this.$refs.index[index].style.display = "block";
+    // },
     handleResize() {
       this.width = window.innerWidth;
       this.height = window.innerHeight;
--- components-vue2/src/components/Footer/footerMenu.js
+++ components-vue2/src/components/Footer/footerMenu.js
@@ -1,27 +1,59 @@
 const footerMenu = [
   {
     title: "Автострахование",
-    items: ["ОСАГО", "Каско", "Зеленая карта"],
-  },
-  {
-    title: "Жизнь и здоровье",
-    items: ["ДМС", "Туризм", "Несчастный случай"],
-  },
-  {
-    title: "О компании",
+    titleRef: "/individual/auto/",
+
     items: [
-      "Новости",
-      "Вакансии",
-      "Закупки",
-      "Раскрытие сведений",
-      "Информация для потребителей",
-      "Правила и тарифы",
-      "Карта сайта",
+      { title: "ОСАГО", ref: "/individual/auto/osago/" },
+      { title: "Каско", ref: "/individual/auto/kasko/" },
+      { title: "Зеленая карта", ref: "/individual/auto/greencard/" },
     ],
   },
+
   {
     title: "Имущество",
-    items: ["Квартира", "Дом", "Ипотека"],
+    titleRef: "/individual/property/",
+
+    items: [
+      { title: "Квартира", ref: "/individual/property/flat/" },
+      { title: "Дом", ref: "/individual/property/reso-dom/" },
+      { title: " Ипотека", ref: "/individual/property/ipoteka/" },
+    ],
+  },
+
+  {
+    title: "Жизнь и здоровье",
+    titleRef: "/individual/",
+
+    items: [
+      { title: "ДМС", ref: "/individual/property/flat/" },
+      { title: "Туризм", ref: "/individual/travel/" },
+      { title: "Несчастный случай", ref: "/individual/accident/" },
+      {
+        title: "Накопительное и инвестиционное страхование",
+        ref: "/individual/life/",
+      },
+    ],
+  },
+
+  {
+    title: "О компании",
+    titleRef: "/about/",
+    items: [
+      { title: "Новости", ref: "/about/news/" },
+      { title: "Вакансии", ref: "/about/career/" },
+      { title: "Закупки", ref: "/about/tenders/" },
+      {
+        title: "Раскрытие сведений",
+        ref: "/investors/disclosure/",
+      },
+      {
+        title: "Информация для потребителей",
+        ref: "/about/consumer-information/",
+      },
+      { title: "Правила и тарифы", ref: "/about/consumer-information/rules/" },
+      { title: "Карта сайта", ref: "/sitemap" },
+    ],
   },
 ];
 
