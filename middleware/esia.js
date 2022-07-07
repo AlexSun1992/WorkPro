b7a36f065c6590f91067babc2e1bdcfd707b0f35
IONKA <ionka@reso.ru>
2022-07-07 16:57:00 +0300

    [LK2-403] Изменен метод на POST.

--- middleware/esia.js
+++ middleware/esia.js
@@ -1,9 +1,9 @@
 export default function ({ route, redirect, $axios, $cookiz }) {
-  const url = `/am/free/v2/data/55/801/0/0?code=${route.query?.code}`;
+  const url = `/am/free/v2/datacard/55/801`;
   return $axios
-    .get(url)
+    .post(url, { code: route.query?.code })
     .then((res) => {
-      const data = res.data[0]._data[0];
+      const data = res.data[0];
       $cookiz.set("auth._token.local", data.ACCESS_TOKEN);
       $cookiz.set("auth._refresh_token.local", data.REFRESH_TOKEN);
       return redirect("/cabinet");
