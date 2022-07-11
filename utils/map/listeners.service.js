83cfa14dbccf274fb2f61dba270f9ac1dfee3bd0
Дмитрий С. Александрин <ALDS@main.reso.ru>
2022-07-11 15:30:07 +0300

    добавил тесты к функции уведомления подписчиков компонента ShowCity

--- utils/map/listeners.service.js
+++ utils/map/listeners.service.js
@@ -1,9 +1,18 @@
-const callbacks = [];
+export const callbacks = [];
 
 export function addListener(func) {
+  if (typeof func !== "function") {
+    throw new Error("Передана не функция");
+  }
   callbacks.push(func);
 }
 
 export function notifyListeners(data) {
-  callbacks.forEach((func) => func(data));
+  callbacks.forEach((func) => {
+    try {
+      func(data);
+    } catch (e) {
+      console.error(e);
+    }
+  });
 }
--- utils/map/listeners.service.test.js
+++ utils/map/listeners.service.test.js
@@ -1,7 +1,29 @@
-import { notifyListeners } from "./listeners.service.js";
+import { addListener, notifyListeners } from "./listeners.service";
+describe("listenerService", () => {
+  it("addListener принимает на вход функцию", () => {
+    expect(() => addListener(() => {})).not.toThrow();
+  });
 
-it("listeners to be called", () => {});
+  it("получаем ошибку если передана не функция", () => {
+    expect(() => addListener()).toThrow();
+  });
 
-it("listener is a function only", () => {});
+  it("функция func1 вызывается", () => {
+    const func1 = jest.fn();
+    addListener(func1);
+    notifyListeners();
 
-it("all functions fired despite error in one of them", () => {});
+    expect(func1).toHaveBeenCalledTimes(1);
+  });
+  it("функция func2 после ошибочной func1 вызывается", () => {
+    const func1 = () => {
+      throw new Error("");
+    };
+    const func2 = jest.fn();
+    addListener(func1);
+    addListener(func2);
+    notifyListeners();
+
+    expect(func2).toHaveBeenCalledTimes(1);
+  });
+});
