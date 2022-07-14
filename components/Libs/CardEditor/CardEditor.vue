93f855854cb6f9bfd64a222a65241263d1f8e404
Дмитрий С. Александрин <ALDS@main.reso.ru>
2022-07-14 11:52:59 +0300

    test

--- components/Libs/CardEditor/CardEditor.vue
+++ components/Libs/CardEditor/CardEditor.vue
@@ -25,7 +25,7 @@
         />
       </b-form>
     </b-modal>
-    <div v-if="data.length">
+    <div v-if="data.length && isLoadedScript">
       <Form
         v-if="!isAccordion && !isBlock"
         class="block-profile"
