61270fd14fe24183dcd7a2dba4e87d2706c7eb7e
Канев Евгений <kanen@reso.ru>
2022-07-26 16:19:10 +0300

    [+] тест на ошибку с цифрами в начале

--- plugins/auth/toast.helper.test.js
+++ plugins/auth/toast.helper.test.js
@@ -1,13 +1,13 @@
 import { getErrorMessage } from "./toast.helper";
 
 describe("Модуль вывода сообщения об ошибке", () => {
-  it("Тестирование сообщения с ORA в тексте без скобок", () => {
+  it("Должен обрабатывать сообщения с ORA в тексте без скобок", () => {
     const errorMessageText =
       'ORA-20105: Некорректный номер телефона\nORA-06512: на  "MOBILE.CLIENTUTILS", line 934\nORA-06512: на  line 1\nORA-06512: на  "SYS.DBMS_SQL", line 1721\nORA-06512: на  "MOBILE.AMUTILSREST", line 3018\nORA-06512: на  line 1\n';
     const errorMessageWithoutORA = getErrorMessage(errorMessageText);
     expect(errorMessageWithoutORA).toBe("Некорректный номер телефона");
   });
-  it("Тестирование сообщения с ORA в тексте с квадратными скобками", () => {
+  it("Должен обрабатывать сообщения с ORA в тексте с квадратными скобками", () => {
     const errorMessageText =
       'ORA-20105: [Данный номер уже использован в другом личном кабинете]\nORA-06512: на  "MOBILE.CLIENTUTILS", line 954\nORA-06512: на  line 1\nORA-06512: на  "SYS.DBMS_SQL", line 1721\nORA-06512: на  "MOBILE.AMUTILSREST", line 3018\nORA-06512: на  line 1\n';
     const errorMessageBrackets = getErrorMessage(errorMessageText);
@@ -16,22 +16,28 @@ describe("Модуль вывода сообщения об ошибке", () =>
     );
   });
 
-  it("Тестирование сообщения об ошибке без ORA в тексте", () => {
+  it("Должен обрабатывать сообщения об ошибке без ORA в тексте", () => {
     const errorMessageText = "Некорректный номер телефона";
     const errorMessageWithoutORA = getErrorMessage(errorMessageText);
     expect(errorMessageWithoutORA).toBe("Некорректный номер телефона");
   });
 
-  it("Тестирование сообщения с двоеточием в тексте ошибки", () => {
+  it("Должен обрабатывать сообщения с двоеточием в тексте ошибки", () => {
     const errorMessageText =
       'ORA-20105: Перечень полей с ошибками: имя, фамилия\nORA-06512: на  "MOBILE.CLIENTUTILS", line 954\nORA-06512: на  line 1\nORA-06512: на  "SYS.DBMS_SQL", line 1721\nORA-06512: на  "MOBILE.AMUTILSREST", line 3018\nORA-06512: на  line 1\n';
     const errorMessage = getErrorMessage(errorMessageText);
     expect(errorMessage).toBe("Перечень полей с ошибками: имя, фамилия");
   });
 
-  it("Тестирование сообщения со словом ORACLE в начале", () => {
+  it("Должен обрабатывать сообщения со словом ORACLE в начале", () => {
     const errorMessageText = "ORACLE-скрипт не валидный";
     const errorMessage = getErrorMessage(errorMessageText);
     expect(errorMessage).toBe("ORACLE-скрипт не валидный");
+  });
+
+  it("Должен обрабатывать сообщения с цифрой в начале", () => {
+    const errorMessageText = "15 ошибок не обработано";
+    const errorMessage = getErrorMessage(errorMessageText);
+    expect(errorMessage).toBe("15 ошибок не обработано");
   });
 });
