6196e151a02bb0b2c0ce3f6c1ac2749bf96f7d8f
Канев Евгений <kanen@reso.ru>
2022-11-07 14:06:10 +0300

    [+] PasswordRecoveryFormWrapper

---
+++ components-vue2/src/components/Login/RecoveryForm/PasswordRecoveryFormWrapper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="row mt-4">
+      <div class="col-12 col-lg-9">
+        <PasswordRecoveryForm />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import PasswordRecoveryForm from "./PasswordRecoveryForm.vue";
+
+export default {
+  name: "PasswordRecoveryFormWrapper",
+  components: {
+    PasswordRecoveryForm,
+  },
+
+  head: {
+    link: [
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href: "https://reso.ru/system/modules/ru.reso.v2/resources/css/bootstrap-grid.css",
+      },
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href: "https://reso.ru/system/modules/ru.reso.v2/resources/css/bootstrap-reboot.css",
+      },
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href: "https://reso.ru/system/modules/ru.reso.v2/resources/css/bootstrap-utilities.css",
+      },
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href: "https://reso.ru/system/modules/ru.reso.v2/resources/css/style_new.css",
+      },
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href: "https://reso.ru/system/modules/ru.reso.v2/resources/css/style_main.css",
+      },
+    ],
+  },
+};
+</script>
