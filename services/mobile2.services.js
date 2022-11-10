5cfc8fce48556f070228e2907c2475bb0fadd518
IONKA <ionka@reso.ru>
2022-11-10 15:00:24 +0300

    [LK2-633] Увеличен таймаут запросов до 5 минут.

--- services/mobile2.services.js
+++ services/mobile2.services.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const mobile2Service = (url) => {
   const instance = axios.create({});
-  instance.defaults.timeout = 30000;
+  instance.defaults.timeout = 300000;
   instance.defaults.timeoutErrorMessage = "Запрос выполняется больше 30 секунд";
   instance.defaults.baseURL =
     url || process.env.MOBILE2_URL || "https://mobile2.reso.ru";
