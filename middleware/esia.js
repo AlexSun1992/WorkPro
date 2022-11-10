38ae8011d17ca8d4740262c797920ed28f17901a
IONKA <ionka@reso.ru>
2022-11-10 11:33:40 +0300

    [LK2-611] Доработка обработки ошибки при входе через Госуслуги.

--- middleware/esia.js
+++ middleware/esia.js
@@ -2,6 +2,9 @@ import { getErrorMessage } from "../plugins/auth/toast.helper";
 
 export default function ({ route, redirect, $axios, $cookiz }) {
   const url = `/am/free/v2/datacard/55/801`;
+  if (route.query?.code === undefined) {
+    return redirect(`/login?error="Не задан параметр код!"`);
+  }
   return $axios
     .post(url, { code: route.query?.code })
     .then((res) => {
@@ -11,9 +14,13 @@ export default function ({ route, redirect, $axios, $cookiz }) {
       return redirect("/cabinet");
     })
     .catch((error) => {
-      console.error(error.response.data);
+      console.error(error?.response?.data);
       return redirect(
-        `/login?error=${getErrorMessage(error.response.data.MESSAGE)}`
+        `/login?error=${
+          error?.response?.data?.MESSAGE
+            ? getErrorMessage(error.response?.data?.MESSAGE)
+            : error
+        }`
       );
     });
 }
