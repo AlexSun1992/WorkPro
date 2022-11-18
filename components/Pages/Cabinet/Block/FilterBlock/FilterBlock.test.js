79833ae1fb93f5168faaea9a695910a48956649e
Гаврилов Алексей <gavan@reso.ru>
2022-11-18 17:15:22 +0300

    написание тестов FilterBlock

--- components/Pages/Cabinet/Block/FilterBlock/FilterBlock.test.js
+++ components/Pages/Cabinet/Block/FilterBlock/FilterBlock.test.js
@@ -1,26 +1,109 @@
 import { createLocalVue, mount } from "@vue/test-utils";
 import axios from "axios";
+import Vuex, { Store } from "vuex";
 import FilterBlock from "./FilterBlock.vue";
+import { allBlocksMocks } from "./FilterBlock.fixtures.js";
+import { state, actions, mutations } from "../../../../../store/blocks";
 
-jest.mock("axios");
+import { getters as getter } from "../../../../../store/blocks";
+import { mutations as mutation } from "../../../../../store/blocks";
+import { state as states } from "../../../../../store/blocks";
+// jest.mock("axios");
 
 describe("FilterBlock", () => {
-  //   let wrapper;
-  //   const createComponent = () => {
-  //     wrapper = mount(FilterBlock);
-  //   };
-  //   beforeEach(() => {
-  //     wrapper.destroy();
-  //   });
-  //
+  const localVue = createLocalVue();
+  localVue.use(Vuex);
 
   it("first test for filterBlock", () => {
-    // Arrange
-    // createComponent();
+    const $route = {
+      fullPath: "/cabinet/55/0/712",
+      query: {},
+    };
 
-    const wrapper = mount(FilterBlock);
+    const $store = new Store({
+      getters: getter,
+      mutations: mutation,
+      state: states,
+    });
 
-    // Assert
-    expect(wrapper.text()).not.toBe(undefined);
+    const wrapper = mount(FilterBlock, {
+      localVue,
+      propsData: {
+        propertyName: "SSTATUS",
+        itemId: "712",
+        showFilteredItemsCount: true,
+      },
+      mocks: {
+        $route,
+        $store,
+      },
+    });
+    expect(wrapper.text()).not.toBe(null);
   });
 });
+
+// const mockRoute = {
+//   params: {
+//     idModule: "55",
+//     idParent: "0",
+//     idItem: "712",
+//   },
+// };
+
+// const mockRouter = {
+//   push: jest.fn(),
+// };
+
+// let wrapper;
+
+// const localVue = createLocalVue();
+// localVue.use(Vuex);
+// let getters;
+// let store;
+
+// beforeEach(() => {
+//   getters = {
+//     getUnfilteredBlockById: jest.fn(),
+//   };
+//   store = new Vuex.Store({
+//     state: {},
+//     getters,
+//   });
+// });
+
+// it("first test for filterBlock", () => {
+//   wrapper = mount(FilterBlock, {
+//     localVue,
+//     store,
+//   });
+//   expect(wrapper.text()).not.toBe(null);
+// });
+
+//
+// const localVue = createLocalVue();
+// localVue.use(Vuex);
+
+// const propsData = {
+//   uniqueItems: ["Проекты", "Действующие", "Архивные"],
+//   defaultValue: null,
+//   propertyName: "SSTATUS",
+//   filterType: "radiobutton",
+//   itemId: "712",
+//   showButtonName: "Все полисы",
+//   showFilteredItemsCount: true,
+// };
+
+// const store = new Vuex.Store({
+//   getters,
+//   state,
+//   actions,
+//   mutations,
+// });
+
+// const createComponent = ({ props } = {}) => {
+//   wrapper = mount(FilterBlock, {
+//     localVue,
+//     props,
+//     store,
+//   });
+// };
