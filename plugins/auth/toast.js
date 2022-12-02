18183ab0ed18cb54a9ac4938d6a66295569b80e6
Гаврилов Алексей <gavan@reso.ru>
2022-12-02 11:34:08 +0000

    [+] Подмена системные ошибки < ORA-10000 на ссылку в ЛК

--- plugins/auth/toast.js
+++ plugins/auth/toast.js
@@ -12,7 +12,33 @@ export function makeToast(error) {
   if (toastCount > 2) {
     $nuxt.$bvToast.hide(toastCount - 2);
   }
-  $nuxt.$bvToast.toast(getErrorMessage(error), {
+
+  const getErrObj = getErrorMessage(error);
+
+  if (typeof getErrObj === "object") {
+    const htmlElement = this.$createElement;
+    const vNodesMSG = htmlElement("p", [
+      htmlElement("p", { props: { type: "grow", small: true } }),
+      `${getErrObj.errorText}`,
+      htmlElement(
+        "b-link",
+        { props: { href: getErrObj.errorHref } },
+        `${getErrObj.errorLink}`
+      ),
+    ]);
+
+    $nuxt.$bvToast.toast(vNodesMSG, {
+      id: toastCount.toString(),
+      title: "Ошибка",
+      variant: "danger",
+      autoHideDelay: 20000,
+      appendToast: false,
+      toaster: "b-toaster-top-full",
+    });
+    return;
+  }
+
+  $nuxt.$bvToast.toast(getErrObj, {
     id: toastCount.toString(),
     title: "Ошибка",
     variant: "danger",
