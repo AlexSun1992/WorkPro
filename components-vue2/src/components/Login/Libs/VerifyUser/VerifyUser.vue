254b42743a0d85e95fc6b600a48a1258423422ab
Гаврилов Алексей <gavan@reso.ru>
2022-12-05 18:36:42 +0300

    fix VeriFyUser

--- components-vue2/src/components/Login/Libs/VerifyUser/VerifyUser.vue
+++ components-vue2/src/components/Login/Libs/VerifyUser/VerifyUser.vue
@@ -301,12 +301,7 @@ export default {
             }
             return null;
           };
-          const response = await mobile2Service().post(
-            getURL(),
-            params,
-            headers
-          );
-
+          const response = await mobile2Service.post(getURL(), params, headers);
           const getSuccessSendMessageText =
             getMessageFromSuccessResponse(response);
           if (getSuccessSendMessageText !== undefined) {
