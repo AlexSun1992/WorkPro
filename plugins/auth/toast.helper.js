86eda51ccc7a8a624fc52da8a7af105524c23a2b
Гаврилов Алексей <gavan@reso.ru>
2022-12-21 17:05:40 +0300

    добавил функцию getErrorNumber

--- plugins/auth/toast.helper.js
+++ plugins/auth/toast.helper.js
@@ -20,14 +20,9 @@ export function convertErrorMessageToArray(errorMessage) {
  * @returns {string}
  */
 
-export function getErrorMessage(errorMessage) {
-  const [errMessageString] = convertErrorMessageToArray(errorMessage);
-
-  const stringWithBrackets = errMessageString.match(/\[(.+)]/);
-
-  const getORAnumber = errorMessage.match(/\s?ORA-\d{5}/);
-
-  if (getORAnumber) {
+export function getErrorNumber(errorMessage) {
+  const getDoubleCloseMistake = errorMessage.match(/\s?ORA-\d{5}:\s?ORA-\d{5}/);
+  if (getDoubleCloseMistake) {
     const getArrWithMistakes = errorMessage.match(/\s?ORA-\d{5}/g);
     const onlyPureMistakesName = getArrWithMistakes.filter(
       (item) => !item.includes("\n")
@@ -41,8 +36,23 @@ export function getErrorMessage(errorMessage) {
     if (onlyPureMistakesName.length <= 1) {
       [compareNumber] = onlyPureMistakesName;
     }
+    return compareNumber;
+  }
+  const getORAnumber = errorMessage.match(/\s?ORA-\d{5}/);
+  return getORAnumber;
+}
 
-    if (MAX_ORA_ERROR > compareNumber) {
+export function getErrorMessage(errorMessage) {
+  const [errMessageString] = convertErrorMessageToArray(errorMessage);
+
+  const stringWithBrackets = errMessageString.match(/\[(.+)]/);
+
+  const getORAnumber = errorMessage.match(/\s?ORA-\d{5}/);
+
+  if (getORAnumber) {
+    const errNumber = getErrorNumber(errorMessage);
+
+    if (MAX_ORA_ERROR > errNumber) {
       return {
         errorText:
           "Приносим извинения, в Личном Кабинете что-то пошло не так.\n" +
