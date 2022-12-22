421a469697bbce4060f67c818018b66d90d5f976
Гаврилов Алексей <gavan@reso.ru>
2022-12-22 11:10:35 +0300

    тест для verifyUser

--- components-vue2/src/components/Login/Libs/VerifyUser/VerifyUser.test.js
+++ components-vue2/src/components/Login/Libs/VerifyUser/VerifyUser.test.js
@@ -1,7 +1,44 @@
+import VerifyUser from "./VerifyUser.vue";
+import { mount } from "@vue/test-utils";
+jest.mock("axios");
+
 describe("VerifyUser", () => {
   let wrapper;
 
   const createComponent = () => {
-    wrapper = mount();
+    wrapper = mount(VerifyUser, {
+      propsData: [
+        "count",
+        "v",
+        "validateState",
+        "disabled",
+        "loginType",
+        "modeType",
+        "label",
+        "context",
+        "textMessage",
+        "tabIndex",
+        "error",
+        "isError",
+        "isCodeFieldValid",
+        "logParams",
+      ],
+
+      mocks: {
+        $store: { commit: () => null },
+      },
+    });
   };
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  it("тестовый тест", () => {
+    createComponent();
+    const result = wrapper;
+    expect(result).not.toBe(null);
+  });
 });
