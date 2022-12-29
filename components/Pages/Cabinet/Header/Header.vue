ad9d02a398135a00005ebd226616eda588ab73e9
podns <podns@reso.ru>
2022-12-29 13:14:48 +0300

    LK2-712 Добавил атрибуты data-testid для тестов

--- components/Pages/Cabinet/Header/Header.vue
+++ components/Pages/Cabinet/Header/Header.vue
@@ -62,6 +62,7 @@
             no-caret
             @show="bodySize('blocksize')"
             @hide="bodySize('unblocksize')"
+            data-testid="cabinetLoginDropDawn"
           >
             <template #button-content>
               {{ userInfo.SSECONDNAME }} {{ userInfo.SFIRSTNAME }}
