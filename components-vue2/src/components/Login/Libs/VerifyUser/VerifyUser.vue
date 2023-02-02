fee6edb0ec9631c542964d114e26ac18337d03c7
Филимонова Виолетта <filvv@reso.ru>
2023-02-02 17:19:06 +0300

    change logs

--- components-vue2/src/components/Login/Libs/VerifyUser/VerifyUser.vue
+++ components-vue2/src/components/Login/Libs/VerifyUser/VerifyUser.vue
@@ -323,6 +323,15 @@ export default {
       }
     },
     async getCode(token = null) {
+      this.$LogEvent({
+        formName: "VerifyUser",
+        idEventType: this.loginType === "phone" ? 155 : 162,
+        controlName: "PasswordRecoveryForm.vue",
+        message: `Нажал на кнопку "Получить код через ${
+          this.loginType === "phone" ? "номер" : "EMAIL"
+        }"`,
+        timeUser: new Date(),
+      });
       this.v.code.$model = null;
       this.codeFieldShown = false;
       this.isPhoneChanged = false;
@@ -370,13 +379,15 @@ export default {
               this.errorMessage =
                 "В Личном кабинете отсутствует профиль с данным номером телефона";
               this.isSendCode = false;
-              this.$LogEvent({
-                formName: "VerifyUser errorMessage",
-                idEventType: this.loginType ? 155 : 162,
-                controlName: "VerifyUser.vue",
-                message: `Показало сообщение об ошибке на ${ this.loginType === "phone" ? "номере" : "EMAIL"}"`,
-                timeUser: new Date(),
-            });
+              // this.$LogEvent({
+              //   formName: "VerifyUser errorMessage",
+              //   idEventType: this.loginType ? 153 : 164,
+              //   controlName: "VerifyUser.vue",
+              //   message: `Показало сообщение об ошибке на ${
+              //     this.loginType === "phone" ? "номере" : "EMAIL"
+              //   }"`,
+              //   timeUser: new Date(),
+              // });
               return;
             }
 
@@ -390,13 +401,15 @@ export default {
               this.loading = false;
               this.isSendCode = false;
               this.errorMessage = response1.data?.INFO ?? "Неизвестная ошибка";
-              this.$LogEvent({
-                formName: "VerifyUser errorMessage",
-                idEventType: this.loginType ? 155 : 162,
-                controlName: "VerifyUser.vue",
-                message: `Показало сообщение об ошибке на ${ this.loginType === "phone" ? "номере" : "EMAIL"}"`,
-                timeUser: new Date(),
-            });
+              // this.$LogEvent({
+              //   formName: "VerifyUser errorMessage",
+              //   idEventType: this.loginType ? 153 : 164,
+              //   controlName: "VerifyUser.vue",
+              //   message: `Показало сообщение об ошибке на ${
+              //     this.loginType === "phone" ? "номере" : "EMAIL"
+              //   }"`,
+              //   timeUser: new Date(),
+              // });
               return;
             }
 
@@ -433,9 +446,11 @@ export default {
                 formName: "VerifyUser errorMessage",
                 idEventType: this.loginType ? 155 : 162,
                 controlName: "VerifyUser.vue",
-                message: `Показало сообщение об ошибке на ${ this.loginType === "phone" ? "номере" : "EMAIL"}"`,
+                message: `Показало сообщение об ошибке на ${
+                  this.loginType === "phone" ? "номере" : "EMAIL"
+                }"`,
                 timeUser: new Date(),
-            });
+              });
               return;
             }
             if (response2?.status === 500 || response2?.data[0]?.ERRORCODE) {
@@ -513,12 +528,14 @@ export default {
                 /^\[|\]$/g,
                 ""
               ) ?? "Неизвестная ошибка";
-              this.$LogEvent({
-                formName: "VerifyUser errorMessage",
-                idEventType: this.loginType ? 155 : 162,
-                controlName: "VerifyUser.vue",
-                message: `Показало сообщение об ошибке на ${ this.loginType === "phone" ? "номере" : "EMAIL"}"`,
-                timeUser: new Date(),
+            this.$LogEvent({
+              formName: "VerifyUser errorMessage",
+              idEventType: this.loginType ? 155 : 162,
+              controlName: "VerifyUser.vue",
+              message: `Показало сообщение об ошибке на ${
+                this.loginType === "phone" ? "номере" : "EMAIL"
+              }"`,
+              timeUser: new Date(),
             });
           }
         } else {
@@ -527,14 +544,16 @@ export default {
       } catch (e) {
         this.loading = false;
         console.log(e);
+        this.$LogEvent({
+          formName: "VerifyUser",
+          idEventType: this.loginType === "phone" ? 155 : 162,
+          controlName: "PasswordRecoveryForm.vue",
+          message: `Нажал на кнопку "Получить код через ${
+            this.loginType === "phone" ? "номер" : "EMAIL"
+          }"`,
+          timeUser: new Date(),
+        });
       }
-      this.$LogEvent({
-              formName: "VerifyUser",
-              idEventType: this.loginType === "phone" ? 155 : 162,
-              controlName: "PasswordRecoveryForm.vue",
-              message: `Нажал на кнопку "Получить код через ${ this.loginType === "phone" ? "номер" : "EMAIL"}"`,
-              timeUser: new Date(),
-            });
     },
 
     getCodeParams() {
@@ -574,14 +593,14 @@ export default {
       this.$emit("isPhoneChangedButtonClicked", this.isPhoneChanged);
       this.isSendCode = false;
       this.$LogEvent({
-          formName: "VerifyUser",
-          idEventType: this.loginType === "phone" ? 156 : 161,
-          controlName: "VerifyUser.vue",
-          message: `Нажал на кнопку "Изменить ${
-            this.loginType === "phone" ? "номер" : "EMAIL"
-          }"`,
-          timeUser: new Date(),
-        });
+        formName: "VerifyUser",
+        idEventType: this.loginType === "phone" ? 156 : 161,
+        controlName: "VerifyUser.vue",
+        message: `Нажал на кнопку "Изменить ${
+          this.loginType === "phone" ? "номер" : "EMAIL"
+        }"`,
+        timeUser: new Date(),
+      });
     },
 
     validateInput(field) {
@@ -625,12 +644,14 @@ export default {
         "В Личном кабинете отсутствует профиль с данным номером телефона"
       );
       this.$LogEvent({
-                formName: "VerifyUser errorMessage",
-                idEventType: this.loginType ? 155 : 162,
-                controlName: "VerifyUser.vue",
-                message: `Показало сообщение об ошибке на ${ this.loginType === "phone" ? "номере" : "EMAIL"}"`,
-                timeUser: new Date(),
-            });
+        formName: "VerifyUser errorMessage",
+        idEventType: this.loginType ? 155 : 162,
+        controlName: "VerifyUser.vue",
+        message: `Показало сообщение об ошибке на ${
+          this.loginType === "phone" ? "номере" : "EMAIL"
+        }"`,
+        timeUser: new Date(),
+      });
       const isMailExist = value.includes(
         "На указанный email отсутствует зарегистрированная уч.запись"
       );
