af83304942c81b28eec13278eb630973f4c726b6
Филимонова Виолетта <filvv@reso.ru>
2023-02-03 17:07:06 +0300

    "error toast with debug test in toast.helper.js"

--- components/Pages/Cabinet/Block/ActionButton.vue
+++ components/Pages/Cabinet/Block/ActionButton.vue
@@ -9,6 +9,7 @@
 </template>
 
 <script>
+import { getErrorMessage } from "../../../../plugins/auth/toast.helper";
 export default {
   name: "ActionButton",
   props: {
@@ -104,7 +105,13 @@ export default {
         });
         return result;
       } catch (err) {
-        console.error(err);
+        console.log(err.response);
+        getErrorMessage(err.response.data.MESSAGE);
+        this.$bvToast.toast(getErrorMessage(err.response.data.MESSAGE), {
+          title: "",
+          variant: "danger",
+          solid: true,
+        });
       }
       return null;
     },
@@ -145,7 +152,7 @@ export default {
     action: {
       get() {
         const action = this.actions.find(
-          (a) => a.ID === parseInt(this.actionId)
+          (a) => a.ID === parseInt(this.actionId, 10)
         );
         return action || null;
       },
