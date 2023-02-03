a6dfba7a7c0640193d8fa9733f5cbc1ca422f262
Филимонова Виолетта <filvv@reso.ru>
2023-02-03 14:44:34 +0300

    "function isBlackListOfRoute"

--- middleware/router.helper.js
+++ middleware/router.helper.js
@@ -1,23 +1,31 @@
-// const ROUTE_BLACK_LIST = [];
-// function createRouteBlackListItem(idModule, idParent) {
-//   return `/cabinet/${idModule}/${idParent}`;
-// }
-// export function isBlackListOfRoute(idModule, idParent) {
-//   if (
-//     (parseInt(idParent) > 912 && parseInt(idParent) < 918) ||
-//     (parseInt(idModule) !== 55 && !isNaN(parseInt(idModule)))
-//   ) {
-//     ROUTE_BLACK_LIST.push(createRouteBlackListItem(idModule, idParent));
-//     // return true;
-//   }
-//   console.log(ROUTE_BLACK_LIST);
-//   return ROUTE_BLACK_LIST;
-// }
+const BLACK_LIST_ROUTES = [
+  { idModule: 55, idParent: 913 },
+  { idModule: 55, idParent: 914 },
+  { idModule: 55, idParent: 915 },
+  { idModule: 55, idParent: 916 },
+  { idModule: 55, idParent: 917 },
+  { idModule: 55, idParent: 918 },
+];
+
+const WHITE_LIST_MODULES = [55];
+
 export function isBlackListOfRoute(idModule, idParent) {
-  if (
-    (parseInt(idParent) > 912 && parseInt(idParent) < 918) ||
-    (parseInt(idModule) !== 55 && !isNaN(parseInt(idModule)))
-  ) {
+  const blackListRoutes = BLACK_LIST_ROUTES.find((blackList) => {
+    if (
+      parseInt(idModule, 10) === blackList.idModule &&
+      parseInt(idParent, 10) === blackList.idParent
+    ) {
+      return true;
+    }
+    return false;
+  });
+  if (blackListRoutes) {
+    return true;
+  }
+  if (WHITE_LIST_MODULES.includes(parseInt(idModule, 10))) {
+    return false;
+  }
+  if (/^\d+$/.test(idModule)) {
     return true;
   }
   return false;
