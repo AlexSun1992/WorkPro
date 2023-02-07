fe9c512bbc9a61b4dbcdac6a7d968b09617414b0
Гаврилов Алексей <gavan@reso.ru>
2023-02-07 14:45:07 +0300

    дорабатываю тесты

--- components-vue2/src/components/Login/RegForm/RegForm.test.js
+++ components-vue2/src/components/Login/RegForm/RegForm.test.js
@@ -1016,7 +1016,102 @@ describe("RegForm", () => {
   });
 
   ///
-  it.only("Проверяем наличие всплывающего окна при регистрации", async () => {
+  // it.only("Проверяем наличие всплывающего окна при регистрации", async () => {
+  //   const localVue = createLocalVue();
+  //   localVue.use(BootstrapVue);
+  //   const wrapper = mount(RegForm, {
+  //     localVue,
+  //     mocks: {
+  //       $LogEvent: (v) => v,
+  //     },
+  //   });
+
+  //   const surnameComponent = wrapper.findComponent({
+  //     ref: "autocompleteSurname",
+  //   });
+  //   const surnameInput = surnameComponent.find("input");
+  //   await surnameInput.setValue("Казимиров");
+  //   //
+  //   const patronymicComponent = wrapper.findComponent({
+  //     ref: "autocompletePatronymic",
+  //   });
+  //   const patronymicInput = patronymicComponent.find("input");
+  //   await patronymicInput.setValue("Александрович");
+  //   //
+  //   const nameComponent = wrapper.findComponent({
+  //     ref: "autocompleteName",
+  //   });
+  //   const nameInput = nameComponent.find("input");
+  //   await nameInput.setValue("Андрей");
+
+  //   const dataPickerInput = wrapper
+  //     .findComponent("#birthday-picker")
+  //     .find("input");
+
+  //   dataPickerInput.setValue("27.06.1989");
+  //   dataPickerInput.trigger("change");
+
+  //   await wrapper.find("#password1").setValue("Carter911");
+  //   await wrapper.find("#password2").setValue("Carter911");
+  //   await wrapper.find("#phone").setValue("+7(985)-686-81-48");
+  //   await wrapper.find("#agreement-check-box").setChecked(true);
+  //   await wrapper.find("#btn_code_verification_lk").trigger("click");
+
+  //   expect(axios.post).toHaveBeenLastCalledWith(
+  //     "/am/free/v2/registerUser1",
+  //     {
+  //       BIRTHDATE: "1989-06-27",
+  //       FIRSTNAME: "Андрей",
+  //       GUID: null,
+  //       PASSWORD: "Carter911",
+  //       PASSWORD_CONFIRM: "Carter911",
+  //       PHONE: "+7(985)-686-81-48",
+  //       POLICY_NUMBER: "",
+  //       SECONDNAME: "Казимиров",
+  //       THIRDNAME: "Александрович",
+  //       USER_CONFIRM: "Y",
+  //       error: false,
+  //       loginType: "phone",
+  //       modeType: "REG",
+  //       token: 1,
+  //     },
+  //     { headers: { "X-Application": "VueJS", recaptcha: 1 } }
+  //   );
+
+  //   // console.log("wrapper:", wrapper.html());
+
+  //   // axios.post.mockReturnValue({
+  //   //   data: [
+  //   //     {
+  //   //       MESSAGE:
+  //   //         "Сейчас поступит входящий звонок. Прослушайте голосовое сообщение, введите код и нажмите на кнопку «Зарегистироваться»",
+  //   //       MESSAGE_CODE: 200,
+  //   //       GUID: "FBF58CA582F2078258408244C178422B",
+  //   //     },
+  //   //   ],
+  //   // });
+
+  //   axios.post.mockImplementationOnce(() =>
+  //     Promise.resolve({
+  //       data: [
+  //         {
+  //           MESSAGE:
+  //             "Сейчас поступит входящий звонок. Прослушайте голосовое сообщение, введите код и нажмите на кнопку «Зарегистироваться»",
+  //           MESSAGE_CODE: 200,
+  //           GUID: "FBF58CA582F2078258408244C178422B",
+  //         },
+  //       ],
+  //       status: 200,
+  //     })
+  //   );
+
+  //   await wrapper.vm.$nextTick();
+  //   await wrapper.vm.$nextTick();
+
+  //   await wrapper.find("#sms-confirm").setValue("11111");
+  // });
+
+  it.only("находим код подтверждения", async () => {
     const localVue = createLocalVue();
     localVue.use(BootstrapVue);
     const wrapper = mount(RegForm, {
@@ -1025,213 +1120,95 @@ describe("RegForm", () => {
         $LogEvent: (v) => v,
       },
     });
+    axios.post.mockReturnValue({
+      data: [
+        {
+          MESSAGE: "Введите код подтверждения из SMS",
+          MESSAGE_CODE: 200,
+          GUID: "68A6B6024E3C03B39C9BFDC78D5E235B",
+        },
+      ],
+    });
+
+    expect(wrapper.findComponent("#sms-confirm").exists()).toBe(false);
+
+    await wrapper.find("#phone").setValue("+7(910)-123-22-33");
+
+    expect(
+      wrapper.find("#btn_code_verification_lk").attributes().disabled
+    ).toBeDefined();
 
     const surnameComponent = wrapper.findComponent({
       ref: "autocompleteSurname",
     });
     const surnameInput = surnameComponent.find("input");
-    await surnameInput.setValue("Казимиров");
-    //
+    await surnameInput.setValue("П");
+    expect(surnameComponent.classes()).toContain("is-valid");
+
     const patronymicComponent = wrapper.findComponent({
       ref: "autocompletePatronymic",
     });
     const patronymicInput = patronymicComponent.find("input");
-    await patronymicInput.setValue("Александрович");
-    //
+    await patronymicInput.setValue("П");
+
     const nameComponent = wrapper.findComponent({
       ref: "autocompleteName",
     });
     const nameInput = nameComponent.find("input");
-    await nameInput.setValue("Андрей");
+    await nameInput.setValue("П");
+
+    const checkboxComponent = wrapper.findComponent("#policy-exist-check-box");
+
+    expect(
+      wrapper.findComponent({ ref: "policyNumber" }).attributes().disabled
+    ).toBeDefined();
+
+    await checkboxComponent.setChecked(true);
+
+    expect(
+      wrapper.findComponent({ ref: "policyNumber" }).attributes().disabled
+    ).not.toBeDefined();
+
+    wrapper.findComponent({ ref: "policyNumber" }).setValue("12345");
 
     const dataPickerInput = wrapper
       .findComponent("#birthday-picker")
       .find("input");
 
-    dataPickerInput.setValue("27.06.1989");
+    dataPickerInput.setValue("21.12.2052");
     dataPickerInput.trigger("change");
-
-    await wrapper.find("#password1").setValue("Carter911");
-    await wrapper.find("#password2").setValue("Carter911");
-    await wrapper.find("#phone").setValue("+7(985)-686-81-48");
-    await wrapper.find("#agreement-check-box").setChecked(true);
-    await wrapper.find("#btn_code_verification_lk").trigger("click");
-
-    expect(axios.post).toHaveBeenLastCalledWith(
-      "/am/free/v2/registerUser1",
-      {
-        BIRTHDATE: "1989-06-27",
-        FIRSTNAME: "Андрей",
-        GUID: null,
-        PASSWORD: "Carter911",
-        PASSWORD_CONFIRM: "Carter911",
-        PHONE: "+7(985)-686-81-48",
-        POLICY_NUMBER: "",
-        SECONDNAME: "Казимиров",
-        THIRDNAME: "Александрович",
-        USER_CONFIRM: "Y",
-        error: false,
-        loginType: "phone",
-        modeType: "REG",
-        token: 1,
-      },
-      { headers: { "X-Application": "VueJS", recaptcha: 1 } }
-    );
-
-    // console.log("wrapper:", wrapper.html());
-
-    // axios.post.mockReturnValue({
-    //   data: [
-    //     {
-    //       MESSAGE:
-    //         "Сейчас поступит входящий звонок. Прослушайте голосовое сообщение, введите код и нажмите на кнопку «Зарегистироваться»",
-    //       MESSAGE_CODE: 200,
-    //       GUID: "FBF58CA582F2078258408244C178422B",
-    //     },
-    //   ],
-    // });
-
-    axios.post.mockImplementationOnce(() =>
-      Promise.resolve({
-        data: [
-          {
-            MESSAGE:
-              "Сейчас поступит входящий звонок. Прослушайте голосовое сообщение, введите код и нажмите на кнопку «Зарегистироваться»",
-            MESSAGE_CODE: 200,
-            GUID: "FBF58CA582F2078258408244C178422B",
-          },
-        ],
-        status: 200,
-      })
-    );
-
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-    await wrapper.vm.$nextTick();
-
-    await wrapper.find("#sms-confirm").setValue("11111");
-
-    // console.log("wrapper:", wrapper.html());
-
-    // axios.post.mockReturnValue({
-    //   data: [{ MESSAGE_CODE: 201 }],
-    // });
-
-    // await wrapper.vm.$nextTick();
-    // await wrapper.vm.$nextTick();
-
-    // const verifyUser = wrapper.findComponent({ ref: "verifyUser" });
-    // const spy = jest.spyOn(verifyUser.vm.$bvModal, "msgBoxConfirm");
-    // spy.mockImplementationOnce(() => Promise.resolve(true));
-
-    // Object.defineProperty(window, "location", {
-    //   value: {
-    //     href: "/",
-    //     pathname: "/",
-    //   },
-    // });
-
-    ///
-
-    // expect(wrapper.findComponent("#sms-confirm").exists()).toBe(false);
-
-    // ;
-
-    // expect(surnameComponent.classes()).toContain("is-valid");
-
-    // const checkboxComponent = wrapper.findComponent("#policy-exist-check-box");
-
-    // expect(
-    //   wrapper.findComponent({ ref: "policyNumber" }).attributes().disabled
-    // ).toBeDefined();
-
-    // await checkboxComponent.setChecked(true);
-
-    // expect(
-    //   wrapper.findComponent({ ref: "policyNumber" }).attributes().disabled
-    // ).not.toBeDefined();
-
-    // wrapper.findComponent({ ref: "policyNumber" }).setValue("12345");
-
-    // dataPickerInput.setValue("21.12.2052");
-    // dataPickerInput.trigger("change");
     // await wrapper.findComponent({ ref: "policyNumber" }).trigger("focus");
     // expect(dataPickerInput.classes()).not.toContain("is-valid");
 
-    // dataPickerInput.setValue("21.12.1852");
-    // dataPickerInput.trigger("change");
+    dataPickerInput.setValue("21.12.1852");
+    dataPickerInput.trigger("change");
     // await wrapper.findComponent({ ref: "policyNumber" }).trigger("focus");
     // expect(dataPickerInput.classes()).not.toContain("is-valid");
 
-    // dataPickerInput.setValue("21.12.2022");
-    // dataPickerInput.trigger("change");
+    dataPickerInput.setValue("21.12.2022");
+    dataPickerInput.trigger("change");
     // await wrapper.findComponent({ ref: "policyNumber" }).trigger("focus");
     // expect(dataPickerInput.classes()).toContain("is-valid");
 
-    //
+    await wrapper.find("#password1").setValue("Aa1234");
+    expect(wrapper.find("#password1").classes()).toContain("is-valid");
 
-    // await wrapper.find("#btn_code_verification_lk").trigger("click");
+    await wrapper.find("#password2").setValue("Aa1234");
+    expect(wrapper.find("#password2").classes()).toContain("is-valid");
 
-    // await wrapper.vm.$nextTick();
-    // await wrapper.vm.$nextTick();
+    await wrapper.find("#agreement-check-box").setChecked(true);
 
-    // expect(wrapper.findComponent("#sms-confirm").exists()).toBe(false);
-    // expect(spy).toHaveBeenCalled();
-    // expect(window.location.href).toEqual("/login/password-recovery");
+    expect(
+      wrapper.find("#btn_code_verification_lk").attributes().disabled
+    ).not.toBeDefined();
 
-    // spy.mockImplementationOnce(() => Promise.resolve(false));
+    await wrapper.find("#btn_code_verification_lk").trigger("click");
 
-    //
+    await wrapper.vm.$nextTick();
+    await wrapper.vm.$nextTick();
 
-    // await wrapper.vm.$nextTick();
-    // await wrapper.vm.$nextTick();
+    console.log("wrapper:", wrapper.findComponent("#sms-confirm").html());
 
-    // expect(wrapper.findComponent("#sms-confirm").exists()).toBe(false);
-
-    // expect(spy).toHaveBeenCalled();
-    // expect(window.location.href).toEqual("/login");
-
-    // spy.mockImplementationOnce(() => Promise.resolve(null));
-
-    // await wrapper.find("#btn_code_verification_lk").trigger("click");
-
-    // await wrapper.vm.$nextTick();
-    // await wrapper.vm.$nextTick();
-
-    // expect(wrapper.findComponent("#sms-confirm").exists()).toBe(false);
-    // expect(spy).toHaveBeenCalled();
-    // expect(wrapper.find("#phone").element.value).toBe("");
+    expect(wrapper.findComponent("#sms-confirm").exists()).toBe(true);
   });
 });
