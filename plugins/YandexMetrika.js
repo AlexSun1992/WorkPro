ef806b59c0665d3e22193e1cf311350684686034
Филимонова Виолетта <filvv@reso.ru>
2023-02-20 16:38:52 +0300

    "vue-yandex-metrika"

--- plugins/YandexMetrika.js
+++ plugins/YandexMetrika.js
@@ -1,16 +1,16 @@
-// import Vue from "vue";
-// import VueYandexMetrika from "vue-yandex-metrika";
+import Vue from "vue";
+import VueYandexMetrika from "vue-yandex-metrika";
 
-// Vue.use(VueYandexMetrika, {
-//   id: "25356824",
-//   env: process.env.NODE_ENV,
-//   debug: false,
-//   options: {
-//     webvisor: true,
-//     clickmap: true,
-//     useCDN: false,
-//     trackLinks: true,
-//     accurateTrackBounce: true,
-//     ecommerce: "dataLayer",
-//   },
-// });
+Vue.use(VueYandexMetrika, {
+  id: "25356824",
+  env: process.env.NODE_ENV,
+  debug: false,
+  options: {
+    webvisor: true,
+    clickmap: true,
+    useCDN: false,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    ecommerce: "dataLayer",
+  },
+});
