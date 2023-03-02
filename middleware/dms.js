1c36754126488dc07c06505a2f455103d7bcb354
IONKA <ionka@reso.ru>
2023-03-02 09:56:08 +0300

    [LK2-790] Добавлены переходы в тестовую ДМС.

--- middleware/dms.js
+++ middleware/dms.js
@@ -1,8 +1,9 @@
 export default async function ({ $auth, redirect }) {
-  const getToken = await $auth.refreshTokens();
-  const { ACCESS_TOKEN } = getToken.data;
-
-  const url = `https://testtelemed.reso.ru/DMSResoRu/login?token=${ACCESS_TOKEN}`;
-
-  return redirect(`${url}`);
+  if ($auth.loggedIn) {
+    const getToken = await $auth.refreshTokens();
+    const { ACCESS_TOKEN } = getToken.data;
+    const url = `https://testtelemed.reso.ru/DMSResoRu/login?token=${ACCESS_TOKEN}`;
+    return redirect(`${url}`);
+  }
+  return redirect("https://testtelemed.reso.ru/DMSResoRu");
 }
--- middleware/telemed.js
+++ middleware/telemed.js
@@ -1,8 +1,10 @@
 export default async function ({ $auth, redirect }) {
-  const getToken = await $auth.refreshTokens();
-  const { ACCESS_TOKEN } = getToken.data;
-
-  const url = `https://telemed.reso.ru/DMSResoRu/reso_iframe?token=${ACCESS_TOKEN}`;
-
-  return redirect(`${url}`);
+  if ($auth.loggedIn) {
+    const getToken = await $auth.refreshTokens();
+    const { ACCESS_TOKEN } = getToken.data;
+    const url = `https://testtelemed.reso.ru/DMSResoRu/reso_iframe?token=${ACCESS_TOKEN}`;
+    return redirect(`${url}`);
+  }
+  return redirect("https://testtelemed.reso.ru/DMSResoRu");
 }
+
