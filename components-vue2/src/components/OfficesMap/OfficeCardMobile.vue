626229841efb713e445a01f8fb11a9ab29b5ef88
Головлев Вячеслав <golvi@reso.ru>
2023-03-21 11:41:06 +0300

    Правки в отображение тэга в баблах карты, добавлен чекед инпут дизаблед

--- components-vue2/src/components/OfficesMap/OfficeCardMobile.vue
+++ components-vue2/src/components/OfficesMap/OfficeCardMobile.vue
@@ -53,6 +53,14 @@
                 </button>
               </div>
             </div>
+            <div class="mt-2">
+              <div
+                class="green-tags"
+                v-if="office.LSPR === true || office.LREG_CENTER === true"
+              >
+                Урегулирование страховых случаев
+              </div>
+            </div>
             <div v-if="office.station" class="card-office-undeground">
               <div>
                 <span
@@ -136,5 +144,18 @@ export default {
 .name {
   display: flex;
   justify-content: space-between;
+}
+.green-tags {
+  font-family: "SF Pro Display";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 0.875rem;
+  line-height: 20px;
+  align-items: center;
+  color: #ffffff;
+  padding: 4px 12px;
+  background: #43b02a;
+  border-radius: 100px;
+  display: table;
 }
 </style>
