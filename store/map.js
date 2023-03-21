d2cfb5413a34b2f9c26de4b610a701358c6ba218
SHESM <shesm@reso.ru>
2023-03-21 12:15:21 +0300

    [RESOSITE-948] Исправлена процедура выборки офисов из базы

--- store/map.js
+++ store/map.js
@@ -12,7 +12,7 @@ export const actions = {
     try {
       commit("setLoading", true);
       const { data } = await this.$axios.get(
-        `/am/free/v2/siteapi/std/agencies/list?lat=${params.coords[0]}&long=${params.coords[1]}${params.id ? ('&idregion=').concat(params.id) : ''}`
+        `/am/free/v2/siteapi/agencies?lat=${params.coords[0]}&long=${params.coords[1]}${params.id ? ('&idregion=').concat(params.id) : ''}`
       );
       commit("setLoading", false);
       commit("setRegionOffices", data);
