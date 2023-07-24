27ba77fe144329f1cd514b582605ba317ed012a1
Головлев Вячеслав <golvi@reso.ru>
2023-07-24 11:51:59 +0300

    правим тесты

--- components/Libs/Controls/ControlRange/ControlRange.vue
+++ components/Libs/Controls/ControlRange/ControlRange.vue
@@ -2,7 +2,9 @@
   <div>
     <label v-if="data.label">
       <span
-        >{{ data.label }}&nbsp;&nbsp;<span class="phb2">{{ insuredSum }}</span>
+        >{{ data.label }}&nbsp;&nbsp;<span class="phb2" id="isuredSum">{{
+          insuredSum
+        }}</span>
         <span v-if="data.helpText" class="tooltipster">
           (?)<vue-easy-tooltip :with-arrow="true" position="top" :offset="4">
             <span v-html="data.helpText" /></vue-easy-tooltip
