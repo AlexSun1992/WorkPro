4a579b736a5cff3fb94228ce24d83bbbfddbe692
Ионов Кирилл <ionka@reso.ru>
2023-07-26 12:19:11 +0300

    [LK2-896] Отключена передача параметров формы в url при запросе списков.

--- api/list.js
+++ api/list.js
@@ -90,6 +90,62 @@ router.get("/list/:idModule/:idItem/:filters", async (req, res, next) => {
   }
 });
 
+router.post("/list/:idModule/:idItem", async (req, res, next) => {
+  try {
+    let mobile2ServiceInstance = mobile2Service();
+    const ipAddress = requestIp.getClientIp(req);
+    mobile2ServiceInstance.defaults.headers.common["x-forwarded-for"] =
+      ipAddress || null;
+    if (req.headers.referer) {
+      mobile2ServiceInstance.defaults.headers.common.Referer =
+        req.headers.referer;
+    }
+    mobile2ServiceInstance.defaults.headers.common["user-agent"] =
+      req.headers["user-agent"];
+    let URL_ADDRESS;
+    let settings = null;
+    mobile2ServiceInstance.defaults.headers.common.Authorization = null;
+    mobile2ServiceInstance.defaults.headers.common["Cookie"] = req.headers
+      ?.cookie
+      ? req.headers.cookie
+      : null;
+    if (req?.query.zone !== "free") {
+      if (req?.headers?.authorization) {
+        mobile2ServiceInstance.defaults.headers.common.Authorization =
+          req.headers.authorization;
+      } else {
+        if (req?.cookies["auth._token.local"]) {
+          mobile2ServiceInstance.defaults.headers.common.Authorization =
+            req?.cookies["auth._token.local"];
+        }
+      }
+      const URLFilters = encodeURIComponent(JSON.stringify(req.body ?? {}));
+      URL_ADDRESS = `${consts.DATA}/${req.params.idModule}/${req.params.idItem}?json=${URLFilters}`;
+      settings = await mobile2ServiceInstance.get(
+        `${consts.CLIENTMENU}/${req.params.idModule}/${req.params.idItem}`
+      );
+    } else {
+      URL_ADDRESS = `${consts.FREEDATA}/${req.params.idModule}/${req.params.idItem}/0/0`;
+    }
+    const list = await mobile2ServiceInstance.get(URL_ADDRESS);
+    res.send({
+      ...listConverter.list(list.data),
+      settings: settings?.data[0],
+      subSettings: settings
+        ? menuConverter.menuObject(settings?.data[0])
+        : undefined,
+    });
+  } catch (err) {
+    if (err?.response?.data.STATUS == 401) {
+      res.status(err.response.data.STATUS).send(err.response.data);
+    } else {
+      res
+        .status(err?.response?.data.STATUS || 500)
+        .send(err?.response?.data || err);
+    }
+  }
+});
+
 router.get("/onetomanylist/:idItem/:id/:rel", (req, res) => {
   try {
     const mobile2ServiceInstance = mobile2Service();
