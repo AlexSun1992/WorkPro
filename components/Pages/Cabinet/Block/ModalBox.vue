109ca1be5a3a3781ac57361295cc70b49cf664d2
Гаврилов Алексей <gavan@reso.ru>
2023-10-26 18:04:50 +0300

    настроен компонент ModalBox

--- components/Pages/Cabinet/Block/ModalBox.vue
+++ components/Pages/Cabinet/Block/ModalBox.vue
@@ -1,10 +1,41 @@
 <template>
   <div>
-    <p>ComponentModalBox</p>
+    <slot name="card"></slot>
+    <slot :isCardHidden="isCardHidden"></slot>
   </div>
 </template>
 <script>
 export default {
   name: "ModalBox",
+  props: {
+    element: {
+      type: String,
+      required: true,
+      default: () => null,
+    },
+    btnClose: {
+      type: String,
+      required: true,
+      default: () => null,
+    },
+  },
+  data() {
+    return {
+      isCardHidden: true,
+    };
+  },
+
+  mounted() {
+    const $el = document.querySelector(`.${this.element}`);
+    const btnHideWindow = document.querySelector(`.${this.btnClose}`);
+
+    $el.addEventListener("click", () => {
+      this.isCardHidden = !this.isCardHidden;
+    });
+
+    btnHideWindow.addEventListener("click", () => {
+      this.isCardHidden = !this.isCardHidden;
+    });
+  },
 };
 </script>
