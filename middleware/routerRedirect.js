2b5265c031f7d19e391fdec20ba92be3813e81db
Подшибякин Никита <podns@reso.ru>
2024-09-30 14:00:49 +0000

    LK2-1283: [f] fix old calls doctor redirect

--- middleware/routerRedirect.js
+++ middleware/routerRedirect.js
@@ -1,12 +1,32 @@
-export default function defineNuxtRouteMiddleware({ route, redirect, $auth }) {
+function getRedirectUrl(oldRelativePath, newRelativePath) {
   if (
-    (route.params.idModule === "55" &&
-      route.params.idParent === "0" &&
-      route.params.idItem === "782" &&
-      $auth.$state.user.ID !== 1484278 &&
-      $auth.$state.user.ID !== 1781030) ||
-    route.fullPath === "/cabinet/55/0/901/0"
+    oldRelativePath !== undefined &&
+    oldRelativePath !== null &&
+    newRelativePath !== undefined &&
+    newRelativePath !== null
   ) {
-    redirect("/cabinet/55/0/871");
+    const originalUrlWithFakeHostName = new URL(
+      oldRelativePath,
+      "https://reso.ru"
+    );
+    const resultUrl = newRelativePath.concat(
+      originalUrlWithFakeHostName.search.toString()
+    );
+    return resultUrl;
+  }
+  throw new Error("Url отсутвует!");
+}
+
+export default function defineNuxtRouteMiddleware({ route, redirect, $auth }) {
+  try {
+    if (
+      (route.params.idItem === "782" || route.params.idItem === "901") &&
+      $auth.$state.user.ID !== 1484278 &&
+      $auth.$state.user.ID !== 1781030
+    ) {
+      redirect(getRedirectUrl(route.fullPath, "/cabinet/55/0/871"));
+    }
+  } catch (error) {
+    console.error("Произошла ошибка при редиректе: ".concat(error));
   }
 }
