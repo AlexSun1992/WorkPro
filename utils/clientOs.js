7ce3cbffe7c38492308256113ec786b65820e958
Андрей Соцков <sotaa@reso.ru>
2024-11-15 15:30:11 +0300

    LK2-1364 Add getMobilePlatform for clientOs util, Refactor

--- utils/clientOs.js
+++ utils/clientOs.js
@@ -1,23 +1,44 @@
-const defaultPlatfrm = 'unknown';
+const defaultPlatform = 'unknown';
 
 function getFromNavigation() {
   const userAgent = navigator?.userAgentData
 
-  return userAgent?.platform ? navigator.platform : defaultPlatfrm;
+  return userAgent?.platform ? navigator.platform : defaultPlatform;
+}
+
+function getFromNavigator() {
+  const platform = navigator?.platform;
+
+  return platform ?? defaultPlatform;
 }
 
 export const clientOS = {
-  get() {
-    const getters = [getFromNavigation]
+  getPlatform() {
+    const getters = [getFromNavigation, getFromNavigator];
 
     for (const item of getters) {
       const val =  item();
 
-      if (val !== defaultPlatfrm) {
+      if (val !== defaultPlatform) {
         return val;
       }
     }
 
-    return defaultPlatfrm;
+    return defaultPlatform;
+  },
+
+  getMobilePlatform() {
+    const mainPlatforms = {'Android': ['Android'], IOS: ['iPad', 'iPhone']};
+    const platform = clientOS.getPlatform();
+
+    for(const [key, values] of Object.entries(mainPlatforms)) {
+      const variant = values.find(item => item.toLowerCase().includes(platform.toLowerCase()));
+
+      if (variant) {
+        return key;
+      }
+    }
+
+    return '';
   }
 }
