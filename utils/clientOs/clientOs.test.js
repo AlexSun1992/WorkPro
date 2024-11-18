5378dbebcddc82e8180f8694eb697182ed4d97e3
Андрей Соцков <sotaa@reso.ru>
2024-11-18 16:11:25 +0300

    LK2-1364 Fix run tests, Add test about empty platform, Refactor

--- utils/clientOs/clientOs.test.js
+++ utils/clientOs/clientOs.test.js
@@ -2,37 +2,38 @@ import { clientOS } from "./clientOs";
 import { clientOsTestData } from "./clientOsTestData";
 
 describe("clientOs utils", () => {
-  let windowSpy;
   let mock;
 
   beforeEach(() => {
-    // windowSpy = jest.spyOn(global, "navigator", "get");
-    jest.resetAllMocks();
+    Object.defineProperty(navigator, 'userAgentData', { value: {}, writable: true, configurable: true });
+    Object.defineProperty(navigator, 'userAgent', { value: "", writable: true, configurable: true });
   });
 
-  /* afterEach(() => {
-    windowSpy.mockRestore();
-  }); */
-
-  test("getPlatform", () => {
+  test("getPlatform for Windows", () => {
     mock = clientOsTestData.variantA;
-    Object.defineProperty(navigator, 'userAgentData', {value: mock.navigator.userAgentData, writable: true });
-    /* windowSpy.mockImplementation(() => mock.navigator); */
-    console.log(navigator.userAgetnData);
+    Object.defineProperty(navigator, 'userAgentData', { value: mock.navigator.userAgentData, writable: true });
+
     expect(clientOS.getPlatform()).toBe('Windows');
   });
 
   test("getMobilePlatform for Android", () => {
     mock = clientOsTestData.variantB;
-    Object.defineProperty(navigator, 'userAgent', {value: mock.navigator.userAgent, writable: true });
+    Object.defineProperty(navigator, 'userAgent', { value: mock.navigator.userAgent, writable: true });
 
     expect(clientOS.getMobilePlatform()).toBe('Android');
   });
 
   test("getMobilePlatform for Iphone", () => {
     mock = clientOsTestData.variantC;
-    Object.defineProperty(navigator, 'userAgent', {value: mock.navigator.userAgent, writable: true });
+    Object.defineProperty(navigator, 'userAgent', { value: mock.navigator.userAgent, writable: true });
 
     expect(clientOS.getMobilePlatform()).toBe('IOS');
+  });
+
+  test("getMobilePlatform for empty", () => {
+    mock = clientOsTestData.variantD;
+    Object.defineProperty(navigator, 'userAgent', { value: mock.navigator.userAgent, writable: true });
+
+    expect(clientOS.getMobilePlatform()).toBe("");
   });
 });
