e4dae12efbf3b541313b6422bb64994ebba446cc
Андрей Соцков <sotaa@reso.ru>
2024-11-18 15:18:19 +0300

    LK2-1364 Fix getMobilePlatform method logic, Add tests

--- utils/clientOs/clientOsTestData.js
+++ utils/clientOs/clientOsTestData.js
@@ -20,8 +20,19 @@ export const clientOsTestData = {
         "platform": "Windows"
       },
       userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
-      platform: "Win32"
     }
 
+  },
+  variantB: {
+    navigator: {
+      userAgentData: "",
+      userAgent: "Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36",
+    }
+  },
+  variantC: {
+    navigator: {
+      userAgentData: "",
+      userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+    }
   }
 }
