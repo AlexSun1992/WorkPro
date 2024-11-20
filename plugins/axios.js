ccd1c01b806c52b1e23745499f2a8abb7dd08a0b
Андрей Соцков <sotaa@reso.ru>
2024-11-20 14:27:40 +0300

    LK2-1364 Remove x-os from axios plugin

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,16 +1,1 @@
-import { clientOS } from "../utils/clientOs/clientOs";
-
-export default function (param) {
-  let platform = "VueJs";
-  const utils = clientOS;
-
-  param.$axios.onRequest(config => {
-    try {
-      platform = utils.getMobilePlatform();
-    } catch (err) {
-      platform = "VueJs";
-    } finally {
-      config.headers.common["X-OS"] = platform;
-    }
-  });
-}
+export default function () {}
