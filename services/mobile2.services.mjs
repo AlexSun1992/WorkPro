a0f0fc6ab2bceca3632f5dc7b756fad1283c5798
Андрей Соцков <sotaa@reso.ru>
2024-11-21 17:20:16 +0300

    LK2-1364 Add catch for method

--- services/mobile2.services.mjs
+++ services/mobile2.services.mjs
@@ -26,6 +26,8 @@ export const mobile2Service = (url) => {
 
   instance.interceptors.response.use(response => {
     console.log(response.config);
+  }, (err) => {
+    return err;
   });
 
   return instance;
