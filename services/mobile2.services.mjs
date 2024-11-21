cf06ebd52a37e338b3fca96371ba4efd9a38eb75
Андрей Соцков <sotaa@reso.ru>
2024-11-21 17:16:08 +0300

    LK2-1364 Add debug method

--- services/mobile2.services.mjs
+++ services/mobile2.services.mjs
@@ -8,16 +8,24 @@ export const mobile2Service = (url) => {
   instance.defaults.baseURL =
     url || process.env.MOBILE2_URL || "https://lk.reso.ru";
   instance.defaults.headers.common["X-Application"] = "VueJS";
-  instance.defaults.headers.common["X-Os"] = "";
+  instance.defaults.headers.common["X-Os"] = "VueJS";
 
   instance.interceptors.request.use(config => {
     const newConfig = Object.assign({}, config);
     const userAgent = newConfig.headers.common["user-agent"];
+    let platform;
 
-    userAgent &&
-    (newConfig.headers["X-Os"] = clientOs.getMobilePlatform(userAgent));
+    if (userAgent) {
+      platform = clientOs.getMobilePlatform(userAgent);
+      newConfig.headers["X-Os"] = platform;
+      newConfig.headers.common["X-Os"] = platform;
+    }
 
     return newConfig;
+  });
+
+  instance.interceptors.response.use(response => {
+    console.log(response.config);
   });
 
   return instance;
