88bc143d69320d0cceaea32f18614eeab87e4406
Андрей Соцков <sotaa@reso.ru>
2024-12-23 11:31:10 +0300

    LK2-1417 Add SearchBox component

---
+++ components/Libs/Controls/ControlDropdown/SearchBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text"
+           @input="$emit('input', $event)">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBox",
+  props: {
+    value: {
+      default: ""
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
