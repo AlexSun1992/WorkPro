f126e78d4a58870ee6060bb6390dde5764106da4
Андрей Соцков <sotaa@reso.ru>
2025-01-22 21:44:10 +0300

    LK2-1364 Refactor remove console.log

--- services/mobile2.services.mjs
+++ services/mobile2.services.mjs
@@ -28,8 +28,6 @@ export const mobile2Service = (url) => {
   });
 
   instance.interceptors.response.use(response => {
-    console.log(response.config);
-
     return response;
   }, (err) => {
     return err;
