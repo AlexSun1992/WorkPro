ff010d35f07ae523bf1856a0614b1cc3e5651808
Андрей Соцков <sotaa@reso.ru>
2025-01-23 14:48:05 +0300

    LK2-1364 Remove device-detector-js. Add new json data, Update clientOsPlatform logic

--- utils/clientOs/clientOs.mjs
+++ utils/clientOs/clientOs.mjs
@@ -1,33 +1,47 @@
-import DeviceDetector from "device-detector-js";
+import clientOsData from "./clientOs.json";
+import clientOsPlatforms from "./clientOsPlatforms.json"
 
 export const clientOs = {
-  deviceDetector: new DeviceDetector(),
   /**
    *
    * @param userAgent {string} - либо передать вызов метода clientOs.getPlatform() который должен быть выполнент на клиенте
-   * @returns {string}
+   * @returns {{isWebview: string, platform: string}}
    */
-  isWebview: null,
-  platformData: null,
   getMobilePlatform(userAgent) {
-    const mobilePlatforms = ["smartphone", "tablet", "phablet", "feature phone"]
+    const mobilePlatforms = [ "smartphone", "tablet", "phablet", "feature phone" ];
     const result = { isWebview: "", platform: "" };
-    const platformData = this.deviceDetector.parse(userAgent);
-    const platformOs = platformData.os?.name;
-
-    if (Boolean(platformData?.client)) {
-      result.isWebview = mobilePlatforms.includes(platformData.device?.type.toLowerCase()) ? "isWebview" : "";
-      result.platform = result.isWebview ? this.getOsPlatform(platformOs) : "";
+    // const platformData = this.getOsInfo(userAgent);
+    // const platformOs = platformData.os?.name;
+    const platformOs = this.getOsInfo(userAgent);
+    console.log(`********** PlatformOs ${ platformOs }`);
+    if (Boolean(platformOs)) {
+      result.platform = this.getOsPlatform(platformOs);
+      result.isWebview = this.isWebview(platformOs) ? "isWebview" : "";
     }
 
     return result;
   },
 
   getOsPlatform(platformOs) {
-    if (Boolean(platformOs) === false) {
-      return -1;
-    }
     // 7 - IOS; 8 - Android
-    return platformOs.toLowerCase() === "ios" ? 7 : 8;
+    if (Boolean(platformOs) && clientOsPlatforms.android.includes(platformOs)) {
+      return 8;
+    }
+
+    if (Boolean(platformOs) && clientOsPlatforms.ios.includes(platformOs)) {
+      return 7;
+    }
+
+    return "";
+  },
+
+  getOsInfo(userAgent = "") {
+    const currentOs = clientOsData.find(item => userAgent.search(item.regex) >= 0);
+
+    return currentOs?.name ?? "";
+  },
+
+  isWebview(os) {
+    return clientOsPlatforms.android.includes(os) || clientOsPlatforms.ios.includes(os);
   }
 }
