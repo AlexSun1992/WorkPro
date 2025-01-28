654d8229353ed84fd3d40cc8c5f0db1a4ea7f107
Андрей Соцков <sotaa@reso.ru>
2025-01-28 10:43:25 +0300

    LK2-1417 Add search position type

---
+++ components/Libs/Controls/ControlTokenBox/searchBoxType.js
@@ -0,0 +1,10 @@
+/*
+ Область с выбранными элементами.
+ inline - Поиск размещается первым элементом
+ inlineReverse - Поиск размещается последним элементом
+ Выпадающий список
+ dropdown - Поиск размещается первым элементом
+ dropdownReverse - Поиск размещается последним элементом
+ */
+
+export default ["inline", "inlineReverse", "dropdown", "dropdownReverse"];
