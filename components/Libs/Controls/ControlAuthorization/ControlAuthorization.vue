31d079ec9ab135c3940006b049fc334bfd731e67
Андрей Соцков <sotaa@reso.ru>
2025-02-06 14:23:59 +0300

    LK2-1480 Add ControlAuthorization component, Add new component to components list

---
+++ components/Libs/Controls/ControlAuthorization/ControlAuthorization.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <button type="button" @click="showModal">Авторизоваться</button>
+
+    <b-modal
+      id="sms-auth-confirm-modal"
+      v-model="isModalVisible"
+      hide-footer
+      :centered="true"
+      :static="true"
+      content-class="sms-confirm-modal"
+      title="Введите код"
+    >
+      <form :action="submitForm">
+
+        <input type="text">
+
+        <input type="text">
+
+        <button type="button"></button>
+      </form>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "ControlAuthorization",
+  components: {  },
+  data: () => ({
+    isModalVisible: false,
+  }),
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    hideModal() {
+      this.isModalVisible = false;
+    },
+    submitForm() {
+
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
