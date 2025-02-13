b4e6fc5139376223731ffe4f2e4e7bf8b8940415
Андрей Соцков <sotaa@reso.ru>
2025-02-13 12:44:01 +0300

    LK2-1480 Add tests about form reset and invalid phoneNumber

--- components/Libs/Controls/ControlAuthorization/controlAuthorization.test.js
+++ components/Libs/Controls/ControlAuthorization/controlAuthorization.test.js
@@ -1,5 +1,6 @@
 import { mount } from "@vue/test-utils";
 import ControlAuthorization from "~/components/Libs/Controls/ControlAuthorization/ControlAuthorization.vue";
+import controlAuthorizationTestData from "~/components/Libs/Controls/ControlAuthorization/controlAuthorizationTestData";
 
 describe("ControlAuthorization", () => {
   let wrapper;
@@ -9,10 +10,37 @@ describe("ControlAuthorization", () => {
 
     wrapper.vm.showModal();
     wrapper.vm.$nextTick(() => {
-      wrapper.setData({ phoneNumber: "89031234567" });
+      wrapper.setData({
+        phoneNumber: controlAuthorizationTestData.validPhoneNumber,
+      });
 
       expect(wrapper.vm.isPhoneValid).toBeTruthy();
       expect(wrapper.contains("#phoneNumber").classList).toBe("is-valid");
     });
+  });
+
+  test("is phoneNumber Invalid", () => {
+    wrapper = mount(ControlAuthorization, {});
+
+    wrapper.vm.showModal();
+    wrapper.vm.$nextTick(() => {
+      wrapper.setData({ phoneNumber: "" });
+
+      expect(wrapper.vm.isPhoneValid).toBeFalsy();
+      expect(wrapper.contains("#phoneNumber").classList).toBe("is-invalid");
+    });
+  });
+
+  test("is form data reset", () => {
+    wrapper = mount(ControlAuthorization, {});
+
+    wrapper.vm.showModal();
+    wrapper.setData({
+      phoneNumber: controlAuthorizationTestData.validPhoneNumber,
+    });
+
+    wrapper.vm.resetForm();
+
+    expect(wrapper.vm.phoneNumber).toBe("");
   });
 });
