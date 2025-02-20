c9b24660cb92793ca0fedc1e08dd613d37f72aef
Андрей Соцков <sotaa@reso.ru>
2025-02-20 17:34:22 +0300

    LK2-1497 Remove unused config prop with logic

--- utils/clientOs/clientOs.mjs
+++ utils/clientOs/clientOs.mjs
@@ -4,60 +4,18 @@ import { OS_TYPES, WEBVIEW_TYPES } from "./clientOsConstants.mjs";
 
 export default {
   updateMobileViewConfig(config) {
-    const webviewData = this.getWebviewData(config);
+    const cookies = config?.headers.common.Cookie ?? "";
     const newConfig = { ...config };
 
-    newConfig.headers["X-DEV"] = webviewData.platform;
-    newConfig.headers["X-Application"] = webviewData.webview;
+    newConfig.headers["X-Application"] = this.getWebview(cookies);
 
     return newConfig;
   },
-  getWebviewData(config) {
-    const userAgent = config?.headers.common["user-agent"] ?? "";
-    const cookies = config?.headers.common.Cookie ?? "";
-    const result = { platform: OS_TYPES.default, webview: WEBVIEW_TYPES.VueJS };
+
+  getWebview(cookies) {
     const isWebview = this.isWebview(cookies);
 
-    result.webview = this.getWebview(isWebview, cookies);
-    result.platform = this.getMobilePlatform(userAgent, isWebview);
-
-    return result;
-  },
-  /**
-   *
-   * @param userAgent {string} - либо передать вызов метода clientOs.getPlatform() который должен быть выполнент на клиенте
-   * @param isWebview {boolean}
-   * @returns {Number}
-   */
-  getMobilePlatform(userAgent, isWebview) {
-    const platformOs = this.getOsInfo(userAgent);
-
-    if (platformOs) {
-      return this.getOsPlatform(platformOs, isWebview);
-    }
-
-    return OS_TYPES.default;
-  },
-
-  getOsPlatform(platformOs, isWebview) {
-    if (!platformOs) {
-      return OS_TYPES.default;
-    }
-    if (clientOsPlatforms.android.includes(platformOs)) {
-      return isWebview ? OS_TYPES.webviewAndroid : OS_TYPES.android;
-    }
-    if (clientOsPlatforms.ios.includes(platformOs)) {
-      return isWebview ? OS_TYPES.webviewIos : OS_TYPES.ios;
-    }
-    return OS_TYPES.web;
-  },
-  getWebview(isWebview, cookies) {
     return isWebview ? this.getWebviewApp(cookies) : WEBVIEW_TYPES.VueJS;
-  },
-  getOsInfo(userAgent = "") {
-    const currentOs = clientOsData.find(item => userAgent.search(item.regex) >= 0);
-
-    return currentOs?.name ?? "";
   },
 
   isWebview(cookies = "") {
