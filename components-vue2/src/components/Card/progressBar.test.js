94f5eb563f47cfc28933858522e9d8292f5260df
Андрей Соцков <sotaa@reso.ru>
2025-03-03 15:56:07 +0300

    LK2-1539 Add test about update stepId

--- components-vue2/src/components/Card/progressBar.test.js
+++ components-vue2/src/components/Card/progressBar.test.js
@@ -2,6 +2,8 @@ import { createLocalVue, mount } from "@vue/test-utils";
 import Vuex from "vuex";
 import ProgressBar from "./ProgressBar.vue";
 import progressBarMock from "./progressBar.mock";
+// eslint-disable-next-line
+import ControlDropdown from "../../../../components/Libs/Controls/ControlDropdown/ControlDropdown.vue";
 
 const propsData = progressBarMock;
 describe("Wizard ProgressBar", () => {
@@ -18,5 +20,20 @@ describe("Wizard ProgressBar", () => {
 
     expect(htmlData).toContain("Данные об авто");
     expect(htmlData).toContain("<h2>Личные данные</h2>");
+  });
+
+  test("Change current step", async () => {
+    const newId = propsData.wizardCursor[0].ID;
+    wrapper = mount(ProgressBar, {
+      propsData,
+      stubs: {ControlDropdown}
+    });
+
+    expect(wrapper.emitted().update).toBeFalsy();
+
+    wrapper.vm.goToTab(newId);
+
+    expect(wrapper.emitted().update).toBeTruthy();
+    expect(wrapper.emitted().update[0][0]).toEqual(newId);
   });
 });
