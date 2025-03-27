7f50a720d1f6fd73ce45b1175a9b14bbe163ea44
Андрей Соцков <sotaa@reso.ru>
2025-03-27 17:47:30 +0300

    LK2-1653 Add base template and props

--- components-vue2/src/components/AsyncModalAction/AsyncModalAction.vue
+++ components-vue2/src/components/AsyncModalAction/AsyncModalAction.vue
@@ -1,13 +1,36 @@
 <script>
 export default {
-  name: "AsyncModalAction"
-}
+  name: "AsyncModalAction",
+  props: {
+    data: {
+      type: Object,
+      default() {
+        return {
+          value: "",
+          label:"",
+        };
+      },
+    },
+  },
+  data() {
+    return {
+      isDialogOpen: false
+    }
+  }
+};
 </script>
 
 <template>
-
+  <dialog :open="isDialogOpen">
+    <header>
+      <span>{{ data.label }}</span>
+      <button type="button">&#2715;</button>
+    </header>
+    <main>
+      {{ data.value }}
+    </main>
+    <footer></footer>
+  </dialog>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
