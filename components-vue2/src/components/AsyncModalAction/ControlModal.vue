60eee69a1cdd61a6eeae3ff8cb8abe2f18db516a
Андрей Соцков <sotaa@reso.ru>
2025-03-28 10:03:40 +0300

    LK2-1653 Add dialog component. Refactor AsyncModalAction

---
+++ components-vue2/src/components/AsyncModalAction/ControlModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <dialog>
+    <header>
+      <span>{{ data.label }}</span>
+      <button type="button" @click="closeModal">&#2715;</button>
+    </header>
+
+    <main>
+      {{ data.value }}
+    </main>
+
+    <footer></footer>
+  </dialog>
+</template>
+
+<script>
+export default {
+  name: "ControlModal",
+  props: {
+    data: {
+      type: Object,
+      default() {
+        return {
+          value: "",
+          title: "Пожалуйста подождите",
+        };
+      },
+    },
+  },
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
