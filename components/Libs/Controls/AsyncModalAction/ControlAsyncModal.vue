9013a5ebea927709b98921d6fc5d01f8b3d0ceeb
Андрей Соцков <sotaa@reso.ru>
2025-04-01 17:34:19 +0300

    LK2-1653 Add execute request with timeouts logic

--- components/Libs/Controls/AsyncModalAction/ControlAsyncModal.vue
+++ components/Libs/Controls/AsyncModalAction/ControlAsyncModal.vue
@@ -29,7 +29,8 @@ export default {
       type: Object,
       default() {
         return {
-          value: "Проверяем данные в АИС Страхование, дождитесь завершения операции",
+          value:
+            "Проверяем данные в АИС Страхование, дождитесь завершения операции",
           label: "Пожалуйста подождите",
           // число попыток выполнить один запрос
           attempts: 6,
@@ -55,13 +56,14 @@ export default {
     },
     dialogBodyText() {
       return this.responseData?.SMESSAGE ?? this.data.value;
-    }
+    },
   },
   data() {
     return {
       isOpen: false,
       abortController: null,
       responseData: null,
+      currentAttempt: this.data.attempts,
     };
   },
   methods: {
@@ -70,32 +72,47 @@ export default {
       this.$refs.modal.openModal();
     },
     openModalHandler() {
-      this.executeAction();
+      this.getRequestData();
     },
-    executeAction() {
-      this.abortController = new AbortController();
-      this.$axios.post(
-        "am/main/v2/osago/CreatePolicySendNsis",
-        { ID: this.cardId },
-        { signal: this.abortController.signal }
-      ).then(data => this.setData(data.data))
-        .catch(err => console.warn(`Ошибка при попытке получить данные по текущей карточке: ${err}`));
+    getRequestData() {
+      this.responseData = null;
 
-      setTimeout(() => {
-        this.abortController?.abort();
-      }, this.intervalComputed)
+      this.executeRequest();
+    },
+    executeRequest() {
+      this.abortController = new AbortController();
+      this.$axios
+        .post(
+          "am/main/v2/osago/CreatePolicySendNsis",
+          { ID: this.cardId },
+          { signal: this.abortController.signal }
+        )
+        .then((data) => this.setData(data.data))
+        .catch((err) =>
+          console.warn(
+            `Ошибка при попытке получить данные по текущей карточке: ${err}`
+          )
+        );
+    },
+    closeRequest() {
+      this.abortController?.abort();
     },
     executeWithTimeout(attempts = this.attemptsComputed) {
-      if (attempts) {
-        this.executeAction();
+      if (!attempts || this.responseData) {
+        return;
       }
-      setTimeout(() => {
 
+      this.executeRequest();
+
+      setTimeout(() => {
+        this.closeRequest();
+
+        this.executeWithTimeout(attempts - 1);
       }, this.intervalComputed);
     },
     setData(data) {
       this.responseData = data[0] ? { ...data[0] } : null;
-    }
+    },
   },
 };
 </script>
