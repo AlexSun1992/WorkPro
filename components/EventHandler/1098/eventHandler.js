32c7dcb5d7cb44671eeb48f02124a4b4f375c484
Тафинцев Артём <tafaa@reso.ru>
2025-04-24 10:38:54 +0000

    LK2-1594: feat(LK-1594): change 1093 1098

--- components/EventHandler/1098/eventHandler.js
+++ components/EventHandler/1098/eventHandler.js
@@ -1,63 +1,70 @@
-function eventHandler(data, item, callback){
-
-  const field = data.find((f) => f.fieldId === item.fieldId);
-  const pencil_one = data.find((f) => f.name === "SURL_PERIODS");
-  const stoa = data.find((f) => f.name === "IDSTOA");
-
-  if(item.name === "SURL_PERIODS"){
-    console.log('click')       ;
+function eventHandler(data, item, callback) {
+  function findField(name) {
+    const field = data.find((field) => field.name === name);
+    if (field) {
+      return field;
+    }
+    console.warn("can't find field", name);
+    return {};
+  }
+  function changeVisibleSafety(name, value) {
+    const field = findField(name);
+    if (field) {
+      field.visible = value;
+    }
   }
 
-  if (item.name === 'SURL_TECH') {
-    stoa.visible = true;
-    data.find((f) => f.name === "STECH_INFO").visible = false;
-    data.find((f) => f.name === "SURL_TECH").visible = false;
+  const stoa = findField("IDSTOA");
 
+  if (item.name === "SURL_TECH") {
+    stoa.visible = true;
+    changeVisibleSafety("STECH_INFO", false);
+    changeVisibleSafety("SURL_TECH", false);
   }
   if (stoa.visible === true) {
     if (stoa.value == 2) {
-      data.find((f) => f.name === "IDLIST_STOA").visible = true;
-      data.find((f) => f.name === "SADDRESS_STOA").visible = false;
-    }
-    else if (stoa.value == 4) {
-      data.find((f) => f.name === "IDLIST_STOA").visible = false;
-      data.find((f) => f.name === "SADDRESS_STOA").visible = true;
-    }
-    else {
-      data.find((f) => f.name === "IDLIST_STOA").visible = false;
-      data.find((f) => f.name === "SADDRESS_STOA").visible = false;
+      changeVisibleSafety("IDLIST_STOA", true);
+      changeVisibleSafety("SADDRESS_STOA", false);
+    } else if (stoa.value == 4) {
+      changeVisibleSafety("IDLIST_STOA", false);
+      changeVisibleSafety("SADDRESS_STOA", true);
+    } else {
+      changeVisibleSafety("IDLIST_STOA", false);
+      changeVisibleSafety("SADDRESS_STOA", false);
     }
   }
-  if (item.name === 'SURL_ADD_DATA') {
-    data.find((f) => f.name === "IDLAST_SERIES_EDIT").visible = true;
-    data.find((f) => f.name === "SLAST_NUMBER_EDIT").visible = true;
-    data.find((f) => f.name === "IDLAST_COMPANY_EDIT").visible = true;
-    data.find((f) => f.name === "SADD_INFO_EDIT").visible = true;
+  if (item.name === "SURL_ADD_DATA") {
+    changeVisibleSafety("IDLAST_SERIES_EDIT", true);
+    changeVisibleSafety("SLAST_NUMBER_EDIT", true);
+    changeVisibleSafety("IDLAST_COMPANY_EDIT", true);
+    changeVisibleSafety("SADD_INFO_EDIT", true);
 
-    data.find((f) => f.name === "SLAST_SERIES").visible = false;
-    data.find((f) => f.name === "SLAST_NUMBER").visible = false;
-    data.find((f) => f.name === "SLAST_COMPANY").visible = false;
-    data.find((f) => f.name === "SADD_INFO").visible = false;
+    changeVisibleSafety("SLAST_SERIES", false);
+    changeVisibleSafety("SLAST_NUMBER", false);
+    changeVisibleSafety("SLAST_COMPANY", false);
+    changeVisibleSafety("SADD_INFO", false);
 
-    data.find((f) => f.name === "SURL_ADD_DATA").visible = false;
+    changeVisibleSafety("SURL_ADD_DATA", false);
   }
-  if (data.find((f) => f.name === "SVEHICLE_DATA").visible === true)  {
-    stoa.visible = false;
-    data.find((f) => f.name === "IDLIST_STOA").visible = false;
-    data.find((f) => f.name === "SADDRESS_STOA").visible = false;
-    data.find((f) => f.name === "IDLAST_SERIES_EDIT").visible = false;
-    data.find((f) => f.name === "SLAST_NUMBER_EDIT").visible = false;
-    data.find((f) => f.name === "IDLAST_COMPANY_EDIT").visible = false;
-    data.find((f) => f.name === "SADD_INFO_EDIT").visible = false;
+  const vehicleData = findField("SVEHICLE_DATA");
+  if (vehicleData.visible) {
+    changeVisibleSafety("IDSTOA", false);
 
-    data.find((f) => f.name === "STECH_INFO").visible = false;
-    data.find((f) => f.name === "SURL_TECH").visible = false;
-    data.find((f) => f.name === "SLAST_SERIES").visible = false;
-    data.find((f) => f.name === "SLAST_NUMBER").visible = false;
-    data.find((f) => f.name === "SLAST_COMPANY").visible = false;
-    data.find((f) => f.name === "SADD_INFO").visible = false;
-    data.find((f) => f.name === "SURL_ADD_DATA").visible = false;
+    changeVisibleSafety("IDLIST_STOA", false);
+    changeVisibleSafety("SADDRESS_STOA", false);
+    changeVisibleSafety("IDLAST_SERIES_EDIT", false);
+    changeVisibleSafety("SLAST_NUMBER_EDIT", false);
+    changeVisibleSafety("IDLAST_COMPANY_EDIT", false);
+    changeVisibleSafety("SADD_INFO_EDIT", false);
+
+    changeVisibleSafety("STECH_INFO", false);
+    changeVisibleSafety("SURL_TECH", false);
+    changeVisibleSafety("SLAST_SERIES", false);
+    changeVisibleSafety("SLAST_NUMBER", false);
+    changeVisibleSafety("SLAST_COMPANY", false);
+    changeVisibleSafety("SADD_INFO", false);
+    changeVisibleSafety("SURL_ADD_DATA", false);
   }
 
-  return data
+  return data;
 }
