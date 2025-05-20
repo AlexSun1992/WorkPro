4e797ac8e4ab63991312130a9e752e578ed3cdb9
Гаврилов Алексей <gavan@reso.ru>
2025-05-20 13:02:46 +0000

    LK2-1797: LK2-1797 заменил название кнопок

--- components/EventHandler/1102/eventHandler.js
+++ components/EventHandler/1102/eventHandler.js
@@ -2,7 +2,9 @@
   function scrollToCardHead() {
     const selector = ".wizard_osago";
 
-    document.querySelector(selector)?.scrollIntoView({behavior: "smooth", block: "start"});
+    document
+      .querySelector(selector)
+      ?.scrollIntoView({ behavior: "smooth", block: "start" });
   }
 
   function initHandler(data) {
@@ -14,8 +16,8 @@
   function eventHandler(data, item) {
     const newData = [...data];
 
-    if (item.name === "Item45847" && Array.isArray(data)) {
-      newData.find((f) => f.name === "Item45848").visible = true;
+    if (item.name === "Item46211" && Array.isArray(data)) {
+      newData.find((f) => f.name === "Item46215").visible = true;
       newData.find((f) => f.name === "Item45849").visible = false;
     }
 
