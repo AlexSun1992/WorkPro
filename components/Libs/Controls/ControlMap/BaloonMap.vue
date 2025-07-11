cd461204372776a219340851bbdfad4a4a891e60
Филимонова Виолетта <filvv@reso.ru>
2025-07-11 11:04:01 +0000

    LK2-1836: LK2-1836 Отображение нескольких точек на карте Map (Map 2) с одинаковыми координатами

--- components/Libs/Controls/ControlMap/BaloonMap.vue
+++ components/Libs/Controls/ControlMap/BaloonMap.vue
@@ -1,48 +1,53 @@
 <template>
   <div>
     <div
-      class="map-balloon"
-      v-if="isShow"
+      v-for="card in data"
+      :key="card.ID"
     >
-      <div class="map-balloon-title">
-        {{ data.SNAME }}
-      </div>
-      <div class="map-balloon-adress">
-        {{ data.SADDRESS }}
-      </div>
-      <button
-        id="btn"
-        type="button"
-        class="btn-secondary mt-4"
-        @click="redirect(data.SREDIRECT)"
-      >
-        {{ data.SBUTTONTEXT[0] }}
-      </button>
-    </div>
-
-    <div
-      class="map-balloon"
-      v-else
-    >
-      <div class="map-balloon-title">{{ data.SNAME }}</div>
-      <div class="map-balloon-adress">Адрес: {{ data.SADDRESS }}</div>
-      <div class="map-balloon-description mt-3">{{ data.SCOMMENT }}</div>
       <div
-        class="mt-2"
-        v-for="item in data.SPHONE"
-        :key="item.SPHONEID"
+        class="map-balloon"
+        v-if="isShow"
       >
-        <a :href="`tel:${item.SPHONE}`">{{ item.SPHONE }}</a>
-        {{ item.SPHONE_TEXT }}<br />
+        <div class="map-balloon-title">
+          {{ card.SNAME }}
+        </div>
+        <div class="map-balloon-adress">
+          {{ card.SADDRESS }}
+        </div>
+        <button
+          :id="card.ID"
+          type="button"
+          class="btn-secondary mt-4 btn-baloon"
+          @click="redirect(card)"
+        >
+          {{ card.SBUTTONTEXT[0] }}
+        </button>
       </div>
-      <button
-        v-if="isShowDefaulteButton"
-        id="btn"
-        type="button"
-        class="btn-secondary mt-4"
+
+      <div
+        class="map-balloon"
+        v-else
       >
-        Выбрать
-      </button>
+        <div class="map-balloon-title">{{ card.SNAME }}</div>
+        <div class="map-balloon-adress">Адрес: {{ card.SADDRESS }}</div>
+        <div class="map-balloon-description mt-3">{{ card.SCOMMENT }}</div>
+        <div
+          class="mt-2"
+          v-for="item in card.SPHONE"
+          :key="item.SPHONEID"
+        >
+          <a :href="`tel:${item.SPHONE}`">{{ item.SPHONE }}</a>
+          {{ item.SPHONE_TEXT }}<br />
+        </div>
+        <button
+          v-if="isShowDefaulteButton"
+          :id="card.ID"
+          type="button"
+          class="btn-secondary mt-4 btn-baloon"
+        >
+          Выбрать
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -57,16 +62,21 @@ export default {
       default: () => {},
     },
   },
+  data() {
+    return {
+      isChooseButton: false,
+    };
+  },
   methods: {
-    redirect(link) {
-      if (link) {
+    redirect(card) {
+      if (card.SREDIRECT) {
         this.$router.push(link);
       }
     },
   },
   computed: {
     isShow() {
-      return "SBUTTONTEXT" in this.data && this.data.SBUTTONTEXT.length;
+      return this.data.every((el) => "SBUTTONTEXT" in el && el.SBUTTONTEXT.length);
     },
   },
   isShowDefaultButton() {
