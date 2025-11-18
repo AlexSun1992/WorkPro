a828575983f850a0be57194cdef6d3c7a5f5d476
Тафинцев Артём <tafaa@reso.ru>
2025-11-18 17:02:20 +0300

    fix(LK2-2206): change date modify

--- components/EventHandler/1093/eventHandler.js
+++ components/EventHandler/1093/eventHandler.js
@@ -115,7 +115,7 @@ function getDate(str) {
   const splitSrt = str?.split(".");
 
   if (Array.isArray(splitSrt) && splitSrt.length === 3) {
-    return new Date(splitSrt.reverse());
+    return new Date(splitSrt.reverse().join("-"));
   }
 
   return null;
