466f893860eb2afa4a666c477fc437346b28af90
Тафинцев Артём <tafaa@reso.ru>
2025-11-21 07:55:19 +0000

    LK2-2150: feat(LK2-2150): add timer for actionButton

--- components/EventHandler/1101/eventHandler.js
+++ components/EventHandler/1101/eventHandler.js
@@ -10,6 +10,22 @@ export function initHandler(data) {
   return data;
 }
 
-export function eventHandler(data, item) {
+export function eventHandler(data, item, action) {
+  const actionId = 46209;
+
+  if (action === "beforeSave") {
+    this.$store.commit("data_card/setFetchingAction", {
+      actionId,
+      isFetching: true,
+    });
+    this.$store.commit("data_card/saveButtonClicked", true);
+  }
+
+  if (action === "afterSave") {
+    this.$store.commit("data_card/setFetchingAction", {
+      actionId,
+      isFetching: false,
+    });
+  }
   return data;
 }
