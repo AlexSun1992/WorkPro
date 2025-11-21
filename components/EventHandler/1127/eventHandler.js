e0017f092616d82a3036768dcd4f844c430a5e39
Филимонова Виолетта <filvv@reso.ru>
2025-11-21 07:24:56 +0000

    LK2-2201: feat(LK2-2201): Поправить номер Item-ов в JS Антиклеща после обновления на проде

--- components/EventHandler/1127/eventHandler.js
+++ components/EventHandler/1127/eventHandler.js
@@ -3,16 +3,22 @@ import { scrollToCardHead } from "@/utils/scroll";
 export async function eventHandler(data, item, callback) {
   const copyData = JSON.parse(JSON.stringify(data));
   const field = copyData.find((f) => f.fieldId === item.fieldId);
+  const numberPhone = copyData.find((f) => f.name === "SPHOLDER_PHONENOAUTH");
+  const codePhone = copyData.find((f) => f.name === "SCODE_PHONE");
+
   if (!field) {
     return copyData;
   }
 
-  if (field.name === "Item47280") {
-    copyData.forEach((f) => {
-      if (f.name === "SCODE_PHONE") f.visible = true;
-      if (f.name === "Empty_1") f.visible = false;
-    });
+  if (field.name === "Item48361" && numberPhone.value?.length && numberPhone.state) {
+    codePhone.visible = true;
   }
+  if (numberPhone.state === false || !numberPhone.value.length) {
+    codePhone.visible = false;
+    codePhone.state = null;
+    codePhone.value = "";
+  }
+
   return copyData;
 }
 export function initHandler(data) {
