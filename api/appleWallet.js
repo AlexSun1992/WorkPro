9aa5b5a4320919ba6c8e676bfcd3a17ab4706679
Тафинцев Артём <tafaa@reso.ru>
2026-01-27 07:25:31 +0000

    LK2-1980: Resolve LK2-1980

--- api/appleWallet.js
+++ api/appleWallet.js
@@ -1,8 +1,9 @@
-/* eslint-disable */
+// eslint-disable-next-line import/extensions
 import { mobile2Service } from "../services/mobile2.services.mjs";
 
 const cookieParser = require("cookie-parser");
 const express = require("express");
+
 const app = express();
 const router = express.Router();
 
@@ -18,7 +19,7 @@ router.use(cookieParser());
 router.get("/walletpass/generate_pass", (req, res) => {
   try {
     const ipAddress = requestIp.getClientIp(req);
-    let mobile2ServiceInstance = mobile2Service();
+    const mobile2ServiceInstance = mobile2Service();
     if (req.headers.referer) {
       mobile2ServiceInstance.defaults.headers.common.Referer = req.headers.referer;
     }
@@ -27,31 +28,26 @@ router.get("/walletpass/generate_pass", (req, res) => {
     mobile2ServiceInstance.defaults.headers.common["Cookie"] = req.headers?.cookie ? req.headers.cookie : null;
     mobile2ServiceInstance.defaults.headers.common["x-forwarded-for"] = ipAddress || null;
 
-    if (req?.headers?.authorization) {
-      mobile2ServiceInstance.defaults.headers.common.Authorization = req.headers.authorization;
-    } else {
-      if (req?.cookies["auth._token.local"]) {
-        mobile2ServiceInstance.defaults.headers.common.Authorization = req?.cookies["auth._token.local"];
-      }
-    }
+    const authFromCookies = req?.cookies["auth._token.local"] || "";
+    const authorize = req?.headers?.authorization ? req.headers.authorization : authFromCookies;
+    mobile2ServiceInstance.defaults.headers.common.Authorization = authorize;
 
-    const URL_ADDRESS = `/policy/v1/rest/walletpass/generate_pass?policyID=${req.query.policyID}`; //2737247196
+    const URL_ADDRESS = `/policy/v1/rest/walletpass/generate_pass?policyID=${req.query.policyID}`;
     mobile2ServiceInstance({
       url: URL_ADDRESS,
       method: "GET",
+      responseType: "arraybuffer",
     })
       .then(async (resp) => {
+        const buffer = await resp.data;
         res.setHeader("Content-Type", "application/vnd.apple.pkpass");
         res.setHeader("Content-Disposition", 'attachment; filename="reso.pkpass"');
-        res.send(resp.data);
+        res.send(Buffer.from(buffer));
       })
       .catch((err) => {
+        console.log(err);
         if (err.response?.status !== 200) {
-          res.status(err.response.status).send(err.response.data);
-          return;
-        }
-        if (err?.response?.data?.STATUS === 401) {
-          res.status(err.response.data.STATUS).send(err.response.data);
+          res.status(err.response?.status || 500).send(err.response?.data || err);
         } else {
           res.status(err?.response?.data?.STATUS || 500).send(err?.response?.data || err);
         }
