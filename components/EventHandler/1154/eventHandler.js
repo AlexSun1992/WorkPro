bb393a06317b3ec6870a1b133a9d6567b6c9976b
Филимонова Виолетта <filvv@reso.ru>
2026-02-25 07:50:34 +0000

    LK2-2331: 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 LK2-2331 Добавить фронт проверки для полей DateEdit 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥

--- components/EventHandler/1154/eventHandler.js
+++ components/EventHandler/1154/eventHandler.js
@@ -1,4 +1,5 @@
 import { scrollToCardHead } from "@/utils/scroll";
+import { getDate } from "../helpers";
 import {
   getBoolean,
   getCopyData,
@@ -109,16 +110,6 @@ function isDatesLatestThenSomeYears(minDate, maxDate, years = 0) {
   modified.setFullYear(modified.getFullYear() + years);
 
   return maxDate.getTime() >= modified.getTime();
-}
-
-function getDate(str) {
-  const splitSrt = str?.split(".");
-
-  if (Array.isArray(splitSrt) && splitSrt.length === 3) {
-    return new Date(splitSrt.reverse().join("-"));
-  }
-
-  return null;
 }
 
 function setNestedFieldState(data, name, state, errMessage, isRequired) {
